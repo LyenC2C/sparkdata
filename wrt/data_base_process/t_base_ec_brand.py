30be369bf8ba4409307c137cfe8a53854f6337bf
wrt <wrt@cs200.(none)>
2015-11-04 11:41:51 +0800

    data_base_process

---
+++ wrt/data_base_process/t_base_ec_brand.py
@@ -0,0 +1,43 @@
+#coding:utf-8
+import sys
+import time
+import rapidjson  as json
+from pyspark import SparkContext
+from pyspark.sql.types import *
+
+sc = SparkContext(appName="spark brandinfo")
+sqlContext = SQLContext(sc)
+hiveContext=HiveContext(sc)
+
+def valid_jsontxt(content):
+	if type(content) == type(u""):
+		return content.encode("utf-8")
+    	else :
+        		return content
+def f(line):
+	line_s = valid_jsontxt(line_s)
+	ob = json.loads(line_s)
+	popularity = str(ob.get("popularity",0))
+	brandName = ob.get("brandName","-")
+	brandId = str(ob.get("brandId",0))
+	brand_list = []
+	brand_list.append(brandName)
+	brand_list.append(popularity)
+	brand_list.append(brandId)
+	return brand_list
+schema = StructType([
+	StructField("popularity",StringType(), True),
+	StructField("brandName",StringType(), True),
+	StructField("brandId",StringType(), True)
+	])
+
+if sys.argv[1] == '-h':
+	comment = '产品信息格式化为hive数据格式'
+		print comment
+		print 'argvs:\n argv[1]:brand info file or dir input\n '
+rdd = sc.textFile(sys.argv[1]).map(lambda x:f(x)\
+	.filter(lambda x:x!=None)\
+df = hiveContext.createDataFrame(rdd,schema)
+hiveContext.registerDataFrameAsTable(df,'data')
+hiveContext.sql('insert overwrite table t_base_ec_brand  PARTITION(ds='+ds2+') select * from data')
+sc.stop()
