d82b7b82140a5fd9806d4554a4e0484acabaab8e
zhanglj <zhanglinjiang@wolongdata.com>
2015-11-04 14:10:53 +0800

    modify

--- wrt/data_base_process/t_base_ec_brand.py
+++ wrt/data_base_process/t_base_ec_brand.py
@@ -1,21 +1,22 @@
 #coding:utf-8
 import sys
-import time
-import rapidjson  as json
 from pyspark import SparkContext
 from pyspark.sql.types import *
+from pyspark.sql import *
+
+import rapidjson  as json
 
 sc = SparkContext(appName="spark brandinfo")
 sqlContext = SQLContext(sc)
-hiveContext=HiveContext(sc)
+hiveContext= HiveContext(sc)
 
 def valid_jsontxt(content):
 	if type(content) == type(u""):
 		return content.encode("utf-8")
-    	else :
-        		return content
+	else :
+		return content
 def f(line):
-	line_s = valid_jsontxt(line_s)
+	line_s = valid_jsontxt(line)
 	ob = json.loads(line_s)
 	popularity = str(ob.get("popularity",0))
 	brandName = ob.get("brandName","-")
@@ -32,12 +33,17 @@ schema = StructType([
 	])
 
 if sys.argv[1] == '-h':
-	comment = '产品信息格式化为hive数据格式'
+		comment = '产品信息格式化为hive数据格式'
 		print comment
 		print 'argvs:\n argv[1]:brand info file or dir input\n '
-rdd = sc.textFile(sys.argv[1]).map(lambda x:f(x)\
-	.filter(lambda x:x!=None)\
+
+sc.textFile
+
+rdd = sc.textFile(sys.argv[1]).map(lambda x:f(x)).filter(lambda x:x!=None)
+
 df = hiveContext.createDataFrame(rdd,schema)
 hiveContext.registerDataFrameAsTable(df,'data')
+
+ds2='20151103'
 hiveContext.sql('insert overwrite table t_base_ec_brand  PARTITION(ds='+ds2+') select * from data')
 sc.stop()
