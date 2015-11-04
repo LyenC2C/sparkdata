59b796a750f16349c451875c5259b031af80a6fc
zhanglj <zhanglinjiang@wolongdata.com>
2015-11-04 17:14:51 +0800

    change

--- zlj/project/simpic/histsimilar.py
+++ zlj/project/simpic/histsimilar.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
 	f1='/home/zlj/pic/38574619410_0.jpg'
 	listfile=os.listdir('/home/zlj/pic')
 	for fw in  listfile:
-		print f1,fw,calc_similar_by_path(f1,fw)
+		print f1,fw,calc_similar_by_path(f1,'/home/zlj/pic/'+fw)
 	# print calc_similar_by_path("/home/zlj/pic/38574619410_1.jpg","/home/zlj/pic/38574619410_0.jpg")
 	# path = r'D:\test'
 	# for i in xrange(1, 7):
