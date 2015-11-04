c228312f8293e9c8497deb612bc65bb7e29b78d9
zhanglj <zhanglinjiang@wolongdata.com>
2015-11-04 15:45:41 +0800

    modify

--- zlj/project/base_data_process/hive/feed/load.sql
+++ zlj/project/base_data_process/hive/feed/load.sql
@@ -7,15 +7,17 @@ set hive.exec.reducers.bytes.per.reducer=500000000;
 
 use wlbase_dev;
 
-LOAD DATA  INPATH '/data/develop/ec/tb/cmt_res_tmp/res2013/cmt2013' OVERWRITE INTO TABLE default.t_base_ec_item_feed_dev_zlj PARTITION (ds='20150000');
--- LOAD DATA  INPATH '/hive/warehouse/wlbase_dev.db/t_base_ec_item_feed_dev_test/ds=20150000' OVERWRITE INTO TABLE t_base_ec_item_feed_dev_zlj PARTITION (ds='20150000');
+-- LOAD DATA  INPATH '/user/zlj/data/res2015_re' OVERWRITE INTO TABLE t_base_ec_item_feed_dev PARTITION (ds='20100000');
+-- LOAD DATA  INPATH '/user/zlj/data/res2014_re' OVERWRITE INTO TABLE t_base_ec_item_feed_dev_zlj PARTITION (ds='20100000');
+LOAD DATA  INPATH '/hive/warehouse/wlbase_dev.db/t_base_ec_item_feed_dev/ds=20100000' OVERWRITE INTO TABLE t_base_ec_item_feed_dev_zlj PARTITION (ds='20100005');
+
 
 
 INSERT overwrite  TABLE t_base_ec_item_feed_dev PARTITION (ds )
 
 select
 item_id,
-
+item_title,
 feed_id,
 user_id,
 content,
@@ -24,9 +26,9 @@ annoy  ,
 ts ,
 regexp_replace(f_date,'-','') ds
 from
-default.t_base_ec_item_feed_dev_zlj
+t_base_ec_item_feed_dev_zlj
 where
-ds='20150000' and LENGTH(f_date)<12
+ds='20100005' and LENGTH(f_date)<12
 ;
 
 
--- zlj/project/datahive/perfer/hive/ freq.sql
+++ zlj/project/datahive/perfer/hive/ freq.sql
@@ -1,10 +1,29 @@
 
 
+SET hive.exec.dynamic.partition=true;
+SET hive.exec.dynamic.partition.mode=nonstrict;
+SET hive.exec.max.dynamic.partitions.pernode = 1000;
+SET hive.exec.max.dynamic.partitions=2000;
 
-select  user_id ,week ,count(1) as times
- from
-(
-select user_id, month()weekofyear(f_date)  as week
-from t_base_ec_item_feed_dev
-where ds>20150101
-)
+set hive.exec.reducers.bytes.per.reducer=500000000;
+
+use wlbase_dev;
+
+
+create table t_zlj_userbuy_freq  as
+SELECT
+  user_id,
+  week,
+  t_month ,
+  count(1) AS times
+FROM
+  (
+    SELECT
+      item_id,
+      user_id,
+      month(f_date) as t_month,
+      weekofyear(f_date) AS week
+    FROM t_base_ec_item_feed_dev
+    WHERE ds > 20150101
+  ) t
+group by user_id ,week, t_month
