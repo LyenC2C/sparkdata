158046578194d3c970ace29a45a1305193660849
Gbacillus <gs464gan@gmail.com>
2015-11-05 17:19:36 +0800

    Merge branch 'master' of 192.168.4.254:datamind/sparkdata

--- wrt/data_base_process/item_pinpai.py
+++ wrt/data_base_process/item_pinpai.py
@@ -24,7 +24,7 @@ def pinpai_en(line):
     return (valid_jsontxt(ss[0]),None)
 def f(x,p_dict,pe_dict):
     n = 0
-    if valid_jsontxt(x[0]) in p_dict.keys() or valid_jsontxt(x[0]) in pe_dict().keys():
+    if valid_jsontxt(x[0]) in p_dict.keys() or valid_jsontxt(x[0]) in pe_dict.keys():
         return x[1] + "\t" + x[0]
 
 hiveContext.sql('use wlbase_dev')
