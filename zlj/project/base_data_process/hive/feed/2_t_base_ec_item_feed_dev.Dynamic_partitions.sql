481df6b00a7bece9f72a9962340e2ebc94bb81de
zhanglj <zhanglinjiang@wolongdata.com>
2015-11-05 13:45:24 +0800

    每天评论增量

--- zlj/project/base_data_process/hive/feed/2_t_base_ec_item_feed_dev.Dynamic_partitions.sql
+++ zlj/project/base_data_process/hive/feed/2_t_base_ec_item_feed_dev.Dynamic_partitions.sql
@@ -1,3 +1,27 @@
+
+#!/bin/bash
+
+source /etc/profile;
+
+m=1
+m_1=`expr $m + 1`
+d_1=$(date -d '-'$m' day' '+%Y-%m-%d')
+d_2=$(date -d '-'$m_1' day' '+%Y-%m-%d')
+
+
+-- d_1=$(date -d '-1 day' '+%Y-%m-%d')
+-- d_2=$(date -d '-2 day' '+%Y-%m-%d')
+
+lastmonth=$(date -d '-1 month' '+%Y-%m-%d')
+path=$2
+
+
+
+##hive  部分
+
+/home/zlj/hive/bin/hive<<EOF
+
+
 SET hive.exec.dynamic.partition=true;
 SET hive.exec.dynamic.partition.mode=nonstrict;
 SET hive.exec.max.dynamic.partitions.pernode = 1000;
@@ -7,24 +31,14 @@ set hive.exec.reducers.bytes.per.reducer=500000000;
 
 use wlbase_dev;
 
-LOAD DATA  INPATH '/data/develop/ec/tb/cmt_tmp_p50' OVERWRITE INTO TABLE t_base_ec_item_feed_dev_zlj PARTITION (ds='20101103');
-
--- -- 每日新增feed量统计
--- INSERT INTO TABLE t_base_ec_feed_add_everyday PARTITION (ds )
---
--- SELECT
---     item_id,
---    count(1)
---     cast(from_unixtime(unix_timestamp()-86400,'yyyyMMdd') as STRING) ds
---     FROM t_base_ec_item_feed_dev_test where ds=20150000
--- group by item_id ;
+LOAD DATA  INPATH '$path' OVERWRITE INTO TABLE t_base_ec_item_feed_dev_zlj PARTITION (ds='20101103');
 
 
 
 -- 动态分区
 -- INSERT overwrite TABLE t_base_ec_item_feed_dev PARTITION (ds )
 
-INSERT INTO TABLE t_base_ec_item_feed_dev_zlj PARTITION (ds )
+INSERT INTO TABLE t_base_ec_item_feed_dev PARTITION (ds )
 select
 t3.item_id,
 t3.item_title,
@@ -41,7 +55,7 @@ from
   t2.*
   ,
   case when t1.item_id is NULL  then 1
-  when t1.item_id is not NULL and t1.feedid<t2.feedid then 1
+  when t1.item_id is not NULL and t1.maxfeed_id<t2.feedid then 1
   else 0 end  as rn
   from
   (
@@ -49,7 +63,8 @@ from
     *
     from
     t_base_ec_feed_add_everyday
-    where ds=cast(from_unixtime(unix_timestamp()-86400*3,'yyyyMMdd') as String)
+--     where ds=cast(from_unixtime(unix_timestamp()-86400*3,'yyyyMMdd') as String)
+    where ds='$d_2'
 
   )t1
   RIGHT  OUTER  join
@@ -64,7 +79,7 @@ from
     annoy  ,
     ts ,
     regexp_replace(f_date,'-','') ds
-    FROM t_base_ec_item_feed_dev_zlj where ds=20101103
+    FROM t_base_ec_item_feed_dev_zlj where ds=20101103  and item_id rlike   '^\\d+$'
 
   )t2 on t1.item_id=t2.item_id
 )t3 where rn=1 ;
@@ -75,27 +90,32 @@ from
 -- 最大分区
 insert overwrite  table t_base_ec_feed_add_everyday PARTITION(ds)
   select
-  item_id,max(feedid) as feedid ,sum(feed_times)
-,cast(from_unixtime(unix_timestamp()-86400*2,'yyyyMMdd') as STRING) ds
+  item_id,max(maxfeed_id) as feedid ,sum(feed_times)
+-- ,cast(from_unixtime(unix_timestamp()-86400*2,'yyyyMMdd') as STRING) ds
+,'$d_1' ds
   from
 (
    SELECT
-    *
+    item_id,
+    maxfeed_id,
+    feed_times
     from
     t_base_ec_feed_add_everyday
-       where ds=cast(from_unixtime(unix_timestamp()-86400*3,'yyyyMMdd') as STRING)
+--        where ds=cast(from_unixtime(unix_timestamp()-86400*3,'yyyyMMdd') as STRING)
+     where ds='$d_2' and item_id rlike   '^\\d+$'
     UNION  ALL
      SELECT
     item_id,
-    max(CAST (feed_id as bigint)) feedid ,
+    max(CAST (feed_id as bigint)) maxfeed_id ,
     count(1) as feed_times
 
-    FROM t_base_ec_item_feed_dev_zlj where ds=20101103
+    FROM t_base_ec_item_feed_dev_zlj where ds=20101103 and item_id rlike   '^\\d+$'
     group by item_id
 
 )t group by item_id  ;
 
 
+EOF
 
 -- insert overwrite  table t_base_ec_feed_add_everyday PARTITION(ds='20151102')
 -- select item_id,max(CAST (feed_id as bigint))  maxfeed_id, count(1) as feed_times from t_base_ec_item_feed_dev
