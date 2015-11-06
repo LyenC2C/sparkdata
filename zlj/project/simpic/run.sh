86052aad828834033cb53f688753fbba5053c03c
zhanglj <zhanglinjiang@wolongdata.com>
2015-11-06 17:55:48 +0800

    偏好 代码整理 相似图片并行化

---
+++ zlj/project/simpic/run.sh
@@ -0,0 +1,18 @@
+python  histsimilar.py  xaa filesort &
+python  histsimilar.py  xab filesort &
+python  histsimilar.py  xac filesort &
+python  histsimilar.py  xad filesort &
+python  histsimilar.py  xae filesort &
+python  histsimilar.py  xaf filesort &
+python  histsimilar.py  xag filesort &
+python  histsimilar.py  xah filesort &
+python  histsimilar.py  xai filesort &
+python  histsimilar.py  xag filesort &
+python  histsimilar.py  xak filesort &
+python  histsimilar.py  xal filesort &
+python  histsimilar.py  xam filesort &
+python  histsimilar.py  xan filesort &
+python  histsimilar.py  xao filesort &
+python  histsimilar.py  xap filesort &
+python  histsimilar.py  xaq filesort &
+
