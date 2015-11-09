683d18ca105d04455db000f622179e790b4a328c
pengzhongzheng <pengzhongzheng@wolongdata.com>
2015-11-09 14:55:42 +0800

    cmt 数据动态分区入库

--- pzz/sh/cmt_inc_clean.sh
+++ pzz/sh/cmt_inc_clean.sh
@@ -5,9 +5,17 @@ all_feed_output=/data/develop/ec/tb/cmt_allfeedid.20151105
 new_feed_output=/data/develop/ec/tb/cmt_newfeedid.20151105
 tmp_data=/data/develop/ec/tb/cmt_tmpdata.20151105
 
-#hadoop fs -rmr $all_feed_output
-#hadoop fs -rmr $new_feed_output
-#hadoop fs -rmr $tmp_data
-#spark-submit --executor-memory 20g --driver-memory 20g --total-executor-cores 100 /mnt/pzz/workspace/sparkdata/pzz/cmt/cmt_inc_clean.py -gen_data_inc /data/develop/ec/tb/cmt_dbfeedid.20151103/part* /commit/comments/*2015-11-05 $all_feed_output $new_feed_output $tmp_data
+hadoop fs -rmr $all_feed_output
+hadoop fs -rmr $new_feed_output
+hadoop fs -rmr $tmp_data
 
+echo 'cp result data for test..'
+hadoop fs -cp $tmp_data ${tmp_data}.test
+
+spark-submit --executor-memory 20g --driver-memory 20g --total-executor-cores 100 /mnt/pzz/workspace/sparkdata/pzz/cmt/cmt_inc_clean.py -gen_data_inc /data/develop/ec/tb/cmt_dbfeedid.20151103/part* /commit/comments/*2015-11-05 $all_feed_output $new_feed_output $tmp_data
+
+echo 'cp result data for test..'
+hadoop fs -cp $tmp_data ${tmp_data}.test
+
+echo 'insert hive'
 sh  /mnt/pzz/workspace/sparkdata/pzz/sh/feed.Dynamic_partitions.sql ${tmp_data}.test
