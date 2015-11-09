b7e2e7af006888a3864dfb960b5e02bcb1cc5f7b
Gbacillus <gs464gan@gmail.com>
2015-11-09 11:07:44 +0800

    Merge branch 'master' of 192.168.4.254:datamind/sparkdata

--- wrt/data_base_process/DDL/item_tag_level.DDL.sql
+++ wrt/data_base_process/DDL/item_tag_level.DDL.sql
@@ -1,10 +1,11 @@
-CREATE  TABLE  if not exists item_tag_level(
+CREATE  TABLE  if not exists t_wrt_item_tag_level(
 brand_id   String COMMENT '品牌id',
 brand_name  String ,
 brand_level String ,
-brand_tag String ,
+stars String ,
+brand_tag String
 )
 COMMENT '电商品牌标签等级表'
 PARTITIONED BY  (ds STRING )
-ROW FORMAT DELIMITED FIELDS TERMINATED BY '\001'   LINES TERMINATED BY '\n'
+ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'   LINES TERMINATED BY '\n'
 stored as textfile ;
