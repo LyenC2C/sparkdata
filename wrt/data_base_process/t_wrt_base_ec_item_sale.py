4453596d85424b11c4d05307ff47f39a3e6c402b
Gbacillus <gs464gan@gmail.com>
2015-11-10 20:58:52 +0800

    update item_sale.py

--- wrt/data_base_process/t_wrt_base_ec_item_sale.py
+++ wrt/data_base_process/t_wrt_base_ec_item_sale.py
@@ -31,50 +31,54 @@ def float_k(x):
     else:
         return float(x)
 def f(line):
-    ss = line.strip().split('\t')
-    zhengwen = ""
-    ts = ss[0]
-    for ln in ss[3:]:
-        zhengwen += ln
-    l = len(zhengwen)
-    result = []
-    ob = json.loads(valid_jsontxt(zhengwen[zhengwen.find("({") + 1:l-1]))
-    if type(ob) != type({}):
-        return [None]
-    if not ob.has_key("data"):
-        return [None]
-    if not ob["data"].has_key("totalResults"):
-        return [None]
-    if not ob["data"].has_key("itemsArray"):
-        return [None]
-    if ob["data"]["totalResults"] == 0:
-        return [None]
-    itemsArray = ob["data"]["itemsArray"]
-    shop_id = ob["data"].get("shopId","-")
-    for item in itemsArray:
-        lv = []
-        item_id = item.get("auctionId","-")
-        item_title = item.get("title","-")
-        r_price = item.get("reservePrice",0.0)
-        s_price = item.get("salePrice",0.0)
-        bc_type = item.get("auctionType","-")
-        quantity = item.get("quantity",0)
-        total_sold = item.get("totalSoldQuantity",0)
-        order_cost = item.get("orderCost",0)
-        lv.append(valid_jsontxt(item_id))
-        lv.append(f_coding(item_title))
-        lv.append(float_k(r_price))
-        lv.append(float_k(s_price))
-        lv.append(valid_jsontxt(bc_type))
-        lv.append(0)
-        lv.append(0)
-        lv.append(0)
-        lv.append(valid_jsontxt(shop_id))
-        lv.append(ts)
-        result.append(lv)
-        # result.append('\001'.join([valid_jsontxt(i) for i in lv]))
-        #result.append(item_id + '\001' + r_price + '\001' + s_price + '\001' + bc_type + '\001' + quantity + '\001' + total_sold + '\001' + order_cost + '\001' + shop_id + '\001' + ts)
-    return result
+    try:
+        ss = line.strip().split('\t')
+        zhengwen = ""
+        ts = ss[0]
+        for ln in ss[3:]:
+            zhengwen += ln
+        l = len(zhengwen)
+        result = []
+        ob = json.loads(valid_jsontxt(zhengwen[zhengwen.find("({") + 1:l-1]))
+        if type(ob) != type({}):
+            return [None]
+        if not ob.has_key("data"):
+            return [None]
+        if not ob["data"].has_key("totalResults"):
+            return [None]
+        if not ob["data"].has_key("itemsArray"):
+            return [None]
+        if ob["data"]["totalResults"] == 0:
+            return [None]
+        itemsArray = ob["data"]["itemsArray"]
+        shop_id = ob["data"].get("shopId","-")
+        for item in itemsArray:
+            lv = []
+            item_id = item.get("auctionId","-")
+            item_title = item.get("title","-")
+            r_price = item.get("reservePrice",0.0)
+            s_price = item.get("salePrice",0.0)
+            bc_type = item.get("auctionType","-")
+            quantity = item.get("quantity",0)
+            total_sold = item.get("totalSoldQuantity",0)
+            order_cost = item.get("orderCost",0)
+            lv.append(valid_jsontxt(item_id))
+            lv.append(f_coding(item_title))
+            lv.append(float_k(r_price))
+            lv.append(float_k(s_price))
+            lv.append(valid_jsontxt(bc_type))
+            lv.append(int_k(quantity))
+            lv.append(int_k(total_sold))
+            lv.append(int_k(order_cost))
+            lv.append(valid_jsontxt(shop_id))
+            lv.append(ts)
+            result.append(lv)
+            # result.append('\001'.join([valid_jsontxt(i) for i in lv]))
+            #result.append(item_id + '\001' + r_price + '\001' + s_price + '\001' + bc_type + '\001' + quantity + '\001' + total_sold + '\001' + order_cost + '\001' + shop_id + '\001' + ts)
+        return result
+    except Exception,e:
+		print e
+		return [None]
 schema = StructType([
     StructField("item_id",StringType(), True),
 	StructField("item_title",StringType(), True),
