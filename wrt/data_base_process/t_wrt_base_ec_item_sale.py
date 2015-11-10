76f761faa8c2807424f2890c752b44ba072f2b36
Gbacillus <gs464gan@gmail.com>
2015-11-10 18:32:37 +0800

    update item_sale.py

--- wrt/data_base_process/t_wrt_base_ec_item_sale.py
+++ wrt/data_base_process/t_wrt_base_ec_item_sale.py
@@ -85,6 +85,7 @@ s = sys.argv[1]
 rdd = sc.textFile(s).flatMap(lambda x:f(x)).filter(lambda x:x!=None)
 df = hiveContext.createDataFrame(rdd, schema)
 hiveContext.registerDataFrameAsTable(df, 'data')
+st = s.find('2015')
 ds2 = s[st:st+4] + s[st+5:st+7] + s[st+8:st+10]
 hiveContext.sql('insert overwrite table t_base_ec_item_sale_dev PARTITION(ds=' + ds2 + ') select * from data')
 		#.saveAsTextFile("/user/wrt/item_sale")
