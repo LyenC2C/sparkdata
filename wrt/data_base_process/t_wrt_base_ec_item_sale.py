3b8b47112389de7b8e75f3f69759b5367beb272b
Gbacillus <gs464gan@gmail.com>
2015-11-10 16:51:56 +0800

    update item_sale.py

--- wrt/data_base_process/t_wrt_base_ec_item_sale.py
+++ wrt/data_base_process/t_wrt_base_ec_item_sale.py
@@ -80,7 +80,7 @@ rdd = sc.textFile("/commit/shopitem/tmall.shop.2.item.2015-10-27").flatMap(lambd
 df = hiveContext.createDataFrame(rdd, schema)
 
 hiveContext.registerDataFrameAsTable(df, 'data')
-ds2 = 20151027
-hiveContext.sql('insert overwrite table t_base_ec_item_sale PARTITION(ds='20151027') select * from data')
+ds2 = '20151027'
+hiveContext.sql('insert overwrite table t_base_ec_item_sale PARTITION(ds=' + ds2 + ') select * from data')
 		#.saveAsTextFile("/user/wrt/item_sale")
 sc.stop()
