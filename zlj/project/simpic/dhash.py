19b3122eab61cf9cf3c0016c39c438d03884a23d
zhanglj <zhanglinjiang@wolongdata.com>
2015-11-10 16:09:08 +0800

    t_base_ec_item_sale_dev  DDL

---
+++ zlj/project/simpic/dhash.py
@@ -0,0 +1,64 @@
+__author__ = 'zlj'
+from PIL import Image
+
+import imagehash
+
+"""
+Demo of hashing
+"""
+
+
+def is_image(filename):
+	f = filename.lower()
+	return f.endswith(".png") or f.endswith(".jpg") or \
+			f.endswith(".jpeg") or f.endswith(".bmp") or f.endswith(".gif")
+# def find_similar_images(userpath, hashfunc = imagehash.average_hash):
+# 	image_filenames = [os.path.join(userpath, path) for path in os.listdir(userpath) if is_image(path)]
+# 	images={}
+#     for img in sorted(image_filenames):
+#         hash = hashfunc(Image.open(img))
+#         itemid=img.split('/')[-1].split('_')[0]
+#         print itemid
+#         images[hash] = images.get(hash, []) + [img]
+#
+#     for k, img_list in images.iteritems():
+#         if len(img_list) > 1:
+#         print(" ".join(img_list))
+def find_sim(userpath, hashfunc = imagehash.average_hash):
+    image_filenames = [os.path.join(userpath, path) for path in os.listdir(userpath) if is_image(path)]
+    images={}
+    for img in sorted(image_filenames):
+        hash = hashfunc(Image.open(img))
+        # itemid=img.split('/')[-1].split('_')[0]
+        # print itemid
+        images[hash] = images.get(hash, []) + [img]
+
+    for k, img_list in images.iteritems():
+        if len(img_list) > 1:
+            s=set([i.split('/')[-1].split('_')[0] for i in img_list])
+            if len(s>1):
+                print(" ".join(s))
+if __name__ == '__main__':
+    import sys, os
+    def usage():
+        sys.stderr.write("""SYNOPSIS: %s [ahash|phash|dhash] [<directory>]
+Identifies similar images in the directory.
+Method:
+  ahash: Average hash
+  phash: Perceptual hash
+  dhash: Difference hash
+(C) Johannes Buchner, 2013
+""" % sys.argv[0])
+        sys.exit(1)
+
+    hashmethod = sys.argv[1] if len(sys.argv) > 1 else usage()
+    if hashmethod == 'ahash':
+        hashfunc = imagehash.average_hash
+    elif hashmethod == 'phash':
+        hashfunc = imagehash.phash
+    elif hashmethod == 'dhash':
+        hashfunc = imagehash.dhash
+    else:
+        usage()
+    userpath = sys.argv[2] if len(sys.argv) > 2 else "."
+    find_sim(userpath=userpath, hashfunc=hashfunc)
