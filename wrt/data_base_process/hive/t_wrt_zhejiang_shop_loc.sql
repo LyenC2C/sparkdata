c8dc60816b99354029ecb6cc67b56088bc6d74a7
Gbacillus <gs464gan@gmail.com>
2015-11-10 09:01:37 +0800

    Merge branch 'master' of 192.168.4.254:datamind/sparkdata

--- wrt/data_base_process/hive/t_wrt_zhejiang_shop_loc.sql
+++ wrt/data_base_process/hive/t_wrt_zhejiang_shop_loc.sql
@@ -1,3 +1,3 @@
 use wlbase_dev;
 create table t_wrt_zhejiang_shop_loc AS
-select shop_id, location from t_base_ec_shop_dev where substr(location,1,2) = '浙江' and ds = '20151030'
+select shop_id, location from t_base_ec_shop_dev where substr(location,1,2) = '浙江' and bc_type = 'B' and ds = '20151030'
