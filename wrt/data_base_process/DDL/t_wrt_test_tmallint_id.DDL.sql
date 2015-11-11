fa1681ea27205fadcb4376acc2b7d4cdd0fe2473
Gbacillus <gs464gan@gmail.com>
2015-11-11 16:57:39 +0800

    update item_sale.py

--- wrt/data_base_process/DDL/t_wrt_test_tmallint_id.DDL.sql
+++ wrt/data_base_process/DDL/t_wrt_test_tmallint_id.DDL.sql
@@ -1,6 +1,6 @@
 use wlbase_dev;
 CREATE  TABLE  if not exists t_wrt_test_tmallint_id(
-item_id	 STRING  COMMENT  '部分国际商品id',
+item_id	 STRING  COMMENT  '部分国际商品id'
 )
 COMMENT '部分国际商品销量测试'
 PARTITIONED BY  (ds STRING )
