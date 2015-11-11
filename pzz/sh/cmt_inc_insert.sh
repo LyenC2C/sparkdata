7f8fafdd1bfc85d35fb6d9949bd537d2573b8086
pengzhongzheng <pengzhongzheng@wolongdata.com>
2015-11-11 15:12:59 +0800

    cmt 数据动态分区入库

--- pzz/sh/cmt_inc_insert.sh
+++ pzz/sh/cmt_inc_insert.sh
@@ -13,7 +13,7 @@ tmp_data=$5
 hadoop fs -rmr $all_feed_output
 hadoop fs -rmr $new_feed_output
 hadoop fs -rmr $tmp_data
-spark-submit --executor-memory 24g --driver-memory 24g --total-executor-cores 120 /mnt/pzz/workspace/sparkdata/pzz/cmt/cmt_inc_clean.py -gen_data_inc $all_feed_input $new_data_input $all_feed_output $new_feed_output $tmp_data
+spark-submit --executor-memory 20g --driver-memory 20g --total-executor-cores 105 /mnt/pzz/workspace/sparkdata/pzz/cmt/cmt_inc_clean.py -gen_data_inc $all_feed_input $new_data_input $all_feed_output $new_feed_output $tmp_data
 
 
 #hive 入库
