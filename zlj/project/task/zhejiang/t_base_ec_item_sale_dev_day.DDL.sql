dc8fd0a54f911b3431bad85c6b142c314a9f2c8e
zhanglj <zhanglinjiang@wolongdata.com>
2015-11-12 14:28:07 +0800

    zhengjiang

--- zlj/project/task/zhejiang/t_base_ec_item_sale_dev_day.DDL.sql
+++ zlj/project/task/zhejiang/t_base_ec_item_sale_dev_day.DDL.sql
@@ -1,12 +1,13 @@
 
 
+use wlbase_dev;
+
 CREATE  TABLE  if not exists t_zlj_base_ec_item_sale_dev_day (
 shop_id   string  ,
 item_id   string ,
-s_num      	FLOAT ,
 price    FLOAT ,
-s_price  FLOAT
-
+sold_num  FLOAT ,
+day_sold_price      	FLOAT
 )
 COMMENT ''
 PARTITIONED BY  (ds STRING )
