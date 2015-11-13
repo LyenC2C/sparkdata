bb51a5ef3e6cad73cefc27b92a0f62dfcdceb286
zhanglj <zhanglinjiang@wolongdata.com>
2015-11-13 16:15:00 +0800

    标签数据整理

--- zlj/project/task/perfer/spark/user_perfer_price.py
+++ zlj/project/task/perfer/spark/user_perfer_price.py
@@ -22,66 +22,48 @@ hiveContext = HiveContext(sc)
 hiveContext.sql('use wlbase_dev')
 
 
-hiveContext.sql('drop table IF EXISTS  t_zlj_ec_perfer_priceavg')
-
-
-# sql='''create table t_zlj_ec_perfer_priceavg as
-#       select  user_id ,count(1)  buytimes,sum(price) as sum_price, avg(price) as avg_price
-#       from  t_zlj_ec_userbuy group by user_id    HAVING  avg(price)>0
-# '''
-
-
-sql='''create table t_zlj_ec_perfer_priceavg as
-      select  user_id ,count(1)  buytimes,sum(price) as sum_price, avg(price) as avg_price
-      from  t_zlj_ec_userbuy group by user_id    HAVING  avg(price)>0
-'''
-hiveContext.sql(sql)
-
-
+# hiveContext.sql('select * from t_zlj_ec_userbuy limit 10').collect()
 # rdd=hiveContext.sql('select cast(price as int) price from wlbase_dev.t_base_ec_item_dev where ds=20151030 and cast(price as int)>0').map(lambda x:x.price)
-rdd1=hiveContext.sql('select user_id,avg(price) avg_price from   t_zlj_ec_userbuy group by user_id    HAVING  avg(price)>0 ').map(lambda x:[x.user_id,x.avg_price])
+# rdd1=hiveContext.sql('select user_id,avg(price) avg_price from   t_zlj_ec_userbuy  where user_id rlike   "^\\\\d+$" group by user_id    HAVING  avg(price)>0 ').map(lambda x:[x.user_id,x.avg_price])
 
-rdd=rdd1.map(lambda x: x[1])
+rdd1=hiveContext.sql('select user_id,avg_price from t_zlj_ec_perfer_priceavg limit 10000 ').map(lambda x:[x.user_id,x.avg_price])
+rdd=rdd1.map(lambda x: x[1]).repartition(100)
 
 # rdd.filter(lambda  x:x<50000).histogram(1000)
 
 
 data=rdd.filter(lambda x:x<30000).map(lambda x:array(x))
-model = KMeans.train(
-    data, 5, maxIterations=20, runs=30, initializationMode="random",
-    seed=50, initializationSteps=5, epsilon=1e-4)
+model = KMeans.train( data, 5, maxIterations=20, runs=30, initializationMode="random",seed=50, initializationSteps=5, epsilon=1e-4)
+
+
+model.centers=sorted(model.centers)
 
 
 
-m={}
-l=[]
-for index,i in enumerate(model.centers):
-    l.append(i[0][0])
-
-sorted(l,reverse=True)
 # [array([ 947.35742005]), array([ 278.74380204]), array([ 2558.86612172]), array([ 41.31785555]), array([ 6119.56633409])]
 
-rdd2=sqlContext.sql('select user_id,price from wlbase_dev.t_zlj_ec_userbuy_1 where price>0')
-userlevel_rdd=rdd2.map(lambda  x: (x.user_id,model.predict(array([x.price]))))
+# rdd2=sqlContext.sql('select user_id,price from wlbase_dev.t_zlj_ec_userbuy_1 where price>0')
+userlevel_rdd=rdd1.map(lambda  x: (x[0],x[1],model.predict(x[1])))
 
 
 # rdd2.map(lambda  x: (x.user_id,x.price))
 schema = StructType([
            StructField("uid", StringType(), True),
+           StructField("avg_price", IntegerType(), True),
            StructField("ulevel",IntegerType(), True)])
 df=sqlContext.createDataFrame(userlevel_rdd,schema)
 sqlContext.registerDataFrameAsTable(df,'userlevel')
-df.cache()
+# df.cache()
 
-hiveContext.sql('use wlbase_dev')
-
-
-result=sqlContext.sql('select  uid,ulevel,num, row_number()  OVER (PARTITION BY uid ORDER BY num desc) as rn '
-               'from (select uid,ulevel,count(1) as num from userlevel group by uid, ulevel) t ')
+# hiveContext.sql('use wlbase_dev')
+#
+#
+# result=sqlContext.sql('select  uid,ulevel,num, row_number()  OVER (PARTITION BY uid ORDER BY num desc) as rn '
+#                'from (select uid,ulevel,count(1) as num from userlevel group by uid, ulevel) t ')
 
 
 # ±£´æ
-hiveContext.registerDataFrameAsTable(result,'sals')
+hiveContext.registerDataFrameAsTable(df,'sals')
 hiveContext.sql('create table wlbase_dev.t_zlj_perfer_user_levelqq as select * from sals')
 
 # sqlContext.sql('select uid,ulevel,count(1) as num from userlevel group by uid, ulevel').take(1)
