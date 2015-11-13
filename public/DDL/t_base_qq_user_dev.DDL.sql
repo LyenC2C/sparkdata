80542cd370770f99b10956a0b971a4cf016f4d27
zhanglj <zhanglinjiang@wolongdata.com>
2015-11-13 14:37:45 +0800

    标签数据整理

--- public/DDL/t_base_qq_user_dev.DDL.sql
+++ public/DDL/t_base_qq_user_dev.DDL.sql
@@ -1,32 +1,32 @@
 use wlbase_dev;
 
 CREATE EXTERNAL TABLE  if not exists t_base_qq_user_dev (
-uin  STRING   COMMENT 'qq��' ,
+uin  STRING   COMMENT 'qq号' ,
 birthday STRING COMMENT 'yyyy-mm-dd',
-phone  STRING COMMENT '�ֻ��ţ��Լ���д�����ݿ���ʧ��' ,
-gender_id  BIGINT COMMENT '�Ա� 1����  2��Ů' ,
-college  STRING   COMMENT 'ѧУ' ,
-lnick  STRING COMMENT '����ǩ��' ,
-loc_id STRING COMMENT '���ڵر���' ,
-loc STRING COMMENT '���ڵ�',
-h_loc_id STRING COMMENT '�������',
-h_loc STRING COMMENT '�����',
-personal  STRING   COMMENT '����˵��' ,
-shengxiao  STRING COMMENT '��Ф' ,
-gender  BIGINT COMMENT '�Ա�  1����  2��Ů' ,
-occupation  STRING  COMMENT 'ְλ' ,
-constel  STRING COMMENT '����' ,
-blood  STRING COMMENT 'Ѫ��' ,
-url  STRING   COMMENT 'ͷ��' ,
-homepage  STRING COMMENT '��ҳ' ,
-nick  STRING COMMENT '�ǳ�' ,
+phone  STRING COMMENT '手机号，自己填写，内容可能失真' ,
+gender_id  BIGINT COMMENT '性别 1：男  2：女' ,
+college  STRING   COMMENT '学校' ,
+lnick  STRING COMMENT '个人签名' ,
+loc_id STRING COMMENT '所在地编码' ,
+loc STRING COMMENT '所在地',
+h_loc_id STRING COMMENT '家乡编码',
+h_loc STRING COMMENT '家乡地',
+personal  STRING   COMMENT '个人说明' ,
+shengxiao  STRING COMMENT '生肖' ,
+gender  BIGINT COMMENT '性别  1：男  2：女' ,
+occupation  STRING  COMMENT '职位' ,
+constel  STRING COMMENT '星座' ,
+blood  STRING COMMENT '血型' ,
+url  STRING   COMMENT '头像' ,
+homepage  STRING COMMENT '主页' ,
+nick  STRING COMMENT '昵称' ,
 email  STRING  COMMENT 'email' ,
-uin2  STRING   COMMENT '��½�����˺�' ,
-mobile  STRING COMMENT '�еĻ����ּ���139********',
-ts STRING COMMENT '�ɼ�ʱ���',
+uin2  STRING   COMMENT '登陆邮箱账号' ,
+mobile  STRING COMMENT '有的话部分加密139********',
+ts STRING COMMENT '采集时间戳',
 age  INT
 )
-COMMENT 'qq�û���Ϣ'
+COMMENT 'qq用户信息'
 PARTITIONED BY  (ds STRING )
 ROW FORMAT DELIMITED FIELDS TERMINATED BY '\001'   LINES TERMINATED BY '\n' ;
 
