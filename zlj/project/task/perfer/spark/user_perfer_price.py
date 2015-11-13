d9537e3da6a52bed435d143f88f07e0694e71791
zhanglj <zhanglinjiang@wolongdata.com>
2015-11-13 16:44:33 +0800

    标签数据整理

--- zlj/project/task/perfer/spark/user_perfer_price.py
+++ zlj/project/task/perfer/spark/user_perfer_price.py
@@ -22,16 +22,8 @@ from pyspark.sql import HiveContext
 hiveContext = HiveContext(sc)
 hiveContext.sql('use wlbase_dev')
 
-
-# hiveContext.sql('select * from t_zlj_ec_userbuy limit 10').collect()
-# rdd=hiveContext.sql('select cast(price as int) price from wlbase_dev.t_base_ec_item_dev where ds=20151030 and cast(price as int)>0').map(lambda x:x.price)
-# rdd1=hiveContext.sql('select user_id,avg(price) avg_price from   t_zlj_ec_userbuy  where user_id rlike   "^\\\\d+$" group by user_id    HAVING  avg(price)>0 ').map(lambda x:[x.user_id,x.avg_price])
-
 rdd1=hiveContext.sql('select user_id,avg_price from t_zlj_ec_perfer_priceavg limit 10000 ').map(lambda x:[x.user_id,x.avg_price])
 rdd=rdd1.map(lambda x: x[1]).repartition(100)
-
-# rdd.filter(lambda  x:x<50000).histogram(1000)
-
 
 data=rdd.filter(lambda x:x<30000).map(lambda x:array(x))
 model = KMeans.train( data, 5, maxIterations=20, runs=30, initializationMode="random",seed=50, initializationSteps=5, epsilon=1e-4)
@@ -40,14 +32,11 @@ model = KMeans.train( data, 5, maxIterations=20, runs=30, initializationMode="ra
 model.centers=sorted(model.centers)
 
 
-
 # [array([ 947.35742005]), array([ 278.74380204]), array([ 2558.86612172]), array([ 41.31785555]), array([ 6119.56633409])]
 
 # rdd2=sqlContext.sql('select user_id,price from wlbase_dev.t_zlj_ec_userbuy_1 where price>0')
-userlevel_rdd=rdd1.map(lambda  x: (x[0],x[1],model.predict(x[1])))
+userlevel_rdd=rdd1.map(lambda  x: (x[0],x[1],model.predict([x[1]])))
 
-
-# rdd2.map(lambda  x: (x.user_id,x.price))
 schema = StructType([
            StructField("uid", StringType(), True),
            StructField("avg_price", IntegerType(), True),
