f8fd0e860a1eb2058188e027d55b22ceb0f17d29
zhanglj <zhanglinjiang@wolongdata.com>
2015-11-13 16:50:02 +0800

    标签数据整理

--- zlj/project/task/perfer/spark/user_perfer_price.py
+++ zlj/project/task/perfer/spark/user_perfer_price.py
@@ -22,19 +22,14 @@ from pyspark.sql import HiveContext
 hiveContext = HiveContext(sc)
 hiveContext.sql('use wlbase_dev')
 
-rdd1=hiveContext.sql('select user_id,avg_price from t_zlj_ec_perfer_priceavg limit 10000 ').map(lambda x:[x.user_id,x.avg_price])
+rdd1=hiveContext.sql('select user_id,avg_price from t_zlj_ec_perfer_priceavg ').map(lambda x:[x.user_id,x.avg_price])
 rdd=rdd1.map(lambda x: x[1]).repartition(100)
 
 data=rdd.filter(lambda x:x<30000).map(lambda x:array(x))
-model = KMeans.train( data, 5, maxIterations=20, runs=30, initializationMode="random",seed=50, initializationSteps=5, epsilon=1e-4)
-
+model = KMeans.train( data, 5, maxIterations=20, runs=50, initializationMode="random",seed=50, initializationSteps=5, epsilon=1e-4)
 
 model.centers=sorted(model.centers)
 
-
-# [array([ 947.35742005]), array([ 278.74380204]), array([ 2558.86612172]), array([ 41.31785555]), array([ 6119.56633409])]
-
-# rdd2=sqlContext.sql('select user_id,price from wlbase_dev.t_zlj_ec_userbuy_1 where price>0')
 userlevel_rdd=rdd1.map(lambda  x: (x[0],x[1],model.predict([x[1]])))
 
 schema = StructType([
@@ -43,21 +38,8 @@ schema = StructType([
            StructField("ulevel",IntegerType(), True)])
 df=sqlContext.createDataFrame(userlevel_rdd,schema)
 sqlContext.registerDataFrameAsTable(df,'userlevel')
-# df.cache()
-
-# hiveContext.sql('use wlbase_dev')
-#
-#
-# result=sqlContext.sql('select  uid,ulevel,num, row_number()  OVER (PARTITION BY uid ORDER BY num desc) as rn '
-#                'from (select uid,ulevel,count(1) as num from userlevel group by uid, ulevel) t ')
-
 
 # 保存
 hiveContext.registerDataFrameAsTable(df,'sals')
 hiveContext.sql('drop table if EXISTS t_zlj_perfer_user_level ')
 hiveContext.sql('create table wlbase_dev.t_zlj_perfer_user_level as select * from sals')
-
-# sqlContext.sql('select uid,ulevel,count(1) as num from userlevel group by uid, ulevel').take(1)
-
-# pricerdd=hiveContext.sql('select * from t_zlj_ec_userbuy_1 where price<0')
-# pricerdd=hiveContext.sql('select * from t_base_ec_shop_dev where ds=20151023 limit 10')
