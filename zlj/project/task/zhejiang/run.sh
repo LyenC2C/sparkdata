9d811c215a29eb0be0959f8fc984f1507ea4e668
zhanglj <zhanglinjiang@wolongdata.com>
2015-11-17 19:53:01 +0800

    增量表修改

--- zlj/project/task/zhejiang/run.sh
+++ zlj/project/task/zhejiang/run.sh
@@ -7,3 +7,10 @@ sh everyday_sold.sql  20151102 20151104
 sh everyday_sold.sql  20151104 20151105
 sh everyday_sold.sql  20151105 20151106
 sh everyday_sold.sql  20151106 20151107
+sh everyday_sold.sql  20151107 20151108
+sh everyday_sold.sql  20151108 20151109
+sh everyday_sold.sql  20151109 20151110
+sh everyday_sold.sql  20151110 20151111
+sh everyday_sold.sql  20151111 20151112
+sh everyday_sold.sql  20151112 20151113
+sh everyday_sold.sql  20151113 20151115
