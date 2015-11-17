f19dab3a4c474ce9a2016b29c7c3aeff63593584
pengzhongzheng <pengzhongzheng@wolongdata.com>
2015-11-17 09:26:19 +0800

    merge hive path file

--- pzz/sh/merge_hive_file.sh
+++ pzz/sh/merge_hive_file.sh
@@ -8,11 +8,12 @@ source ~/.bashrc
 
 
 path=$1
-
+local_tmp_path=/mnt/pzz/hdfs_merge_tmp
 #hadoop fs -ls $path
 
 echo $path
 
+merge_day="`date +%Y%m%d`"
 
 
 hadoop fs  -ls $path  |sed  '1d' |awk -F"ds="  '{if(length($NF)==8) print "ds="$NF }' >ds_log
@@ -25,17 +26,17 @@ echo 'deal file merger'
 while read line
 do
     echo  "hadoop fs -cat $path/$line/* > part-00000"
-    hadoop fs -cat $path/$line/* > part-00000
+    hadoop fs -cat $path/$line/* > ${local_tmp_path}/${merge_day}-0000
 
     echo "hadoop fs -rm $path/$line/part-*"
     hadoop fs -rm $path/$line/*
 
     echo  "hadoop fs -put part-00000  $path/$line/"
-    hadoop fs -put part-00000  $path/$line/
-    rm part-00000
+    hadoop fs -put ${local_tmp_path}/${merge_day}-0000  $path/$line/
+    rm ${local_tmp_path}/${merge_day}-000
 done<ds_log
 
 while read line
 do
-    hadoop fs -rm $path/$line/
+    hadoop fs -rm $path/$line/*
 done<ds_log_error
