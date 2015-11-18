6381772855102094424c556c65d8c861134b9b50
zhanglj <zhanglinjiang@wolongdata.com>
2015-11-18 16:08:37 +0800

    tfidf

--- public/sh/sale.sh
+++ public/sh/sale.sh
@@ -1,2 +1,5 @@
+pre_path='/home/wrt/sparkdata'
+
+
 spark-submit  --executor-memory 4G  --driver-memory 20G  --total-executor-cores 80 t_wrt_base_ec_item_sale.py
-sh everyday_sold.sql  20151113 20151115
+sh $pre_path/zlj/project/task/zhejiang/everyday_sold.sql   20151113 20151115
