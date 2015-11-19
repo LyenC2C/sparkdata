19d1e831df6529b413fb559634dac490c5e76cb1
Gbacillus <gs464gan@gmail.com>
2015-11-19 18:53:26 +0800

    update item_sale.py

--- wrt/data_base_process/sh/quchong.sh
+++ wrt/data_base_process/sh/quchong.sh
@@ -1,9 +1,8 @@
 pre_path='/home/wrt/wrt/sparkdata'
-sh $pre_path/wrt/data_base_process/hive/t_wrt_quchong.sql 20151110
-#sh $pre_path/wrt/data_base_process/hive/t_wrt_quchong.sql 20151111
-#sh $pre_path/wrt/data_base_process/hive/t_wrt_quchong.sql 20151112
-#sh $pre_path/wrt/data_base_process/hive/t_wrt_quchong.sql 20151113
-#sh $pre_path/wrt/data_base_process/hive/t_wrt_quchong.sql 20151114
-#sh $pre_path/wrt/data_base_process/hive/t_wrt_quchong.sql 20151115
-#sh $pre_path/wrt/data_base_process/hive/t_wrt_quchong.sql 20151116
-#sh $pre_path/wrt/data_base_process/hive/t_wrt_quchong.sql 20151117
+sh $pre_path/wrt/data_base_process/hive/t_wrt_quchong.sql 20151111
+sh $pre_path/wrt/data_base_process/hive/t_wrt_quchong.sql 20151112
+sh $pre_path/wrt/data_base_process/hive/t_wrt_quchong.sql 20151113
+sh $pre_path/wrt/data_base_process/hive/t_wrt_quchong.sql 20151114
+sh $pre_path/wrt/data_base_process/hive/t_wrt_quchong.sql 20151115
+sh $pre_path/wrt/data_base_process/hive/t_wrt_quchong.sql 20151116
+sh $pre_path/wrt/data_base_process/hive/t_wrt_quchong.sql 20151117
