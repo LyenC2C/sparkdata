a1d212a07c8ea6a184df8728184ac1a6ea038c0d
Gbacillus <gs464gan@gmail.com>
2015-11-20 19:03:35 +0800

    update item_sale.py

--- wrt/data_base_process/total_sale_tianbu.py
+++ wrt/data_base_process/total_sale_tianbu.py
@@ -47,7 +47,8 @@ schema = StructType([
 	])
 
 hiveContext.sql('use testhive')
-s1 = "/hive/warehouse/wlbase_dev.db/t_base_ec_item_sale_dev/ds=" + sys.argv[1] #today
+ds = sys.argv[1]
+s1 = "/hive/warehouse/wlbase_dev.db/t_base_ec_item_sale_dev/ds=" + ds #today
 s2 = "/hive/warehouse/wlbase_dev.db/t_base_ec_item_sale_dev/ds=" + sys.argv[2] #yesterday
 rdd1 = sc.textFile(s1).map(lambda x:f(x)).filter(lambda x:x!=None).map(lambda x:(x[0],x[1:]))
 rdd2 = sc.textFile(s2).map(lambda x:f(x)).filter(lambda x:x!=None).map(lambda x:(x[0],x[1:]))
@@ -58,7 +59,7 @@ hiveContext.registerDataFrameAsTable(df, 'data')
 #ds2 = s[st:st+4] + s[st+5:st+7] + s[st+8:st+10]
 #l = len(s1)
 #ds1 = s1[l-8:]
-hiveContext.sql('insert overwrite table t_base_ec_item_sale_dev PARTITION(ds=' + s1 + ') select * from data')
+hiveContext.sql('insert overwrite table t_base_ec_item_sale_dev PARTITION(ds=' + ds + ') select * from data')
 		#.saveAsTextFile("/user/wrt/item_sale")
 sc.stop()
 #spark-submit  --executor-memory 4G  --driver-memory 20G  --total-executor-cores 80 t_wrt_base_ec_item_sale.py
