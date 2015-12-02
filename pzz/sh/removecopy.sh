5f7c784d1a9accec4e4b56d43ce5cee51d03a263
crawler173 <hadoop@crawler173>
2015-12-02 10:07:20 +0800

    结果　sort

--- pzz/sh/removecopy.sh
+++ pzz/sh/removecopy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#useage 
+#sh removecopy.sh hdfs://192.168.4.200:9400/xxx/xxx
 dirname=$1
 echo 'pre pare to clean the copy in '$dirname
 
@@ -53,8 +55,11 @@ awk '{
         }
      }'|
 
+#sort them
+sort |
+
 #list they
-xargs -I {} hadoop fs -ls {}
+#xargs -I {} hadoop fs -ls {}
 
 #or remove they
 #xargs -I {} hadoop fs -rm {}
