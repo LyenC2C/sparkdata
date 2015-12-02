96110f18a3b3161cba6b5c606f7dd1d2cfa8c687
tink <tink@192.168.2.111>
2015-12-02 12:41:12 +0800

    fix bug hadoop fs -ls  会输出完整路径

--- pzz/sh/merge_small_hive_file.sh
+++ pzz/sh/merge_small_hive_file.sh
@@ -23,18 +23,17 @@ hadoop fs -ls $path  |sed  '1d' |awk -F"ds="  '{if(length($NF)!=8) print "ds="$N
 echo 'deal file merger'
 
 while read line
-do
-    echo  "hadoop fs -cat $path/$line/* > ${merge_day}-0000"
+do    
     #256*1024*1024 268435456
-    #grep except the floder
     #cat
-    hadoop fs -ls $path/$line/ |grep -v '^d'|sed '1d'|awk '{if($5<268435456){print $NF}}'|xargs -I {} hadoop fs -cat $path/$line/{} > ${local_tmp_path}/${merge_day}-0000
-    echo "hadoop fs -rm $path/$line/part-*"
-
-    hadoop fs -ls $path/$line/ |grep -v '^d'|sed '1d'|awk '{if($5<268435456){print $NF}}'|xargs -I {} hadoop fs -rm $path/$line/{} 
-    echo  "hadoop fs -put ${merge_day}-0000 $path/$line/"
+    hadoop fs -ls $path/$line/ |grep -v '^d'|sed '1d'|awk '{if($5<268435456){print $NF}}'|xargs -t -I {} hadoop fs -cat {} > ${local_tmp_path}/${merge_day}-0000
+    #rm
+    hadoop fs -ls $path/$line/ |grep -v '^d'|sed '1d'|awk '{if($5<268435456){print $NF}}'|xargs -t -I {} hadoop fs -rm {} 
+    #put            
     hadoop fs -put ${local_tmp_path}/${merge_day}-0000  $path/$line/
+
     rm ${local_tmp_path}/${merge_day}-0000
+    
 done<ds_log
 
 while read line
