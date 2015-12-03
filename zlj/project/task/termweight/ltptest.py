1a86e69f4b613b544218d0793209b64a98b40dfd
zhanglj <zhanglinjiang@wolongdata.com>
2015-12-03 10:45:31 +0800

    hanlp seg

---
+++ zlj/project/task/termweight/ltptest.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+__author__ = 'zlj'
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+from pyspark import SparkContext
+from pyspark.sql import *
+
+sc = SparkContext(appName="cmt")
+sqlContext = SQLContext(sc)
+hiveContext = HiveContext(sc)
+
+root='/home/hadoop/common/segfile/tlpdata'
+from pyltp import Segmentor
+segmentor = Segmentor()
+segmentor.load(root+'cws.model')
+
+
+sc.textFile('/user/zlj/temp/100line_title').map(lambda x: ' '.join(segmentor.segment(x))).saveAsTextFile('/user/zlj/temp/100line_title_ltp')
