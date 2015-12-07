d8fbea7df03c75c5898153ac72784ac96063db0e
pengzhongzheng <pengzhongzheng@wolongdata.com>
2015-12-07 19:14:34 +0800

    about pzz

--- pzz/sh/cmt_inc_insert.sh
+++ pzz/sh/cmt_inc_insert.sh
@@ -33,7 +33,7 @@ echo "Start spark job."
 hadoop fs -rmr $all_feed_output
 hadoop fs -rmr $new_feed_output
 hadoop fs -rmr $tmp_data
-spark-submit --executor-memory 16g --driver-memory 25g --total-executor-cores 160 /mnt/pzz/workspace/sparkdata/pzz/cmt/cmt_inc_clean.py -gen_data_inc ${all_feed_input}/part* $new_data_input $all_feed_output $new_feed_output $tmp_data
+spark-submit --executor-memory 14g --driver-memory 25g --total-executor-cores 140 /mnt/pzz/workspace/sparkdata/pzz/cmt/cmt_inc_clean.py -gen_data_inc ${all_feed_input}/part* $new_data_input $all_feed_output $new_feed_output $tmp_data
 echo "spark job finished."
 
 #本地临时文件
