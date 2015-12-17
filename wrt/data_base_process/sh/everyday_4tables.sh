62f340eff7ad9b9ca5e3e84bdeb136b1d5795ef0
Gbacillus <gs464gan@gmail.com>
2015-12-17 11:45:27 +0800

    update item_sale.py

--- wrt/data_base_process/sh/everyday_4tables.sh
+++ wrt/data_base_process/sh/everyday_4tables.sh
@@ -6,9 +6,6 @@ pre_path='/home/wrt/sparkdata'
 zuotian='20151211'
 qiantian='20151210'
 
-spark-submit  --executor-memory 8G  --driver-memory 10G  --total-executor-cores 80 \
-$pre_path/wrt/data_base_process/t_wrt_base_ec_item_sale.py $zuotian $qiantian
-
 spark-submit  --total-executor-cores  80   --executor-memory  8g  --driver-memory 10g \
 $pre_path/zlj/project/base_data_process/hive/item/1_item_inc.py  -inc \
 /commit/iteminfo/$zuotian/*  $qiantian $zuotian
@@ -16,5 +13,8 @@ $pre_path/zlj/project/base_data_process/hive/item/1_item_inc.py  -inc \
 spark-submit  --total-executor-cores  80  --executor-memory 8g  --driver-memory 10g \
 $pre_path/zlj/project/base_data_process/hive/shop/1_shop_inc.py  -inc \
 /commit/iteminfo/$zuotian/*  $qiantian $zuotian
+
+spark-submit  --executor-memory 8G  --driver-memory 10G  --total-executor-cores 80 \
+$pre_path/wrt/data_base_process/t_wrt_base_ec_item_sale.py $zuotian $qiantian
 
 sh $pre_path/zlj/project/task/zhejiang/everyday_sold.sql $qiantian $zuotian $zuotian $zuotian
