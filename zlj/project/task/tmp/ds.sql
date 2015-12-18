4ef60e503b45fcd4f391f97e842a73f5b9050284
zhanglj <zhanglinjiang@wolongdata.com>
2015-12-18 10:59:20 +0800

    类目整理tag 处理

--- zlj/project/task/tmp/ds.sql
+++ zlj/project/task/tmp/ds.sql
@@ -1,32 +1,23 @@
-  CREATE TABLE t_zlj_tmp_t_base_ec_item_title_cut_with_brand_tf
-    AS
-select user_id,word ,count(1) from
-      (
-        SELECT
-          user_id,
-          word
-        FROM
-          (SELECT
-             user_id,
-             hmm
-           FROM
-             (
-               SELECT
-                 item_id,
-                 concat_ws(' ', title_cut, concat(cat_name, '_c'), concat(brand_name, '_b')) AS hmm
-               FROM t_base_ec_item_title_cut_with_brand
-             ) t1
-             JOIN
-             (
-               SELECT
-                 item_id,
-                 user_id
-               FROM t_base_ec_item_feed_dev_tmp
-             ) t2
-               ON t1.item_id = t2.item_id
-          ) t
-        lateral VIEW explode(split(hmm, ' ')) myTable AS word
-      )t1
-   WHERE  length(word)>1
-
-      GROUP BY user_id, word
+#  统计类目
+SELECT
+  cate_level1_name,
+  root_cat_id,
+  num
+FROM
+  (SELECT
+     cate_level1_id,
+     cate_level1_name
+   FROM t_base_ec_dim
+   GROUP BY cate_level1_id, cate_level1_name)
+  t1
+  JOIN
+  (
+    SELECT
+      root_cat_id,
+      count(1) AS num
+    FROM
+      t_zlj_userbuy_cat_count
+    GROUP BY root_cat_id
+  ) t2
+    ON
+      t1.cate_level1_id = t2.root_cat_id ;
