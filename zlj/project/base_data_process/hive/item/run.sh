c80b9ed89f9b351045ee872831a5382bc9bc8cb4
zhanglj <zhanglinjiang@wolongdata.com>
2015-12-20 14:51:54 +0800

    item_inc  优化

--- zlj/project/base_data_process/hive/item/run.sh
+++ zlj/project/base_data_process/hive/item/run.sh
@@ -1,6 +1,12 @@
-sh step.sh  /commit/iteminfo/tmall.shop.2.item.2015-10-27.iteminfo.2015-11-01  
-sh step.sh  /commit/iteminfo/tmall.shop.2.item.2015-10-27.iteminfo.2015-11-02
-20151101  20151102 w
-sh step.sh  /commit/iteminfo/tmall.shop.2.item.2015-10-27.iteminfo.2015-11-01  
-sh step.sh  /commit/iteminfo/tmall.shop.2.item.2015-10-27.iteminfo.2015-11-01  
-sh step.sh  /commit/iteminfo/tmall.shop.2.item.2015-10-27.iteminfo.2015-11-01  
+#sh step.sh  /commit/iteminfo/tmall.shop.2.item.2015-10-27.iteminfo.2015-11-01
+#sh step.sh  /commit/iteminfo/tmall.shop.2.item.2015-10-27.iteminfo.2015-11-02
+#20151101  20151102 w
+#sh step.sh  /commit/iteminfo/tmall.shop.2.item.2015-10-27.iteminfo.2015-11-01
+#sh step.sh  /commit/iteminfo/tmall.shop.2.item.2015-10-27.iteminfo.2015-11-01
+#sh step.sh  /commit/iteminfo/tmall.shop.2.item.2015-10-27.iteminfo.2015-11-01
+
+hadoop fs -rm -r /user/zlj/data/temp/t_base_ec_item_dev_tmp
+
+spark-submit  --total-executor-cores  120   --executor-memory  10g  --driver-memory 10g opt.py  -inc /commit/iteminfo/20151213/*  20151212 20151213
+
+sh 1_item_inc.sql 20151213
