c3237f6051f6b7abf6e46aac7592f84fca78bd65
Gbacillus <gs464gan@gmail.com>
2015-12-21 09:35:41 +0800

    update item_sale.py

--- wrt/data_base_process/sh/quchong.sh
+++ wrt/data_base_process/sh/quchong.sh
@@ -1,12 +1,26 @@
 pre_path='/home/sparkdata'
 
-spark-submit  --total-executor-cores  80  --executor-memory 8g  --driver-memory 8g \
+spark-submit  --total-executor-cores  80  --executor-memory 8g  --driver-memory 10g \
+$pre_path/zlj/project/base_data_process/hive/shop/1_shop_inc.py  -inc \
+/commit/iteminfo/20151218/*  20151217 20151218
+
+spark-submit  --total-executor-cores  80  --executor-memory 8g  --driver-memory 10g \
 $pre_path/zlj/project/base_data_process/hive/shop/1_shop_inc.py  -inc \
 /commit/iteminfo/20151219/*  20151218 20151219
 
-spark-submit  --total-executor-cores  80  --executor-memory 8g  --driver-memory 8g \
+spark-submit  --total-executor-cores  80  --executor-memory 8g  --driver-memory 10g \
 $pre_path/zlj/project/base_data_process/hive/shop/1_shop_inc.py  -inc \
 /commit/iteminfo/20151220/*  20151219 20151220
+
+spark-submit  --executor-memory 8G  --driver-memory 10G  --total-executor-cores 80 \
+$pre_path/wrt/data_base_process/t_wrt_base_ec_item_sale.py 20151218 20151217
+
+spark-submit  --executor-memory 8G  --driver-memory 10G  --total-executor-cores 80 \
+$pre_path/wrt/data_base_process/t_wrt_base_ec_item_sale.py 20151219 20151218
+
+spark-submit  --executor-memory 8G  --driver-memory 10G  --total-executor-cores 80 \
+$pre_path/wrt/data_base_process/t_wrt_base_ec_item_sale.py 20151220 20151219
+
 
 #spark-submit  --executor-memory 20G  --driver-memory 10G  --total-executor-cores 200 \
 #$pre_path/wrt/data_base_process/t_wrt_base_ec_item_sale.py 20151212 20151211
