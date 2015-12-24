addd6950cc2e6da17bea184020968f66f5702b9a
Gbacillus <gs464gan@gmail.com>
2015-12-24 18:13:01 +0800

    update item_sale.py

--- wrt/data_base_process/sh/log3.sh
+++ wrt/data_base_process/sh/log3.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 pre_path='/home/wrt/sparkdata'
 
-spark-submit  --total-executor-cores  80   --executor-memory  8g  --driver-memory 10g \
-$pre_path/zlj/project/base_data_process/hive/item/1_item_inc.py  -inc \
-/commit/iteminfo/20151213/*  20151212 20151213
+hadoop fs -rm -r /user/zlj/data/temp/t_base_ec_item_dev_tmp
+spark-submit  --total-executor-cores  120   --executor-memory  12g  --driver-memory 10g \
+$pre_path/zlj/project/base_data_process/hive/item/1_item_inc_opt.py  -inc /commit/iteminfo/20151220/*  20151219  20151220
+sh $pre_path/zlj/project/base_data_process/hive/item/1_item_inc.sql 20151220
+
+#spark-submit  --total-executor-cores  80   --executor-memory  8g  --driver-memory 10g \
+#$pre_path/zlj/project/base_data_process/hive/item/1_item_inc.py  -inc \
+#/commit/iteminfo/20151213/*  20151212 20151213
 
 #spark-submit  --total-executor-cores  80   --executor-memory  8g  --driver-memory 10g \
 #$pre_path/zlj/project/base_data_process/hive/item/1_item_inc.py  -inc \
