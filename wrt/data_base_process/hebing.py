422b8f9b53bcfd4384016a87f8e7021f623ca343
Gbacillus <gs464gan@gmail.com>
2015-12-25 11:06:02 +0800

    update item_sale.py

--- wrt/data_base_process/hebing.py
+++ wrt/data_base_process/hebing.py
@@ -6,9 +6,9 @@ ds = sys.argv[1]
 sc = SparkContext(appName="hebing item_sale_" + ds)
 
 
-s = "/hive/warehouse/wlbase_dev.db/t_base_ec_item_sale_dev" + ds
+s = "/hive/warehouse/wlbase_dev.db/t_base_ec_item_sale_dev/" + ds
 rdd = sc.textFile(s).coalesce(200)
-rdd.saveAsTextFile("/hive/warehouse/testhive.db/t_base_ec_item_sale_dev"+ds)
+rdd.saveAsTextFile("/hive/warehouse/testhive.db/t_base_ec_item_sale_dev/" + ds)
 
 # spark-submit  --total-executor-cores  80   --executor-memory  8g  --driver-memory 10g \
 # hebing.py 20151221
