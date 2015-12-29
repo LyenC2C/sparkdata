74c2e768845c7d3093d0c4742b78c8625216c681
zhanglj <zhanglinjiang@wolongdata.com>
2015-12-29 17:22:53 +0800

    term weight  重构

--- zlj/project/task/termweight/0_run.sh
+++ zlj/project/task/termweight/0_run.sh
@@ -6,3 +6,5 @@ hadoop fs -rmr /user/zlj/tmp/data/ds
 
 spark-commit   3_clean_fliter_corpus.py
 
+sh  4_group_corpus.sql
+spark-submit  --total-executor-cores  120   --executor-memory  12g  --driver-memory 12g  tfidf_new.py  -new  1  40  20151226  t_zlj_userbuy_item_tfidf_tagbrand_weight_2015_v2
