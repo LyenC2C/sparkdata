720a17cab8b15727b66f7953d0c171fe5c51f459
zhanglj <zhanglinjiang@wolongdata.com>
2015-12-30 10:39:10 +0800

    term weight  重构

--- zlj/project/task/termweight/1_feed_item_join_corpus.sql
+++ zlj/project/task/termweight/1_feed_item_join_corpus.sql
@@ -1,4 +1,12 @@
 
+
+/home/hadoop/hive/bin/hive<<EOF
+
+
+use wlbase_dev;
+
+drop table if EXISTS  t_zlj_item_feed_title_cut_20151226;
+
 create table t_zlj_item_feed_title_cut_20151226 as
 select user_id,title_cut
 from
@@ -13,6 +21,8 @@ group by item_id,user_id
 t2
 on t1.item_id=t2.item_id ;
 
+
+EOF
 
 -- create table t_zlj_tmp as  select * from t_base_ec_item_title_cut where ds=20151225 limit 5
 -- ;
