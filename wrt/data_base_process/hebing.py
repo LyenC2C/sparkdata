640611b3b59d22a67cd3f2ecfd643e06bd71688d
Gbacillus <gs464gan@gmail.com>
2015-12-31 09:32:25 +0800

    update item_sale.py

--- wrt/data_base_process/hebing.py
+++ wrt/data_base_process/hebing.py
@@ -6,9 +6,9 @@ from pyspark import SparkContext
 sc = SparkContext(appName="hebing feed")
 
 
-s = "/hive/warehouse/wlbase_dev.db/t_wrt_tmp_20151230" #+ ds
+s = "/hive/warehouse/wlbase_dev.db/t_wrt_tmp_20151230/*" #+ ds
 rdd = sc.textFile(s).coalesce(16)
-rdd.saveAsTextFile("/user/wrt/feed_2015")
+rdd.saveAsTextFile("/user/wrt/feed_2015/")
 
 # spark-submit  --total-executor-cores  80   --executor-memory  8g  --driver-memory 10g \
 # hebing.py 20151221
