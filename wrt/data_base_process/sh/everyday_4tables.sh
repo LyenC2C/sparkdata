7a3b6b16ed92c8d301d9906132248faab1c951b9
Gbacillus <gs464gan@gmail.com>
2016-01-08 09:37:50 +0800

    update item_sale.py

--- wrt/data_base_process/sh/everyday_4tables.sh
+++ wrt/data_base_process/sh/everyday_4tables.sh
@@ -3,7 +3,7 @@ source ~/.bashrc
 pre_path='/home/wrt/sparkdata'
 #zuotian=$(date -d '1 days ago' +%Y%m%d)
 #qiantian=$(date -d '2 days ago' +%Y%m%d)
-zuotian='20160101'
+zuotian='20160104'
 qiantian='20151231'
 
 spark-submit  --total-executor-cores  120  --executor-memory 12g  --driver-memory 20g \
@@ -16,9 +16,9 @@ $pre_path/zlj/project/base_data_process/hive/item/1_item_inc_opt.py  -inc /commi
 >> ./log_date/log_$zuotian 2>&1
 sh $pre_path/zlj/project/base_data_process/hive/item/1_item_inc.sql $zuotian >> ./log_date/log_$zuotian 2>&1
 
-hadoop fs -rm -r /user/wrt/sale_tmp >> ./log_date/log_$zuotian 2>&1
-spark-submit  --executor-memory 12G  --driver-memory 20G  --total-executor-cores 120 \
-$pre_path/wrt/data_base_process/t_wrt_base_ec_item_sale.py $qiantian $zuotian >> ./log_date/log_$zuotian 2>&1
-sh $pre_path/wrt/data_base_process/t_wrt_base_ec_item_sale.sql $zuotian >> ./log_date/log_$zuotian 2>&1
-
-sh $pre_path/zlj/project/task/zhejiang/everyday_sold.sql $qiantian $zuotian $zuotian $zuotian >> ./log_date/log_$zuotian 2>&1
+#hadoop fs -rm -r /user/wrt/sale_tmp >> ./log_date/log_$zuotian 2>&1
+#spark-submit  --executor-memory 12G  --driver-memory 20G  --total-executor-cores 120 \
+#$pre_path/wrt/data_base_process/t_wrt_base_ec_item_sale.py $qiantian $zuotian >> ./log_date/log_$zuotian 2>&1
+#sh $pre_path/wrt/data_base_process/t_wrt_base_ec_item_sale.sql $zuotian >> ./log_date/log_$zuotian 2>&1
+#
+#sh $pre_path/zlj/project/task/zhejiang/everyday_sold.sql $qiantian $zuotian $zuotian $zuotian >> ./log_date/log_$zuotian 2>&1
