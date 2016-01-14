bbb52ce67936003fb0c37366f23921f51af0e83e
zhanglj <zhanglinjiang@wolongdata.com>
2016-01-14 11:01:17 +0800

    perfer

--- zlj/project/task/perfer/hive/5.2_perfer_price.sh
+++ zlj/project/task/perfer/hive/5.2_perfer_price.sh
@@ -4,4 +4,4 @@
 
 sh  5.1_perfer_price.sql
 
-spark-submit   --total-executor-cores  80  --executor-memory 10G  --driver-memory 10g   ../spakr/user_perfer_price_mult.py
+spark-submit   --total-executor-cores  80  --executor-memory 10G  --driver-memory 10g   ../spark/user_perfer_price_mult.py
