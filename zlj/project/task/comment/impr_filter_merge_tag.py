de61ae47840d811c91b278fc4316c764320e9b93
zhanglj <zhanglinjiang@wolongdata.com>
2016-01-26 18:45:29 +0800

    评价rule

--- zlj/project/task/comment/impr_filter_merge_tag.py
+++ zlj/project/task/comment/impr_filter_merge_tag.py
@@ -375,7 +375,10 @@ def pos_neg(words):
     if neg==0 and neg_emo>0: flag= -1
     if neg==0 and pos_emo==0 and neg_emo==0 :flag= 0
     neg_word=""
-    if neg>0:neg_word=list((neg_emo_set&words_set))[0]
+    if neg>0:
+        try:
+            neg_word=list((neg_emo_set&words_set))[0]
+        except:pass
     return flag,'_'.join(str(i) for i in [neg*degree,neg_emo,pos_emo]),neg_word
 
 
