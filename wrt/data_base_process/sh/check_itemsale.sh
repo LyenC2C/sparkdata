54795c9c224565b0f6911aa72858966dbeceeffc
Gbacillus <gs464gan@gmail.com>
2016-02-18 14:30:39 +0800

    update item_sale.py

--- wrt/data_base_process/sh/check_itemsale.sh
+++ wrt/data_base_process/sh/check_itemsale.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 source ~/.bashrc
 pre_path='/home/wrt/sparkdata'
-today='20151124'
-hadoop fs -rm -r /user/wrt/itemsale/ds=$today &> $pre_path/wrt/data_base_process/sh/check_log/log_$today
-spark-submit  --total-executor-cores  40  --executor-memory 4g  --driver-memory 8g \
-$pre_path/wrt/data_base_process/itemsale_check.py $today >> $pre_path/wrt/data_base_process/sh/check_log/log_$today 2>&1
-today='20151125'
-hadoop fs -rm -r /user/wrt/itemsale/ds=$today &> $pre_path/wrt/data_base_process/sh/check_log/log_$today
-spark-submit  --total-executor-cores  40  --executor-memory 4g  --driver-memory 8g \
-$pre_path/wrt/data_base_process/itemsale_check.py $today >> $pre_path/wrt/data_base_process/sh/check_log/log_$today 2>&1
 today='20151126'
 hadoop fs -rm -r /user/wrt/itemsale/ds=$today &> $pre_path/wrt/data_base_process/sh/check_log/log_$today
 spark-submit  --total-executor-cores  40  --executor-memory 4g  --driver-memory 8g \
