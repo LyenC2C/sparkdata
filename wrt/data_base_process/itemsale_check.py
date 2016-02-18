2f5d9a369de7379f1bf5be839b39f49f45df5a28
Gbacillus <gs464gan@gmail.com>
2016-02-18 10:56:07 +0800

    update item_sale.py

--- wrt/data_base_process/itemsale_check.py
+++ wrt/data_base_process/itemsale_check.py
@@ -7,5 +7,5 @@ s1 = "/commit/itemsale/ds=" + today
 rdd = sc.textFile(s1)
 rdd2 = rdd.filter(lambda x:len(x.split('\001')) == 11)
 rdd2.coalesce(100).saveAsTextFile('/user/wrt/itemsale/ds=' + today)
-fout = open("log/result_"+today,'w')
+fout = open("sh/check_log/result_"+today,'w')
 fout.write(str(rdd.count() - rdd2.count()))
