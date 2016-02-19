adb8853681ac3cb7fac2d11949db2a23f8f87e85
pengzhongzheng <pengzhongzheng@wolongdata.com>
2016-02-19 17:09:56 +0800

    about pzz

--- pzz/sh/feed.Dynamic_partitions.sql
+++ pzz/sh/feed.Dynamic_partitions.sql
@@ -7,7 +7,7 @@ path=$1
 
 SET hive.exec.dynamic.partition=true;
 SET hive.exec.dynamic.partition.mode=nonstrict;
-SET hive.exec.max.dynamic.partitions.pernode = 1000;
+SET hive.exec.max.dynamic.partitions.pernode = 2000;
 SET hive.exec.max.dynamic.partitions=2000;
 
 set hive.exec.reducers.bytes.per.reducer=500000000;
