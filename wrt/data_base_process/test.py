5b6237e6c6cceb2a074a9b215d32e8bd1502c3fe
Gbacillus <gs464gan@gmail.com>
2016-02-19 17:40:09 +0800

    update item_sale.py

--- wrt/data_base_process/test.py
+++ wrt/data_base_process/test.py
@@ -3,7 +3,7 @@ import sys
 import rapidjson as json
 from pyspark import SparkContext
 sc = SparkContext(appName="title_price_picurl")
-s1 = "/commit/iteminfoorg/item.info.crawler171.2016-01-04"
+s1 = "/commit/iteminfoorg"
 def valid_jsontxt(content):
     if type(content) == type(u""):
         return content.encode("utf-8")
