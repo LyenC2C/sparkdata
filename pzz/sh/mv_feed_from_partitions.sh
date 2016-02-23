fcb683c04402f0e95c4d5188e11a342c6a0b2a61
pengzhongzheng <pengzhongzheng@wolongdata.com>
2016-02-23 15:30:03 +0800

    about pzz

--- pzz/sh/mv_feed_from_partitions.sh
+++ pzz/sh/mv_feed_from_partitions.sh
@@ -1,5 +1,5 @@
 #source /home/yarn/.bashrc
 input_dir=$1
-hadoop fs -ls $1 |awk -F '/'  '{if(length($NF)==8) print $NF }' |awk '{if($1 ~/^[0-9]+$/) print $1" "'${input_dir}'}' | less
+hadoop fs -ls $1 |awk -F '/'  '{if(length($NF)==8) print $NF }' |awk '{if($1 ~/^[0-9]+$/) print $1" '${input_dir}'\"}' | less
 #hadoop fs -ls $1 |awk -F '/'  '{if(length($NF)==8) print $NF }' |awk '{if($1 ~/^[0-9]+$/) print $1\t${input_dir}}'  | xargs -n2 -P 20 -i sh mv_step.sh {}
 #echo $cmd
