569784908526d65f2630ce7fdca795b5232c7bde
pengzhongzheng <pengzhongzheng@wolongdata.com>
2016-02-23 15:40:39 +0800

    about pzz

--- pzz/sh/mv_feed_from_partitions.sh
+++ pzz/sh/mv_feed_from_partitions.sh
@@ -1,5 +1,6 @@
 #source /home/yarn/.bashrc
 input_dir=$1
-hadoop fs -ls $1 |awk -F '/'  '{if(length($NF)==8) print $NF }' |awk '{if($1 ~/^[0-9]+$/) print $1" '${input_dir}'"}' | xargs -n2 -P 20 -i sh mv_step.sh {}
+table=$2
+hadoop fs -ls $1 |awk -F '/'  '{if(length($NF)==8) print $NF }' |awk '{if($1 ~/^[0-9]+$/) print $1" '${input_dir}' '${table}'"}' | xargs -n3 -P 20 sh mv_step.sh
 #hadoop fs -ls $1 |awk -F '/'  '{if(length($NF)==8) print $NF }' |awk '{if($1 ~/^[0-9]+$/) print $1\t${input_dir}}'  | xargs -n2 -P 20 -i sh mv_step.sh {}
 #echo $cmd
--- pzz/sh/mv_step.sh
+++ pzz/sh/mv_step.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 inputdir=$1
 filedate=$2
-echo $1"\t"$2
-#hadoop fs -mv ${inputdir}/$2  /hive/warehouse/wlbase_dev.db/t_base_ec_item_feed_dev_test/ds=$2/$2_`date +%s`
+table=$3
+hadoop fs -mv ${inputdir}/$filedate  /hive/warehouse/wlbase_dev.db/$table/ds=$filedate/$filedate_`date +%s`
 #echo "-cp ${inputdir}/${1}_`date +%s`  /hive/warehouse/wlbase_dev.db/t_base_ec_item_feed_dev_test/ds=$1/"
