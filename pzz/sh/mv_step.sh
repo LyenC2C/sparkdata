61c0e97ff62ced3e4c0ed530ba61cbafee7474fc
pengzhongzheng <pengzhongzheng@wolongdata.com>
2016-02-23 15:33:04 +0800

    about pzz

--- pzz/sh/mv_step.sh
+++ pzz/sh/mv_step.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 inputdir=$1
-hadoop fs -mv ${inputdir}/$2  /hive/warehouse/wlbase_dev.db/t_base_ec_item_feed_dev_test/ds=$2/$2_`date +%s`
+filedate=$2
+echo $1"\t"$2
+#hadoop fs -mv ${inputdir}/$2  /hive/warehouse/wlbase_dev.db/t_base_ec_item_feed_dev_test/ds=$2/$2_`date +%s`
 #echo "-cp ${inputdir}/${1}_`date +%s`  /hive/warehouse/wlbase_dev.db/t_base_ec_item_feed_dev_test/ds=$1/"
