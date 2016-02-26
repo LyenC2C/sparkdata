eca7f678e6e4c3318d7e1ec0fd69d6bbacdadc13
Gbacillus <gs464gan@gmail.com>
2016-02-26 16:10:38 +0800

    update item_sale.py

--- wrt/data_base_process/DDL/t_wrt_feed_tongji.sql
+++ wrt/data_base_process/DDL/t_wrt_feed_tongji.sql
@@ -1,11 +1,9 @@
 use testhive;
-CREATE  TABLE  if not exists t_wrt_feed_tongji(
+CREATE  TABLE  if not exists t_wrt_feed_tongji3(
 item_id   String ,
 feed_id   String ,
 user_id String  ,
-content String ,
-clauses String
+content String
 )
-PARTITIONED BY  (ds STRING )
-ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'   LINES TERMINATED BY '\n'
+ROW FORMAT DELIMITED FIELDS TERMINATED BY '\001'   LINES TERMINATED BY '\n'
 stored as textfile ;
