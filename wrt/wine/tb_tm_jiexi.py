4a72a6d781e1027695cb0cb4e19c4bfb8958fe65
Gbacillus <gs464gan@gmail.com>
2016-03-09 11:15:14 +0800

    d

--- wrt/wine/tb_tm_jiexi.py
+++ wrt/wine/tb_tm_jiexi.py
@@ -18,13 +18,15 @@ def f(line):
     props = ob.get("props")
     if type(props) != type([]): return None
     categoryId = valid_jsontxt(ob.get("itemInfoModel",{}).get("categoryId","-"))
-    if categoryId != "50008144" and categoryId != "50013052": return None
+    # if categoryId != "50008144" and categoryId != "50013052": return None
     for ln in props:
-        if valid_jsontxt("香型") in valid_jsontxt(ln["name"]):
+        # if valid_jsontxt("香型") in valid_jsontxt(ln["name"]):
+        #     return None
+        if valid_jsontxt("度数") == valid_jsontxt(ln["name"]):
             return None
     return line
 
 rdd = sc.textFile("/user/zlj/temp/zlj_wine.iteminfo.2016-03-07").map(lambda x:f(x)).filter(lambda x:x!=None)
-rdd.saveAsTextFile('/user/zlj/temp/wrt_wine_tb_tm_wuxiangxing')
+rdd.saveAsTextFile('/user/zlj/temp/wrt_wine_tb_tm_wudushu')
 
 # spark-submit  --executor-memory 4G  --driver-memory 4G  --total-executor-cores 40 tb_tm_jiexi.py
