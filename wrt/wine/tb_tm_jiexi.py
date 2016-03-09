502629ac932907bd3b000dfb280419353d4cb52e
Gbacillus <gs464gan@gmail.com>
2016-03-09 10:56:19 +0800

    d

--- wrt/wine/tb_tm_jiexi.py
+++ wrt/wine/tb_tm_jiexi.py
@@ -27,4 +27,4 @@ def f(line):
 rdd = sc.textFile("/user/zlj/temp/zlj_wine.iteminfo.2016-03-07").map(lambda x:f(x)).filter(lambda x:x!=None)
 rdd.saveAsTextFile('/user/zlj/temp/wrt_wine_tb_tm_wuxiangxing')
 
-# spark-submit  --executor-memory 8G  --driver-memory 10G  --total-executor-cores 80 tongkuan.py
+# spark-submit  --executor-memory 4G  --driver-memory 4G  --total-executor-cores 40 tb_tm_jiexi.py
