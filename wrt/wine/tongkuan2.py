f6e646ac9be3d32a344491d923d4f4af0d7af43b
Gbacillus <gs464gan@gmail.com>
2016-03-21 17:55:47 +0800

    d

--- wrt/wine/tongkuan2.py
+++ wrt/wine/tongkuan2.py
@@ -96,6 +96,6 @@ def f2(x,y):
 
 rdd = sc.textFile("/user/zlj/wine/tb_wine_title_groupby_cut_sonbrand").map(lambda x:f1(x)).filter(lambda x:x!=None)
 rdd2 = rdd.groupByKey().mapValues(list).flatMap(lambda (x,y):f2(x,y))
-rdd2.saveAsTextFile('/user/zlj/temp/wine_wuliangye_sonbrand')
+rdd2.saveAsTextFile('/user/zlj/temp/wine_wuliangye_sonbrand_dushu')
 
 #spark-submit  --executor-memory 8G  --driver-memory 10G  --total-executor-cores 80 tongkuan.py
