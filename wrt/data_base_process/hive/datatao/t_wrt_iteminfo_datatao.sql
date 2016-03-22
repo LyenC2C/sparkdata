8438e1a90d61d9388e1715d5366eda281b8dc1f5
Gbacillus <gs464gan@gmail.com>
2016-03-22 10:36:58 +0800

    d

---
+++ wrt/data_base_process/hive/datatao/t_wrt_iteminfo_datatao.sql
@@ -0,0 +1,16 @@
+create table t_wrt_iteminfo_datatao(
+item_id STRING,
+title STRING,
+cat_name STRING,
+root_cat_name STRING,
+price STRING,
+price_zone STRING,
+favor STRING,
+brand_name STRING,
+shop_id STRING,
+location STRING
+);
+
+insert overwrite table t_wrt_iteminfo_datatao
+select item_id,title,cat_name,root_cat_name,price,price_zone,favor,brand_name,shop_id,location from
+t_base_ec_item_dev where ds = 20160225 and (cat_id =50025435 or cat_id = 50830004) and bc_type = 'B' limit 1000;
