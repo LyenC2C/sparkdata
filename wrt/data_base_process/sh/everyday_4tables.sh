e767a54be6bea69987bc2116d6657b8d115d4f9c
Gbacillus <gs464gan@gmail.com>
2016-04-01 10:49:11 +0800

    d

--- wrt/data_base_process/sh/everyday_4tables.sh
+++ wrt/data_base_process/sh/everyday_4tables.sh
@@ -3,13 +3,12 @@ source ~/.bashrc
 pre_path='/home/wrt/sparkdata'
 #zuotian=$(date -d '1 days ago' +%Y%m%d)
 #qiantian=$(date -d '2 days ago' +%Y%m%d)
-
-#
-#spark-submit  --total-executor-cores  80  --executor-memory 8g  --driver-memory 10g \
-#$pre_path/zlj/project/base_data_process/hive/shop/1_shop_inc.py  -inc \
-#/commit/iteminfo/$zuotian/*  $qiantian $zuotian >> ./log_date/log_$zuotian 2>&1
 zuotian='20160318'
 qiantian='20160225'
+
+spark-submit  --total-executor-cores  80  --executor-memory 8g  --driver-memory 10g \
+$pre_path/zlj/project/base_data_process/hive/shop/1_shop_inc.py  -inc \
+/commit/iteminfo/$zuotian/*  $qiantian $zuotian >> ./log_date/log_$zuotian 2>&1
 
 hadoop fs -rm -r /user/wrt/t_base_ec_item_dev_tmp >> ./log_date/log_$zuotian 2>&1
 spark-submit  --total-executor-cores  80   --executor-memory  8g  --driver-memory 10g \
