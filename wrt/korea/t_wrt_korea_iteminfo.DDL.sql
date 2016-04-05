7f5463e4bc954c52d9c795144a5579a4c7ba2c51
Gbacillus <gs464gan@gmail.com>
2016-04-05 15:29:43 +0800

    d

---
+++ wrt/korea/t_wrt_korea_iteminfo.DDL.sql
@@ -0,0 +1,20 @@
+CREATE EXTERNAL TABLE  if not exists t_wrt_korea_iteminfo(
+    item_id String,
+    title String,
+    categoryId String,
+    cate_name String,
+    cate_root_name String,
+    brandId String,
+    brand_name String,
+    price String,
+    price_zone String,
+    favor String,
+    seller_id String,
+    shopId String,
+    country String,
+    location String,
+    ts String
+)
+COMMENT ''
+ROW FORMAT DELIMITED FIELDS TERMINATED BY '\001'   LINES TERMINATED BY '\n'
+stored as textfile ;
