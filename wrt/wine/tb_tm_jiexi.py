43b84ecba4fdce991ee8d386bbc9b423073b1003
Gbacillus <gs464gan@gmail.com>
2016-04-06 09:56:08 +0800

    d

--- wrt/wine/tb_tm_jiexi.py
+++ wrt/wine/tb_tm_jiexi.py
@@ -30,6 +30,9 @@ def f(line,cate_dict):
     # cate_rootid = cate_dict.get(categoryId,["-","-"])[1]
     # if cate_rootid != "50008141": return None
     if categoryId != "50008144" and categoryId != "50013052": return None
+    trackParams = ob.get('trackParams',{})
+    BC_type = trackParams.get('BC_type','-')
+    if BC_type != 'B': return None
     # for ln in props:
     #     # if valid_jsontxt("香型") in valid_jsontxt(ln["name"]):
     #     #     return None
