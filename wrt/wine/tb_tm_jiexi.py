e34ffc4adf9084bf8ac6b698cb81ae88630a44b7
Gbacillus <gs464gan@gmail.com>
2016-04-06 09:51:05 +0800

    d

--- wrt/wine/tb_tm_jiexi.py
+++ wrt/wine/tb_tm_jiexi.py
@@ -27,9 +27,9 @@ def f(line,cate_dict):
     if itemInfoModel == "-": return None
     # categoryId = valid_jsontxt(ob.get("itemInfoModel",{}).get("categoryId","-"))
     categoryId = itemInfoModel.get('categoryId','-')
-    cate_rootid = cate_dict.get(categoryId,["-","-"])[1]
-    if cate_rootid != "50008141": return None
-    # if categoryId != "50008144" and categoryId != "50013052": return None
+    # cate_rootid = cate_dict.get(categoryId,["-","-"])[1]
+    # if cate_rootid != "50008141": return None
+    if categoryId != "50008144" and categoryId != "50013052": return None
     # for ln in props:
     #     # if valid_jsontxt("香型") in valid_jsontxt(ln["name"]):
     #     #     return None
