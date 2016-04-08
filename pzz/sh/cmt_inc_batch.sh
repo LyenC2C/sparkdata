3c911b1f46c1ef8f7f9cead8becc58375a576e36
pengzhongzheng <pengzhongzheng@wolongdata.com>
2016-04-08 13:31:54 +0800

    about pzz

--- pzz/sh/cmt_inc_batch.sh
+++ pzz/sh/cmt_inc_batch.sh
@@ -4,7 +4,7 @@ source /home/yarn/.bashrc
 #sh cmt_inc_updated.sh 20160325 20160324
 #sh cmt_inc_updated.sh 20160326 20160325
 #sh cmt_inc_updated.sh 20160327 20160326
-sh cmt_inc_updated.sh 20160328 20160326
+#sh cmt_inc_updated.sh 20160328 20160326
 sh cmt_inc_updated.sh 20160329 20160328
 sh cmt_inc_updated.sh 20160330 20160329
 sh cmt_inc_updated.sh 20160331 20160330
