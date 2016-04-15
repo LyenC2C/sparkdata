6b1020ab73e60457c734f8ee87874c494c6937ee
Gbacillus <gs464gan@gmail.com>
2016-04-15 10:42:21 +0800

    d

--- wrt/wine/tongkuan3.py
+++ wrt/wine/tongkuan3.py
@@ -54,16 +54,22 @@ def f1(line):
     # if ss[1].strip() == "": return None
     item_id = ss[0]
     brand = ss[1]
+    son_bra = ss[2]
+
     title = ss[3].split("\t")#[1:]
     words = []
     # values = []
+    dushu = '-'
     for ln in title:
         # if len(ln.split("_")) != 2: return None
         word = uniform(ln) #所有字母变小写
         # value = float(ln.split("_")[1]) #匹配权值
+        if title[i].encode('utf-8') == '度' and i != 0:
+            if title[i-1].isdigit():
+                dushu = title[i-1]
         words.append(word)
         # values.append(value)
-    return (brand,[item_id,words])
+    return (brand,[item_id,words,son_bra,dushu])
 
 def f2(x,y):
     brand_list = y
@@ -73,11 +79,21 @@ def f2(x,y):
         for j in range(i+1,l):
             k1 = brand_list[i]
             k2 = brand_list[j]
-            pipei_value = pipei(k1[1],k2[1]) #([words,values],[words,values])
+            item_id1 = k1[0]
+            item_id2 = k2[0]
+            son_bra1 = k1[2]
+            son_bra2 = k1[2]
+            if (dushu1 == dushu2 or (dushu1 == '-' or dushu2 == '-')) and\
+                    (son_bra1 == son_bra2 or (son_bra1 == '-' or son_bra2 == '-')):
+                pipei_value = pipei(k1[1],k2[1])
+            else:
+                pipei_value = 0.0
+            # pipei_value = pipei(k1[1],k2[1]) #([words,values],[words,values])
             title1 = "".join(k1[1])
             title2 = "".join(k2[1])
-            if float(pipei_value) > 0.9:
-                result.append(x + "\t" + title1 + "\t" + title2 + "\t" + str(pipei_value))
+
+            if float(pipei_value) > 0.8:
+                result.append(item_id1 + "\t" + title1 + "\t" + item_id2 + "\t" + title2 + "\t" + str(pipei_value))
     return result
 
 
@@ -85,4 +101,4 @@ rdd = sc.textFile("/user/zlj/wine/hive-warehouse-wlservice wuliangy_cut_sonbrand
 rdd2 = rdd.groupByKey().mapValues(list).flatMap(lambda (x,y):f2(x,y))
 rdd2.saveAsTextFile('/user/wrt/temp/lzlj_pipei_result_09')
 
-#spark-submit  --executor-memory 8G  --driver-memory 10G  --total-executor-cores 80 tongkuan.py
+#spark-submit  --executor-memory 8G  --driver-memory 10G  --total-executor-cores 80 tongkuan3.py
