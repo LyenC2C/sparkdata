121831c7b945dec713541bc781872aeac95a6bae
Gbacillus <gs464gan@gmail.com>
2016-04-21 15:47:14 +0800

    d

--- wrt/pzz/id_title_price_url.py
+++ wrt/pzz/id_title_price_url.py
@@ -21,7 +21,7 @@ def parse_price(price_dic):
         if '-' in tmp:     v=tmp.split('-')[0]
         else :             v=tmp
         if v.isdigit():
-            v = float()
+            v = float(v)
         else:
             v = 0.0
         if min>v:
