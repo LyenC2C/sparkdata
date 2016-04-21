833a5814ee1a69cf0b71213663148bf4488a094f
Gbacillus <gs464gan@gmail.com>
2016-04-21 14:21:24 +0800

    d

--- wrt/data_base_process/t_base_item_info.py
+++ wrt/data_base_process/t_base_item_info.py
@@ -113,7 +113,8 @@ def f(line,cate_dict):
     result.append(item_info)
     result.append(sku_info)
     result.append(ts)
-    return (item_id,result)
+    # return (item_id,result)
+    return result
 
 def quchong(x, y):
     max = 0
@@ -133,8 +134,8 @@ def quchong(x, y):
 s = "/hive/warehouse/wlbase_dev.db/t_base_ec_item_house/part*"
 s_dim = "/hive/warehouse/wlbase_dev.db/t_base_ec_dim/ds=20151023/1073988839"
 cate_dict = sc.broadcast(sc.textFile(s_dim).map(lambda x: get_cate_dict(x)).filter(lambda x:x!=None).collectAsMap()).value
-rdd_c = sc.textFile(s).map(lambda x: f(x,cate_dict)).filter(lambda x:x!=None)
-rdd = rdd_c.groupByKey().mapValues(list).map(lambda (x, y): quchong(x, y))
+rdd = sc.textFile(s).map(lambda x: f(x,cate_dict)).filter(lambda x:x!=None)
+# rdd = rdd_c.groupByKey().mapValues(list).map(lambda (x, y): quchong(x, y))
 rdd.saveAsTextFile('/user/wrt/temp/iteminfo_test')
 
 
