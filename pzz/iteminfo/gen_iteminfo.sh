183de48e22b815cb940d963011149da36e8cba0f
pengzhongzheng <pengzhongzheng@wolongdata.com>
2016-04-25 17:58:52 +0800

    about pzz

--- pzz/iteminfo/gen_iteminfo.sh
+++ pzz/iteminfo/gen_iteminfo.sh
@@ -5,5 +5,5 @@ source ~/.bashrc
 workspace_path=/home/yarn/pzz/workspace/sparkdata/pzz
 echo "\targv[1]:-genbase, 形成基础仓库.  argv[2]:inputpath   argv[3]:outputpath\n
     \targv[1]:-geninc, 每日新增数据.  argv[2]:input data path   argv[3]:input base path  argv[4]:outputpath"
-spark_midlle $workspace_path/xzx/gen_iteminfo.py $1 $2 $3 $4
+spark_middle $workspace_path/xzx/gen_iteminfo.py $1 $2 $3 $4
 
