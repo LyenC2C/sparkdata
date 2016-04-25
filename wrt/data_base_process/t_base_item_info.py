463d0fc91cb53df86d087608861016d83fd5d91b
Gbacillus <gs464gan@gmail.com>
2016-04-25 16:53:53 +0800

    d

--- wrt/data_base_process/t_base_item_info.py
+++ wrt/data_base_process/t_base_item_info.py
@@ -142,7 +142,7 @@ s_dim = "/hive/warehouse/wlbase_dev.db/t_base_ec_dim/ds=20151023/1073988839"
 cate_dict = sc.broadcast(sc.textFile(s_dim).map(lambda x: get_cate_dict(x)).filter(lambda x:x!=None).collectAsMap()).value
 rdd = sc.textFile(s).map(lambda x: f(x,cate_dict)).filter(lambda x:x!=None)
 # rdd = rdd_c.groupByKey().mapValues(list).map(lambda (x, y): quchong(x, y))
-rdd.saveAsTextFile('/user/wrt/temp/iteminfo_test')
+rdd.saveAsTextFile('/user/wrt/temp/iteminfo_tmp')
 
 
 # spark-submit  --executor-memory 8G  --driver-memory 8G  --total-executor-cores 80 t_base_item_info.py
