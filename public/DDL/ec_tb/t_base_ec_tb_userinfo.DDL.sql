5c682cfccc002e29aba1b098165f2f44886a2d74
Gbacillus <gs464gan@gmail.com>
2016-05-03 14:36:57 +0800

    d

---
+++ public/DDL/ec_tb/t_base_ec_tb_userinfo.DDL.sql
@@ -0,0 +1,11 @@
+CREATE EXTERNAL TABLE  if not exists t_base_ec_item_dev (
+uid string comment 'userid',
+alipay string comment '是否支付宝实名 1为是，0为否',
+buycnt string comment '购买记录',
+verify string comment '淘宝等级',
+regtime string comment '注册时间',
+location string comment '用户地点'
+)
+COMMENT '电商淘宝用户信息表'
+PARTITIONED BY  (ds STRING )
+ROW FORMAT DELIMITED FIELDS TERMINATED BY '\001'   LINES TERMINATED BY '\n' ;
