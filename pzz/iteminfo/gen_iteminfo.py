95f6b53ff7418910e92b753bb5d0e18b9ce46e3c
pengzhongzheng <pengzhongzheng@wolongdata.com>
2016-05-03 14:42:17 +0800

    about pzz

--- pzz/iteminfo/gen_iteminfo.py
+++ pzz/iteminfo/gen_iteminfo.py
@@ -158,6 +158,7 @@ if __name__ == "__main__":
                 .groupByKey()\
                 .map(lambda (x,y):gen_item_inc(x,y))\
                 .map(lambda x:"\001".join(x))\
+                .coalesce(2600)\
                 .saveAsTextFile(output_path)
 
         sc.stop()
