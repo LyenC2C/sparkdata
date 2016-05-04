378fe784fe08d1716b201e6b83cb7166e07f61aa
Gbacillus <gs464gan@gmail.com>
2016-05-04 15:52:30 +0800

    d

--- wrt/data_base_process/t_base_item_sale.py
+++ wrt/data_base_process/t_base_item_sale.py
@@ -29,9 +29,11 @@ def f1(line):
     if len(ss) != 2: return [None]
     ts = ss[0]
     zhengwen = ss[1]
-    if zhengwen == "<!DOCTYPE html>": return [None]
+    # if zhengwen == "<!DOCTYPE html>": return [None]
     # l = len(zhengwen)
-    star = zhengwen.find("({") + 1
+    star = zhengwen.find("({")# + 1
+    if star == -1: return [None]
+    else: star += 1
     end = zhengwen.rfind("})") + 1
     # end = l-2
     text = zhengwen[star:end]
@@ -164,5 +166,26 @@ rdd = rdd1.union(rdd2).groupByKey().mapValues(list).map(lambda (x, y): quchong_2
 rdd_final = rdd.union(rdd3).groupByKey().mapValues(list).map(lambda (x, y):quchong_3(x, y)).coalesce(200)
 rdd_final.saveAsTextFile('/user/wrt/sale_tmp')
 
-# spark-submit  --executor-memory 9G  --driver-memory 10G  --total-executor-cores 120 t_base_item_sale.py 20160424 20160425 20160424
-#LOAD DATA  INPATH '/user/wrt/sale_tmp' OVERWRITE INTO TABLE t_base_ec_item_sold_dev PARTITION (ds='20160424');
+"""
+hfs -rmr /user/wrt/sale_tmp
+spark-submit  --executor-memory 9G  --driver-memory 10G  --total-executor-cores 120 t_base_item_sale.py 20160426 20160427 20160424
+LOAD DATA  INPATH '/user/wrt/sale_tmp' OVERWRITE INTO TABLE t_base_ec_item_sold_dev PARTITION (ds='20160424');
+insert into table t_base_ec_item_sold_dev PARTITION (ds='20160426')
+select
+t2.item_id,
+t2.price,
+t2.amount,
+t2.total,
+t2.qu,
+t2.st,
+t2.insale,
+t2.start,
+t2.cp_flag,
+t2.ts
+from
+(select * from t_base_ec_item_sold_dev where ds = 20160425)t1
+join
+(select * from wlservice.t_wrt_sold_tmp where ds = 20160426)t2
+on
+t1.item_id = t2.item_id
+"""
