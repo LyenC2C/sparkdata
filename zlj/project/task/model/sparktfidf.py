a871146b9565b6ef21e4d63d38380339a30cd888
zhanglj <zhanglinjiang@wolongdata.com>
2016-05-23 18:37:04 +0800

    评价修改

--- zlj/project/task/model/sparktfidf.py
+++ zlj/project/task/model/sparktfidf.py
@@ -26,7 +26,7 @@ path = '/commit/project/wxtitle/wxtitle_cut/'
 
 # doc = sc.textFile(path).map(lambda line: line.split('\001')).map(lambda x: (x[0], x[1].split() + [x[0] + '_doc']))
 
-docs = sc.textFile(path).map(lambda x:x.split()).repartition(10)
+docs = sc.textFile(path).map(lambda x:x.split()).repartition(100)
 
 words=docs.flatMap(lambda x:x).distinct().collect()
 
@@ -49,7 +49,7 @@ tf=docs.map(lambda x:count([word_index_bc[i] for i in x]))
 tf.cache()
 idf = IDF(minDocFreq=1).fit(tf)
 tfidf = idf.transform(tf)
-tfidf.repartition(10).map(lambda x:'\t'.join(['_'.join([index_word_bc[i],str(w)]) for  i ,w in zip(x.indices,x.values)])).\
+tfidf.repartition(20).map(lambda x:'\t'.join(['_'.join([index_word_bc[i],str(w)]) for  i ,w in zip(x.indices,x.values)])).\
     filter(lambda x:len(x)>2).saveAsTextFile('/commit/project/wxtitle/wxtitle_cut_tfidf')
 
 
