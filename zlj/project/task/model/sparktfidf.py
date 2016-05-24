f5dc1a343423897038acb0dbacf03474afbf22c4
zhanglj <zhanglinjiang@wolongdata.com>
2016-05-24 09:55:24 +0800

    评价修改

--- zlj/project/task/model/sparktfidf.py
+++ zlj/project/task/model/sparktfidf.py
@@ -22,13 +22,15 @@ sc = SparkContext(appName="cmt")
 sqlContext = SQLContext(sc)
 hiveContext = HiveContext(sc)
 
-path = '/user/zlj/nlp/t_zlj_item_title/'
+path = '/user/zlj/nlp/t_zlj_item_title/part-00000'
 
 # doc = sc.textFile(path).map(lambda line: line.split('\001')).map(lambda x: (x[0], x[1].split() + [x[0] + '_doc']))
 
-docs = sc.textFile(path).map(lambda x:x.split('\001')).map(lambda x:x[1].split()+[x[0]+"_doc"]).repartition(100)
+docs= sc.textFile(path).map(lambda x:x.split('\001')).map(lambda x:([x[0]],x[1].split())).repartition(100)
 
-words=docs.flatMap(lambda x:x).distinct().collect()
+# docs=docs_index.map(lambda x:x[0][1].split())
+
+words=docs.map(lambda x:x[1]).flatMap(lambda x:x).distinct().collect()
 
 words_bc=sc.broadcast(words).value
 word_index={}
@@ -41,11 +43,10 @@ word_index_bc=sc.broadcast(word_index).value
 index_word_bc=sc.broadcast(index_word).value
 
 size=len(words_bc)
-def count(v):
-    id=size
+def count(v,doc_id):
     dict= sorted(Counter(v).iteritems(), key=lambda d:d[0])
-    return Vectors.sparse(id,[i[0] for i in dict ],[i[1] for i in dict ])
-tf=docs.map(lambda x:count([word_index_bc[i] for i in x]))
+    return Vectors.sparse(size,[i[0] for i in dict ]+[int(doc_id)],[i[1] for i in dict ]+[1])
+tf=docs.map(lambda x:count([word_index_bc[i] for i in x[1]],x[0]))
 tf.cache()
 idf = IDF(minDocFreq=1).fit(tf)
 tfidf = idf.transform(tf)
