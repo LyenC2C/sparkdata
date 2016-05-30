f540f67c60b1e7116314bd86c3a95fa98c433764
Gbacillus <gs464gan@gmail.com>
2016-05-30 16:39:06 +0800

    d

--- wrt/tb_userinfo/tb_userinfo.py
+++ wrt/tb_userinfo/tb_userinfo.py
@@ -17,20 +17,20 @@ def get_p_dict(line):
     return (ss[0],ss[1])
 
 def map_line(line,p_dict):
-    try:
-        j = json.loads(valid_jsontxt(line.strip()))
-        #{"alipay": "已通过支付宝实名认证", "uid": "12871760", "buycnt": "1520", "verify": "VIP等级5", "regtime": "2005.03.10", "nick": "happle_1999", "location": "北京"}
-        alipay = '0' if j['alipay'] == None else '1'
-        uid = j['uid']
-        buycnt = j["buycnt"]
-        verify = j['verify']
-        regtime = j['regtime']
-        nick = j['nick']
-        location = j['location']
-        location = p_dict.get(location,"") + "\t" + location
-        return '\001'.join([uid,alipay,buycnt,verify,regtime,nick,location])
-    except:
-        pass
+    # try:
+    j = json.loads(valid_jsontxt(line.strip()))
+    #{"alipay": "已通过支付宝实名认证", "uid": "12871760", "buycnt": "1520", "verify": "VIP等级5", "regtime": "2005.03.10", "nick": "happle_1999", "location": "北京"}
+    alipay = '0' if j['alipay'] == None else '1'
+    uid = j['uid']
+    buycnt = j["buycnt"]
+    verify = j['verify']
+    regtime = j['regtime']
+    nick = j['nick']
+    location = j['location']
+    location = p_dict.get(location,"") + "\t" + location
+    return '\001'.join([uid,alipay,buycnt,verify,regtime,nick,location])
+    # except:
+    #     pass
 
 # def quchong(line):
 
@@ -40,7 +40,7 @@ s = "/commit/taobao/userinfo/tbuid." + sys.argv[1]
 s_p = '/user/wrt/city_pro'
 p_dict = sc.broadcast(sc.textFile(s_p).map(lambda x: get_p_dict(x)).filter(lambda x:x!=None).collectAsMap()).value
 rdd = sc.textFile(s).map(lambda x:map_line(x,p_dict)).filter(lambda x:x!=None)
-# rdd_c = sc.textFile("")
+rdd_c = sc.textFile("")
 rdd.saveAsTextFile("/user/wrt/temp/tb_userinfo")
 
 # spark-submit  --executor-memory 6G  --driver-memory 8G  --total-executor-cores 80 tb_userinfo.py 20160423
