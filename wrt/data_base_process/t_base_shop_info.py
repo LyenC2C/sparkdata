7c3eed50c3b34b41df374b134a8d55d8bf8b590d
Gbacillus <gs464gan@gmail.com>
2016-06-06 09:54:33 +0800

    d

--- wrt/data_base_process/t_base_shop_info.py
+++ wrt/data_base_process/t_base_shop_info.py
@@ -94,7 +94,7 @@ def quchong(x,y):
 
 s = "/hive/warehouse/wlbase_dev.db/t_base_ec_item_house/part*"
 rdd_c = sc.textFile(s).map(lambda x: f(x)).filter(lambda x:x!=None)
-rdd = rdd_c.groupbykey().mapValues(list).map(lambda (x, y):quchong(x,y))
+rdd = rdd_c.groupByKey().mapValues(list).map(lambda (x, y):quchong(x,y))
 rdd.saveAsTextFile('/user/wrt/temp/iteminfo_tmp')
 
 #spark-submit  --executor-memory 6G  --driver-memory 8G  --total-executor-cores 80 t_base_shop_info.py
