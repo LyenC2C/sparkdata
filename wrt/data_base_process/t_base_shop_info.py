a624a103a4b753211d1db6878e4c00efa053b574
Gbacillus <gs464gan@gmail.com>
2016-06-06 09:51:27 +0800

    d

--- wrt/data_base_process/t_base_shop_info.py
+++ wrt/data_base_process/t_base_shop_info.py
@@ -28,14 +28,14 @@ def valid_jsontxt(content):
 def f(line):
     ss = line.strip().split("\001")
     ts = ss[2]
-    if (ss[5]) == "": return [None]
+    if (ss[5]) == "": return None
     text = decompress(ss[5])
     ss = text.strip().split("\t",2)
-    if len(ss) != 3: return [None]
+    if len(ss) != 3: return None
     ob = json.loads(ss[2])
     location = valid_jsontxt(itemInfoModel.get('location', '-'))
     seller = ob.get("seller",[])
-    if seller == []: return [None]
+    if seller == []: return None
     evaluateInfo = seller.get("evaluateInfo", [])
     shopId = seller.get("shopId", "-")
     seller_id = seller.get("userNumId", "-")
@@ -58,7 +58,7 @@ def f(line):
             goodRatePercentage = 0.0
     except Exception, e:
         print e, line
-        return [None]
+        return None
     weitaoId = seller.get("weitaoId", "-")
     shopTitle = seller.get("shopTitle", "--")
     desc_score = evaluateInfo[0].get("score", '0.0')
@@ -93,8 +93,8 @@ def quchong(x,y):
     return "\001".join([str(valid_jsontxt(i)) for i in result])
 
 s = "/hive/warehouse/wlbase_dev.db/t_base_ec_item_house/part*"
-rdd = sc.textFile(s).map(lambda x: f(x)).filter(lambda x:x!=None).groupbykey().mapValues(list)\
-    .map(lambda (x, y):quchong(x,y))
+rdd_c = sc.textFile(s).map(lambda x: f(x)).filter(lambda x:x!=None)
+rdd = rdd_c.groupbykey().mapValues(list).map(lambda (x, y):quchong(x,y))
 rdd.saveAsTextFile('/user/wrt/temp/iteminfo_tmp')
 
 #spark-submit  --executor-memory 6G  --driver-memory 8G  --total-executor-cores 80 t_base_shop_info.py
