370ecd39a551f34fbd83cabb03dfedfcfd5ea674
Gbacillus <gs464gan@gmail.com>
2016-06-06 15:24:37 +0800

    d

--- wrt/test.py
+++ wrt/test.py
@@ -12,6 +12,7 @@ def get_id_dict(x):
 def f(line,id_dict):
     ss = line.strip().split("\001")
     item_id = ss[0]
+    if len(ss) != 11: return None
     if not id_dict.has_key(item_id): return None
     sold = ss[6]
     flag = ss[10]
