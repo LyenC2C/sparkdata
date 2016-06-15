ef94e17cb54e1bc75a684d10240dba3c6f62e0c8
zhanglj <zhanglinjiang@wolongdata.com>
2016-06-15 11:04:11 +0800

    tag

--- zlj/project/task/perfer/spark/user_cat_tag_v2.py
+++ zlj/project/task/perfer/spark/user_cat_tag_v2.py
@@ -3652,7 +3652,7 @@ def f(x):
 # path='/hive/warehouse/wlbase_dev.db/t_zlj_t_base_ec_item_feed_dev_2015_iteminfo_t/'
 # rdd=sc.textFile(path).map(lambda x:f(x)).filter(lambda x: x is not None).flatMap(lambda x:x)
 hiveContext.sql('use wlbase_dev')
-rdd=hiveContext.sql('select user_id,root_cat_id,cate_level2_id,price  from t_base_ec_dim  t1 join  t_zlj_ec_userbuy_test t2 on t2.cat_id=t1.cate_id where cate_level2_id is not null')\
+rdd=hiveContext.sql('select user_id,root_cat_id,cate_level2_id,price  from t_base_ec_dim  t1 join  t_zlj_ec_userbuy t2 on t2.cat_id=t1.cate_id where cate_level2_id is not null')\
     .repartition(150).map(lambda x:f(x)).filter(lambda x: x is not None).flatMap(lambda x:x)
 rdd1=rdd.repartition(150).reduceByKey(lambda a,b:a+b).map(lambda (x,score):(x.split('_')[0],x.split('_')[1]+"_"+str(score)))
 
