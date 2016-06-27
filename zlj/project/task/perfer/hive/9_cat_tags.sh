ec43333914bc42a8281303ee404c226899bea70a
zhanglj <zhanglinjiang@wolongdata.com>
2016-06-27 09:11:10 +0800

    perfer 修改

--- zlj/project/task/perfer/hive/9_cat_tags.sh
+++ zlj/project/task/perfer/hive/9_cat_tags.sh
@@ -1,3 +1,3 @@
 
 
-spark-submit  --total-executor-cores  100   --executor-memory  10g  --driver-memory 10g   ../spark/user_cat_tag.py
+spark-submit  --total-executor-cores  150   --executor-memory  10g  --driver-memory 10g   ../spark/user_cat_tag_v2.py
