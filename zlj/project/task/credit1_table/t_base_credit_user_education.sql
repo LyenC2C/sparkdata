3ead35633e4c2825ce0b6106bf64beeb15e46623
Gbacillus <gs464gan@gmail.com>
2016-07-05 20:20:44 +0800

    d

---
+++ zlj/project/task/credit1_table/t_base_credit_user_education.sql
@@ -0,0 +1,7 @@
+-- 教育信息来自qq微博
+
+create table t_base_credit_user_education AS
+select id, year1, background1, school1, department1, year2, background2, school2, department2,
+year3, background3,school3, department3
+from
+t_qqweibo_user_info where year1 <> '-'
