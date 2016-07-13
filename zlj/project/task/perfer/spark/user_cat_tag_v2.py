69a5e2a6218710256b3eeec65ddccb15f8a5c2ea
zhanglj <zhanglinjiang@wolongdata.com>
2016-07-13 11:09:39 +0800

    征信数据整理

--- zlj/project/task/perfer/spark/user_cat_tag_v2.py
+++ zlj/project/task/perfer/spark/user_cat_tag_v2.py
@@ -8,9 +8,14 @@ sys.setdefaultencoding('utf8')
 from pyspark import SparkContext
 from pyspark.sql import *
 from pyspark.sql.types import *
+from pyspark import SparkConf
 # import rapidjson as json
-
-sc = SparkContext(appName="user_cattags")
+conf = SparkConf()
+conf.set("spark.kryoserializer.buffer.mb","1024")
+conf.set("spark.akka.frameSize","100")
+conf.set("spark.network.timeout","1000s")
+conf.set("spark.driver.maxResultSize","8g")
+sc = SparkContext(appName="user_cattags",conf=conf)
 sqlContext = SQLContext(sc)
 hiveContext = HiveContext(sc)
 a = {
