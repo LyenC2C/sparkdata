369b8e7b1ef242d0ddf7bbb8e32dd56b3b3bf7b1
Gbacillus <gs464gan@gmail.com>
2016-07-22 14:45:37 +0800

    d

--- public/DDL/ec_tb/t_base_ec_shopitem_dev.DDL.sql
+++ public/DDL/ec_tb/t_base_ec_shopitem_dev.DDL.sql
@@ -1,4 +1,4 @@
-CREATE EXTERNAL TABLE  if not exists t_base_ec_shopitem (
+CREATE EXTERNAL TABLE  if not exists t_base_ec_shopitem_dev (
 shop_id String COMMENT '店铺id',
 shopTitle String COMMENT '店铺名字',
 item_count String COMMENT '店铺商品总数',
@@ -18,6 +18,10 @@ up_day String COMMENT '上架日期',
 down_day String COMMENT '下架日期（未下架用0）',
 ts String COMMENT '采集时间戳'
 )
-COMMENT '电商商品用户销量状态表'
+COMMENT '店铺的商品列表'
 PARTITIONED BY  (ds STRING )
 ROW FORMAT DELIMITED FIELDS TERMINATED BY '\001' LINES TERMINATED BY '\n' ;
+
+--此表用于：
+-- 1.给采集组提供最新的item_id,其中item_id可以按照销量排序
+-- 2.给开发组提供“店铺运营”项目数据
