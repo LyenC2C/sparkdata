f13992dfab324f77936f95a6af9c872a780cd30b
zhanglj <zhanglinjiang@wolongdata.com>
2016-07-28 17:26:26 +0800

    闲鱼数据整理

--- wrt/tb_userinfo/tmp.py
+++ wrt/tb_userinfo/tmp.py
@@ -7,6 +7,6 @@ sys.setdefaultencoding('utf8')
 
 for line in open('loc.map.final'):
     id ,loc =line.split()
-    print ''.join([ i if len(i)==2 else '0'+i for i in id.split('-')[1:]])+"\' ,\'"+'-'.join(loc.split('-')[1:3])
+    print ''.join([ i if len(i)==2 else '0'+i for i in id.split('-')[1:]])+"\' :\'"+'-'.join(loc.split('-')[1:3])
 
 
