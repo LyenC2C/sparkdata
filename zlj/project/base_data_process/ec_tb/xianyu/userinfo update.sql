4e8a5e63c56015ca0d0767dbd9e7145811d44ce3
zhanglj <zhanglinjiang@wolongdata.com>
2016-07-29 14:35:29 +0800

    闲鱼数据整理

--- zlj/project/base_data_process/ec_tb/xianyu/userinfo update.sql
+++ zlj/project/base_data_process/ec_tb/xianyu/userinfo update.sql
@@ -1,5 +1,6 @@
 
 
+-- 更新用户信息
 drop table t_base_user_info_s_tbuserinfo_t_0728 ;
 
 create table t_base_user_info_s_tbuserinfo_t_0728  as
@@ -21,23 +22,15 @@ tel_prov
 
 from
 t_base_ec_tb_xianyu_userinfo t3
-  join t_base_user_info_s_tbuserinfo_t t4
-on t3.userid =t4.tb_id
+join t_base_user_info_s_tbuserinfo_t t4
+on t3.userid = t4.tb_id
 ;
 
-
-
-SELECT
-t1.* ,t2.province
-
-from t_base_ec_tb_xianyu_userinfo t1 join
-t_base_city_province_dev t2
-
-on t1.city=t2.city limit 10;
 
 
 create table t_base_ec_tb_xianyu_item_0728_dim  AS
 
 SELECT t2.* ,t1.cate_id,cate_name , cate_level1_id ,cate_level1_name
 from t_base_ec_dim  t1 join t_base_ec_tb_xianyu_item t2
-on t2.categoryid=t1.cate_id;
+on t2.categoryid=t1.cate_id
+;
