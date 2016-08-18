8e318ddf946d9ab734e144943ca469dbc48346f5
zhanglj <zhanglinjiang@wolongdata.com>
2016-08-18 11:24:54 +0800

    数据统计脚本 crontab

--- zlj/project/task/weibo/sd.py
+++ zlj/project/task/weibo/sd.py
@@ -90,8 +90,8 @@ def try_fun(line):
     try:
         return fun(line)
     except: return None
-# sc.textFile('/commit/weibo_dc').map(lambda x:try_fun(x)).filter(lambda x:x!=None).flatMap(lambda x:x).saveAsTextFile('/commit/weibo_dc_parse2015')
-sc.textFile('/user/zlj/tmp/1').map(lambda x:fun(x)).filter(lambda x:x!=None).flatMap(lambda x:x).saveAsTextFile('/user/zlj/tmp/1_parse')
+sc.textFile('/commit/weibo_dc').map(lambda x:try_fun(x)).filter(lambda x:x!=None).flatMap(lambda x:x).saveAsTextFile('/commit/weibo_dc_parse2015')
+# sc.textFile('/user/zlj/tmp/1').map(lambda x:fun(x)).filter(lambda x:x!=None).flatMap(lambda x:x).saveAsTextFile('/user/zlj/tmp/1_parse')
 
 
 # wf_2.close()
