001845f5e6ff07583008ae242d1ab62b7d111640
Gbacillus <gs464gan@gmail.com>
2016-09-08 15:55:56 +0800

    d

--- wrt/data_base_process/weibo/career_clear.py
+++ wrt/data_base_process/weibo/career_clear.py
@@ -12,8 +12,9 @@ def f(line,word_dict):
     company = ss[2]
     for word in word_dict:
         if word in company:
-            return line.strip()
-    return None
+            # return line.strip()
+            return None
+    return line.strip()
 
 s_word = '/user/wrt/career_word_1000'
 rdd = sc.textFile("/hive/warehouse/wlbase_dev.db/t_base_weibo_career/ds=20160830")
@@ -21,4 +22,5 @@ word_dict = sc.broadcast(sc.textFile(s_word).map(lambda x: (x.strip(),0)).filter
 rdd.map(lambda x:f(x, word_dict)).filter(lambda x:x!=None).saveAsTextFile('/user/wrt/temp/weibo_career_yes')
 #hfs -rmr /user/wrt/temp/weibo_career_yes
 # spark-submit  --executor-memory 6G  --driver-memory 8G  --total-executor-cores 80 career_clear.py
-
+#create table t_wrt_tmp_career_yes like wlbase_dev.t_base_weibo_career;
+#LOAD DATA  INPATH '/user/wrt/temp/weibo_career_yes' OVERWRITE INTO TABLE t_wrt_tmp_career_yes PARTITION (ds='20160908');
