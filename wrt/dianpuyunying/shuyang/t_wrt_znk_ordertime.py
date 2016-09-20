790bd13eeba0294105ddb8e8f2e5e4c822b69e9d
Gbacillus <gs464gan@gmail.com>
2016-09-20 19:24:56 +0800

    d

--- wrt/dianpuyunying/shuyang/t_wrt_znk_ordertime.py
+++ wrt/dianpuyunying/shuyang/t_wrt_znk_ordertime.py
@@ -12,10 +12,11 @@ sc = SparkContext(appName="t_znk_record")
 now_day = sys.argv[1]
 
 def valid_jsontxt(content):
-    res = content
+    # res = content
     if type(content) == type(u""):
         res = content.encode("utf-8")
-    # return res.replace("\\n", " ").replace("\n"," ").replace("\u0001"," ").replace("\001", "").replace("\\r", "")
+    else:
+        res = str(content)
     return res.replace('\n',"").replace("\r","").replace('\001',"").replace("\u0001","")
 
 def f(line):
