9162b91a3e0e8c2a07762d26236373fea4397a3d
Gbacillus <gs464gan@gmail.com>
2016-09-27 15:12:33 +0800

    d

--- wrt/dianpuyunying/shuyang/sh/znk_week.sh
+++ wrt/dianpuyunying/shuyang/sh/znk_week.sh
@@ -3,4 +3,5 @@ source ~/.bashrc
 zuotian=$(date -d '1 days ago' +%Y%m%d)
 #last_week=$(date -d '7 days ago' +%Y%m%d)
 
-sh znk_upgrad.sh $zuotian 20160919 $zuotian &> znk_log/$zuotian_log
+#sh znk_upgrad.sh $zuotian 20160919 $zuotian &> znk_log/$zuotian_log
+sh znk_upgrad.sh 20160925 20160919 20160919 &> znk_log/$zuotian_log
