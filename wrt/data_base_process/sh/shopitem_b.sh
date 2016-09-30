ddb38820dd782a36b3a4e3affe90813be2accb49
Gbacillus <gs464gan@gmail.com>
2016-09-30 11:47:42 +0800

    d

---
+++ wrt/data_base_process/sh/shopitem_b.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+source ~/.bashrc
+pre_path='/home/wrt/sparkdata'
+#zuotian=$(date -d '1 days ago' +%Y%m%d)
+#qiantian=$(date -d '2 days ago' +%Y%m%d)
+
+#zuotian='20160919'
+#qiantian='20160918'
+zuotian=$1
+qiantian=$2
+
+hfs -rmr /user/wrt/shopitem_tmp >> $pre_path/wrt/data_base_process/sh/log_shopitem/log_$zuotian 2>&1
+
+spark-submit  --executor-memory 6G  --driver-memory 5G  --total-executor-cores 80 \
+$pre_path/wrt/data_base_process/t_base_shopitem_b.py $zuotian $qiantian>> \
+$pre_path/wrt/data_base_process/sh/log_shopitem/log_$zuotian 2>&1
+
+sh $pre_path/wrt/data_base_process/hive/t_base_item_sale.sql $zuotian >> \
+$pre_path/wrt/data_base_process/sh/log_shopitem/log_$zuotian 2>&1
