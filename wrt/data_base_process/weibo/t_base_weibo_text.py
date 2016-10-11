8e1d2542153ad0c051614546ae99460394e569ea
Gbacillus <gs464gan@gmail.com>
2016-10-11 18:49:01 +0800

    d

--- wrt/data_base_process/weibo/t_base_weibo_text.py
+++ wrt/data_base_process/weibo/t_base_weibo_text.py
@@ -84,7 +84,7 @@ def quchong(x, y):
     return "\001".join(lv)
 
 
-rdd_c = sc.textFile("/commit/weibo/tmp").flatMap(lambda x:f1(x)).filter(lambda x:x != None)
+rdd_c = sc.textFile("/commit/weibo/tmp").flatMap(lambda x:f(x)).filter(lambda x:x != None)
 rdd = rdd_c.groupByKey().mapValues(list).map(lambda (x, y): quchong(x, y))
 rdd.saveAsTextFile('/user/wrt/temp/weibo_text')
 
