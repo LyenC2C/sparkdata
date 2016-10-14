b9a9993d4369e8400ccd2c61f583cdd9f8ce560c
Gbacillus <gs464gan@gmail.com>
2016-10-14 15:42:23 +0800

    d

--- wrt/data_base_process/weibo/weibo_keyword_textrank.py
+++ wrt/data_base_process/weibo/weibo_keyword_textrank.py
@@ -35,19 +35,19 @@ def f(line):
     # text = pattern4.sub('',pattern3.sub('',pattern2.sub('',pattern1.sub('',valid_jsontxt(ss[3])))))
     text = text.split("//")[0]  #去掉转发
     text = text.split("@")[0]   #将刚才未清洗干净的@去掉
-    if text.strip() == "" or text.stip() == "转发微博": return None
+    if text.strip() == "" or text.strip() == "转发微博": return None
     return (ss[1],text)
 
 def weibo_juhe(x,y):
     text_list = y
     user_weibo = "\n".join([valid_jsontxt(i) for i in text_list])
-    # res = ja.textrank(user_weibo, topK=20, withWeight=True,
-    #                          allowPOS=('an', 'i', 'j', 'l', 'n', 'nr', 'nrfg', 'ns', 'nt', 'nz', 't', 'eng'))
-    # words = []
-    # for ln in res:
-    #     words.append(valid_jsontxt(ln[0]) + "_" + valid_jsontxt(ln[1]))
-    # return valid_jsontxt(x) + "\001" + "\t".join(words)
-    return user_weibo
+    res = ja.textrank(user_weibo, topK=20, withWeight=True,
+                             allowPOS=('an', 'i', 'j', 'l', 'n', 'nr', 'nrfg', 'ns', 'nt', 'nz', 't', 'eng'))
+    words = []
+    for ln in res:
+        words.append(valid_jsontxt(ln[0]) + "_" + valid_jsontxt(ln[1]))
+    return valid_jsontxt(x) + "\001" + "\t".join(words)
+    # return user_weibo
 
 
 
