07966a3231240cda472b1ab9a9cda1e8fd7104ba
Gbacillus <gs464gan@gmail.com>
2016-10-17 09:15:17 +0800

    d

--- wrt/dianpuyunying/shuyang/t_wrt_znk_iteminfo.py
+++ wrt/dianpuyunying/shuyang/t_wrt_znk_iteminfo.py
@@ -4,7 +4,8 @@ import sys
 import copy
 import math
 import time
-import rapidjson as json
+# import rapidjson as json
+import json
 from pyspark import SparkContext
 now_day = sys.argv[1]
 last_day = sys.argv[2]
