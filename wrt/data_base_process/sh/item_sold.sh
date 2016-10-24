5f601f3f5eaa926260c5a0731192299a12372430
Gbacillus <gs464gan@gmail.com>
2016-10-24 10:45:27 +0800

    d

--- wrt/data_base_process/sh/item_sold.sh
+++ wrt/data_base_process/sh/item_sold.sh
@@ -18,11 +18,11 @@ $pre_path/wrt/data_base_process/sh/log_date/log_$zuotian 2>&1
 sh $pre_path/wrt/data_base_process/hive/t_base_item_sale.sql $zuotian >> \
 $pre_path/wrt/data_base_process/sh/log_date/log_$zuotian 2>&1
 
-hadoop fs -rm -r /user/wrt/daysale_tmp >> $pre_path/wrt/data_base_process/sh/log_daysale/log_$qiantian 2>&1
-
-spark-submit  --total-executor-cores  120   --executor-memory  9g  --driver-memory 10g \
-$pre_path/wrt/data_base_process/cal_daysale.py $qiantian $zuotian >> \
-$pre_path/wrt/data_base_process/sh/log_daysale/log_$qiantian 2>&1
-
-sh $pre_path/wrt/data_base_process/hive/cal_daysale.sql $qiantian >> \
-$pre_path/wrt/data_base_process/sh/log_daysale/log_$qiantian 2>&1
+#hadoop fs -rm -r /user/wrt/daysale_tmp >> $pre_path/wrt/data_base_process/sh/log_daysale/log_$qiantian 2>&1
+#
+#spark-submit  --total-executor-cores  120   --executor-memory  9g  --driver-memory 10g \
+#$pre_path/wrt/data_base_process/cal_daysale.py $qiantian $zuotian >> \
+#$pre_path/wrt/data_base_process/sh/log_daysale/log_$qiantian 2>&1
+#
+#sh $pre_path/wrt/data_base_process/hive/cal_daysale.sql $qiantian >> \
+#$pre_path/wrt/data_base_process/sh/log_daysale/log_$qiantian 2>&1
