ea20c7303cd17999779f5d70b6ae99508f340c51
Gbacillus <gs464gan@gmail.com>
2016-10-25 11:50:03 +0800

    d

--- wrt/data_base_process/sh/item_sold.sh
+++ wrt/data_base_process/sh/item_sold.sh
@@ -9,20 +9,20 @@ zuotian=$1
 qiantian=$2
 iteminfo_day=$3
 
-hfs -rmr /user/wrt/sale_tmp >> $pre_path/wrt/data_base_process/sh/log_date/log_$zuotian 2>&1
-
-spark-submit  --executor-memory 9G  --driver-memory 10G  --total-executor-cores 120 \
-$pre_path/wrt/data_base_process/t_base_item_sale.py $qiantian $zuotian $iteminfo_day >> \
-$pre_path/wrt/data_base_process/sh/log_date/log_$zuotian 2>&1
-
-sh $pre_path/wrt/data_base_process/hive/t_base_item_sale.sql $zuotian >> \
-$pre_path/wrt/data_base_process/sh/log_date/log_$zuotian 2>&1
-
-#hadoop fs -rm -r /user/wrt/daysale_tmp >> $pre_path/wrt/data_base_process/sh/log_daysale/log_$qiantian 2>&1
+#hfs -rmr /user/wrt/sale_tmp >> $pre_path/wrt/data_base_process/sh/log_date/log_$zuotian 2>&1
 #
-#spark-submit  --total-executor-cores  120   --executor-memory  9g  --driver-memory 10g \
-#$pre_path/wrt/data_base_process/cal_daysale.py $qiantian $zuotian >> \
-#$pre_path/wrt/data_base_process/sh/log_daysale/log_$qiantian 2>&1
+#spark-submit  --executor-memory 9G  --driver-memory 10G  --total-executor-cores 120 \
+#$pre_path/wrt/data_base_process/t_base_item_sale.py $qiantian $zuotian $iteminfo_day >> \
+#$pre_path/wrt/data_base_process/sh/log_date/log_$zuotian 2>&1
 #
-#sh $pre_path/wrt/data_base_process/hive/cal_daysale.sql $qiantian >> \
-#$pre_path/wrt/data_base_process/sh/log_daysale/log_$qiantian 2>&1
+#sh $pre_path/wrt/data_base_process/hive/t_base_item_sale.sql $zuotian >> \
+#$pre_path/wrt/data_base_process/sh/log_date/log_$zuotian 2>&1
+
+hadoop fs -rm -r /user/wrt/daysale_tmp >> $pre_path/wrt/data_base_process/sh/log_daysale/log_$qiantian 2>&1
+
+spark-submit  --total-executor-cores  120   --executor-memory  9g  --driver-memory 10g \
+$pre_path/wrt/data_base_process/cal_daysale.py $qiantian $zuotian >> \
+$pre_path/wrt/data_base_process/sh/log_daysale/log_$qiantian 2>&1
+
+sh $pre_path/wrt/data_base_process/hive/cal_daysale.sql $qiantian >> \
+$pre_path/wrt/data_base_process/sh/log_daysale/log_$qiantian 2>&1
