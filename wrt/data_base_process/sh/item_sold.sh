4477c003de3351310b37915da663999711ce086b
Gbacillus <gs464gan@gmail.com>
2016-10-31 13:52:27 +0800

    d

--- wrt/data_base_process/sh/item_sold.sh
+++ wrt/data_base_process/sh/item_sold.sh
@@ -7,7 +7,7 @@ iteminfo_day='20161013'
 
 zuotian=$1
 qiantian=$2
-#iteminfo_day=$3
+#iteminfo_ day=$3
 
 hfs -rmr /user/wrt/sale_tmp >> $pre_path/wrt/data_base_process/sh/log_date/log_$zuotian 2>&1
 
