0b87e868777600c85977fbea16b9a59ba6d2983c
Gbacillus <gs464gan@gmail.com>
2016-11-10 10:58:21 +0800

    d

---
+++ wrt/pinpaizhishu/huazhuangpin/ppzs_itemid_info.sql
@@ -0,0 +1,11 @@
+create table wlservice.ppzs_itemid_info(
+item_id string,
+brand_id string,
+title string,
+picurl string,
+price string
+)
+COMMENT '品牌指数项目商品信息表'
+PARTITIONED BY  (ds STRING )
+ROW FORMAT DELIMITED FIELDS TERMINATED BY '\001'   LINES TERMINATED BY '\n'
+stored as textfile ;
