c5e1d8994769ad1d1f594367b555d2042a33553b
Gbacillus <gs464gan@gmail.com>
2016-11-16 22:51:15 +0800

    d

--- wrt/data_base_process/repair/repair_shopitem_b_sold.sh
+++ wrt/data_base_process/repair/repair_shopitem_b_sold.sh
@@ -2,8 +2,7 @@
 source ~/.bashrc
 pre_path='/home/wrt/sparkdata'
 
-spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120\
-$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20160908 20160907
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 $pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20160908 20160907
 #spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120\
 #./repair_shopitem_b_sold.py 20160909 20160908
 #spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120\
