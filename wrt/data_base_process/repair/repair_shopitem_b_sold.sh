4d1feb2a3c1a01dd160958359cb0a998517c3c60
Gbacillus <gs464gan@gmail.com>
2016-11-16 22:58:10 +0800

    d

--- wrt/data_base_process/repair/repair_shopitem_b_sold.sh
+++ wrt/data_base_process/repair/repair_shopitem_b_sold.sh
@@ -2,8 +2,91 @@
 source ~/.bashrc
 pre_path='/home/wrt/sparkdata'
 
-spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 $pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20160908 20160907
-#spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120\
-#./repair_shopitem_b_sold.py 20160909 20160908
-#spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120\
-#./repair_shopitem_b_sold.py 20160909 20160908
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20160909 20160908
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20160910 20160909
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20160912 20160910
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20160914 20160912
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20160915 20160914
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20160929 20160915
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20160930 20160929
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161001 20160930
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161002 20161001
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161003 20161002
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161004 20161003
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161005 20161004
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161006 20161005
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161007 20161006
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161011 20161007
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161012 20161011
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161013 20161012
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161014 20161013
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161015 20161014
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161016 20161015
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161017 20161016
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161018 20161017
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161019 20161018
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161020 20161019
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161021 20161020
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161022 20161021
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161023 20161022
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161026 20161023
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161027 20161026
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161028 20161027
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161029 20161028
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161030 20161029
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161031 20161030
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161101 20161031
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161102 20161101
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161103 20161102
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161104 20161103
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161105 20161104
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161106 20161105
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161107 20161106
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161108 20161107
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161109 20161108
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161110 20161109
+spark-submit --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 \
+$pre_path/wrt/data_base_process/repair/repair_shopitem_b_sold.py 20161111 20161110
