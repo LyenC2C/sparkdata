f39aafd8d193e737b8974566285e773365d77528
zhanglj <zhanglinjiang@wolongdata.com>
2016-11-16 09:49:28 +0800

    weibo 数据修复

--- zlj/project/task/weibo/base_weibo/t_base_weibo_user_fri_bi_friends_groupby.sql
+++ zlj/project/task/weibo/base_weibo/t_base_weibo_user_fri_bi_friends_groupby.sql
@@ -1,27 +1,27 @@
 
 -- 相互关注的人聚合
 
+-- create table t_base_weibo_user_fri_bi_friends_groupby as
+--   SELECT  id1 as weibo_id ,
+--   concat_ws(',',collect_set( id2 )) as follow_ids
+--   from
+--   (
+--     SELECT
+--       id1,
+--       id2
+--     FROM t_base_weibo_user_fri_bi_friends
+--     UNION ALL
+--     SELECT
+--       id2 AS id1,
+--       id1 AS id2
+--     FROM t_base_weibo_user_fri_bi_friends
+--   )t group by id1
+-- ;
+
+
+-- 最新版本1106
+Drop table t_base_weibo_user_fri_bi_friends_groupby ;
 create table t_base_weibo_user_fri_bi_friends_groupby as
-  SELECT  id1 as weibo_id ,
-  concat_ws(',',collect_set( id2 )) as follow_ids
-  from
-  (
-    SELECT
-      id1,
-      id2
-    FROM t_base_weibo_user_fri_bi_friends
-    UNION ALL
-    SELECT
-      id2 AS id1,
-      id1 AS id2
-    FROM t_base_weibo_user_fri_bi_friends
-  )t group by id1
-;
-
-
--- 最新版本
-Drop table t_base_weibo_user_fri_bi_friends_groupby_1106 ;
-create table t_base_weibo_user_fri_bi_friends_groupby_1106 as
   SELECT  id1 as weibo_id ,
     concat_ws(',',collect_set( id2 )) as follow_ids
   from
@@ -38,6 +38,8 @@ create table t_base_weibo_user_fri_bi_friends_groupby_1106 as
   )t group by id1
 ;
 
+
+--
 
 SELECT COUNT(1) from t_base_weibo_user_fri_bi_friends_groupby where size(split(follow_ids,','))>5000
 
