08c28f48f57639a99a349d7346ae968bf18deef1
Gbacillus <gs464gan@gmail.com>
2016-11-17 17:00:03 +0800

    d

--- wrt/data_base_process/t_base_shopitem_b.py
+++ wrt/data_base_process/t_base_shopitem_b.py
@@ -105,6 +105,6 @@ rdd1 = rdd1_c.groupByKey().mapValues(list).map(lambda (x, y):quchong(x, y)) #去
 rdd2 = sc.textFile(s2).map(lambda x:f2(x)).filter(lambda x:x != None) #导入昨天数据
 rdd = rdd1.union(rdd2).groupByKey().mapValues(list).map(lambda (x, y):twodays(x, y)) #两天数据合并
 rdd.saveAsTextFile('/user/wrt/shopitem_tmp')
-#hfs -rmr /user/wrt/shopitem_tmp
+#hfs -rmr  /user/wrt/shopitem_tmp
 #spark-submit  --executor-memory 9G  --driver-memory 8G  --total-executor-cores 120 t_base_shopitem_b.py 20160906 20160905
 #LOAD DATA  INPATH '/user/wrt/shopitem_tmp' OVERWRITE INTO TABLE t_base_ec_shopitem_b PARTITION (ds='20160906');
