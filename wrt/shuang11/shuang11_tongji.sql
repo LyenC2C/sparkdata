724502b13a5ac95a5d7cbf4f674c12eb5d79d0d5
Gbacillus <gs464gan@gmail.com>
2016-11-20 14:31:47 +0800

    d

--- wrt/shuang11/shuang11_tongji.sql
+++ wrt/shuang11/shuang11_tongji.sql
@@ -162,33 +162,52 @@ ON
 t1.item_id = t2.item_id
 
 --6.2
+select sum((t1.sold - t2.sold)) as sold, sum((t1.sold - t2.sold) * t1.saleprice) as sales from
+(select item_id,sold,saleprice from t_base_ec_shopitem_b where ds = 20161111)t1
+JOIN
+(select item_id,sold from t_base_ec_shopitem_b where ds = 20161110)t2
+ON
+t1.item_id = t2.item_id
 
-select sum(sold),sum(sold*saleprice) as sales from t_base_ec_shopitem_b where ds = 20161111
+
 
 --7
 --7.1
 select root_cat_name,sum(day_sold) as sold ,sum(day_sold_price) as  sales from
-(select item_id,day_sold,day_sold_price,root_cat_name from t_base_ec_item_daysale_dev_new where ds = 20151111)t1
+(select item_id,day_sold,day_sold_price from t_base_ec_item_daysale_dev_new where ds = 20151111)t1
 JOIN
 (select item_id,root_cat_name from t_base_ec_item_dev_new where ds = 20161104 and bc_type = 'B')t2
 ON
 t1.item_id = t2.item_id
 group by root_cat_name
 --7.2
-select t2.root_cat_name,sum(sold)as sold,sum(sold*saleprice) as sales from
-(select * from t_base_ec_shopitem_b where ds = 20161111)t1
+
+select tt2.root_cat_name, sum(tt1.sold)as sold, sum(tt1.sales) as sales from
+(
+select t1.item_id,(t1.sold - t2.sold) as sold, (t1.sold - t2.sold) * t1.saleprice as sales from
+(select item_id,sold,saleprice from t_base_ec_shopitem_b where ds = 20161111)t1
 JOIN
-wlservice.t_wrt_tmp_shuang11_iteminfo t2
+(select item_id,sold from t_base_ec_shopitem_b where ds = 20161110)t2
 ON
 t1.item_id = t2.item_id
-group by t2.root_cat_name
-
-
+)tt1
+JOIN
+wlservice.t_wrt_tmp_shuang11_iteminfo tt2
+ON
+tt1.item_id = tt2.item_id
+group by tt2.root_cat_name
 --8
-select shop_id,
-(
-max(cast(desc_score as float) + cast(service_score as float) + cast(desc_highgap as float))
--
-min(cast(desc_score as float) + cast(service_score as float) + cast(desc_highgap as float))
-)
+
+select
+cast(((cast(desc_highgap as float) + cast(service_highgap as float) + cast(wuliu_highgap as float)
++ 279.260009765625) / 57.926000976562499)
+as int ) as point,count(1)
+from
+(select shopid,max(desc_highgap) as desc_highgap,max(service_highgap) as service_highgap ,max(wuliu_highgap) as wuliu_highgap
 from wlservice.t_wrt_tmp_shuang11_iteminfo
+group by shopid
+)t
+group by
+cast(((cast(desc_highgap as float) + cast(service_highgap as float) + cast(wuliu_highgap as float)
++ 279.260009765625) / 57.926000976562499)
+as int )
