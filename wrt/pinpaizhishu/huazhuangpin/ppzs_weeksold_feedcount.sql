55a53ddaf7fa40bcb3b7e734d38ba52ffcf9a47f
Gbacillus <gs464gan@gmail.com>
2016-11-21 22:03:35 +0800

    d

--- wrt/pinpaizhishu/huazhuangpin/ppzs_weeksold_feedcount.sql
+++ wrt/pinpaizhishu/huazhuangpin/ppzs_weeksold_feedcount.sql
@@ -16,4 +16,4 @@ join
 (select * from wlservice.ppzs_brandid_rate_count where ds = '${hiveconf:yes_day}')t2
 ON
 t1.brand_id = t2.brand_id
--- " >> ppzs_brandid_weeksold_feedcount_testd
+-- " >> ppzs_brandid_weeksold_feedcount_test
