5b70dff4fbeacc13d19e8ca9b339aa8e0a20d43d
Gbacillus <gs464gan@gmail.com>
2016-11-21 21:30:38 +0800

    d

--- wrt/pinpaizhishu/huazhuangpin/ppzs_weeksold_feedcount.sql
+++ wrt/pinpaizhishu/huazhuangpin/ppzs_weeksold_feedcount.sql
@@ -9,9 +9,9 @@
 
 -- hive -e "
 select t1.brand_id,t1.weeksold,t2.good_count,t2.mid_count,t2.bad_count from
-(select * from wlservice.ppzs_brandid_weeksold where ds = '20161106')t1
+(select * from wlservice.ppzs_brandid_weeksold where ds = '${hiveconf:yes_day}')t1
 join
-(select * from wlservice.ppzs_brandid_rate_count where ds = '20161108')t2
+(select * from wlservice.ppzs_brandid_rate_count where ds = '${hiveconf:yes_day}')t2
 ON
 t1.brand_id = t2.brand_id
 -- " >> ppzs_brandid_weeksold_feedcount_test
