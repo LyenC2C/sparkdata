34342d63e530602c61c14396f51ffa1ba2d7b7de
Gbacillus <gs464gan@gmail.com>
2016-11-21 21:26:16 +0800

    d

--- wrt/pinpaizhishu/huazhuangpin/ppzs_weeksold_feedcount.sql
+++ wrt/pinpaizhishu/huazhuangpin/ppzs_weeksold_feedcount.sql
@@ -1,8 +1,17 @@
-drop table wlservice.ppzs_brandid_weeksold_feedcount;
-create table wlservice.ppzs_brandid_weeksold_feedcount as
+-- drop table wlservice.ppzs_brandid_weeksold_feedcount;
+-- create table wlservice.ppzs_brandid_weeksold_feedcount as
+-- select t1.brand_id,t1.weeksold,t2.good_count,t2.mid_count,t2.bad_count from
+-- (select * from wlservice.ppzs_brandid_weeksold where ds = '20161106')t1
+-- join
+-- (select * from wlservice.ppzs_brandid_rate_count where ds = '20161108')t2
+-- ON
+-- t1.brand_id = t2.brand_id
+
+-- hive -e "
 select t1.brand_id,t1.weeksold,t2.good_count,t2.mid_count,t2.bad_count from
 (select * from wlservice.ppzs_brandid_weeksold where ds = '20161106')t1
 join
 (select * from wlservice.ppzs_brandid_rate_count where ds = '20161108')t2
 ON
 t1.brand_id = t2.brand_id
+-- " >> ppzs_brandid_weeksold_feedcount_test
