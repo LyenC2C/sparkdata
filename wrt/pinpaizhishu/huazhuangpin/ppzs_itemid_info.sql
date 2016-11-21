5af5b132a98f6dc4d9ba9c18a13ee472297536bc
Gbacillus <gs464gan@gmail.com>
2016-11-21 21:42:27 +0800

    d

--- wrt/pinpaizhishu/huazhuangpin/ppzs_itemid_info.sql
+++ wrt/pinpaizhishu/huazhuangpin/ppzs_itemid_info.sql
@@ -1,12 +1,2 @@
-create table wlservice.ppzs_itemid_info(
-item_id string,
-brand_id string,
-title string,
-picurl string,
-price string
-)
-COMMENT '品牌指数项目商品信息表'
-PARTITIONED BY  (ds STRING )
-ROW FORMAT DELIMITED FIELDS TERMINATED BY '\001'   LINES TERMINATED BY '\n'
-stored as textfile ;
---
+LOAD DATA  INPATH '/user/wrt/temp/ppzs_itemid_info' OVERWRITE INTO TABLE
+wlservice.ppzs_itemid_info PARTITION (ds=$yes_day);
