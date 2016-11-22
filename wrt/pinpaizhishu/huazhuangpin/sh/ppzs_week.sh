8d7fac12a70e93d2efce0e5ef2fe0fdcd7a2edbe
Gbacillus <gs464gan@gmail.com>
2016-11-22 16:25:49 +0800

    d

--- wrt/pinpaizhishu/huazhuangpin/sh/ppzs_week.sh
+++ wrt/pinpaizhishu/huazhuangpin/sh/ppzs_week.sh
@@ -6,7 +6,7 @@ last_day=$(date -d '8 days ago' +%Y%m%d)
 threemonth_day=$(date -d '93 days ago' +%Y%m%d)
 save_day=$(date -d '0 days ago' +%Y%m%d)
 
-sh $dev_path/sh/ppzs_upgrad.sh 20161120 20161114 20160820 20161121
+sh $dev_path/sh/ppzs_upgrad.sh 20161120 20161114 20160820 20161121 &> $dev_path/sh/ppzs_log/log_$save_day
 #sh $dev_path/sh/ppzs_upgrad.sh $yes_day $last_day $threemonth_day $save_day
 
 
