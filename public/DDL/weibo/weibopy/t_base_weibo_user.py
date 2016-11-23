147b9ac3bdb4b0657aa52ae928706e8a0d0fe59b
Gbacillus <gs464gan@gmail.com>
2016-11-23 14:17:49 +0800

    d

--- public/DDL/weibo/weibopy/t_base_weibo_user.py
+++ public/DDL/weibo/weibopy/t_base_weibo_user.py
@@ -37,6 +37,7 @@ def parse(line):
         return None
     if type(ob)!=type({}):return None
     id=ob.get('id','-1')
+    if id == '':return None
     if  int(id)<-1:return None
     idstr=ob.get('idstr','-')
     screen_name=ob.get('screen_name','-')
