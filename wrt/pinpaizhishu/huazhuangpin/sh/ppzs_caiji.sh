79704e4156992d4ca4c22a32bd99dbc029cffdda
Gbacillus <gs464gan@gmail.com>
2016-11-23 10:18:54 +0800

    d

--- wrt/pinpaizhishu/huazhuangpin/sh/ppzs_caiji.sh
+++ wrt/pinpaizhishu/huazhuangpin/sh/ppzs_caiji.sh
@@ -2,7 +2,7 @@
 source ~/.bashrc
 dev_path='/home/wrt/sparkdata/wrt/pinpaizhishu/huazhuangpin'
 save_path='/mnt/raid1/wrt/pinpaizhishu/development'
-#每周六入库，周六当天计划任务执行该脚本即可
-saturday=$(date -d '0 days ago' +%Y%m%d)
+#每周六入库，周六当天计划任务执行该脚本即可,参数为周五，因为需要取周五的shopitem数据
+friday=$(date -d '1 days ago' +%Y%m%d)
 iteminfo_day="20161104"
-hive -hiveconf saturday=$saturday iteminfo_day=$iteminfo_day -f $dev_path/t_wrt_caiji_ppzs_itemid.sql
+hive -hiveconf friday=$friday iteminfo_day=$iteminfo_day -f $dev_path/t_wrt_caiji_ppzs_itemid.sql
