65ebcc18b5ad87658356ce433c43ce1891591f83
Gbacillus <gs464gan@gmail.com>
2016-11-23 10:29:25 +0800

    d

--- wrt/pinpaizhishu/huazhuangpin/sh/ppzs_caiji.sh
+++ wrt/pinpaizhishu/huazhuangpin/sh/ppzs_caiji.sh
@@ -5,4 +5,5 @@ save_path='/mnt/raid1/wrt/pinpaizhishu/development'
 #每周六入库，周六当天计划任务执行该脚本即可,参数为周五，因为需要取周五的shopitem数据
 friday=$(date -d '1 days ago' +%Y%m%d)
 iteminfo_day="20161104"
-hive -hiveconf friday=$friday iteminfo_day=$iteminfo_day -f $dev_path/t_wrt_caiji_ppzs_itemid.sql
+hive -hiveconf friday=$friday iteminfo_day=$iteminfo_day -f \
+$dev_path/t_wrt_caiji_ppzs_itemid.sql &>  $dev_path/sh/ppzs_log/caiji_log_$friday
