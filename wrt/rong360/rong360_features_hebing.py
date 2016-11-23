7167a047eae03fd861fef49432d0b105251c834e
Gbacillus <gs464gan@gmail.com>
2016-11-23 17:50:03 +0800

    d

--- wrt/rong360/rong360_features_hebing.py
+++ wrt/rong360/rong360_features_hebing.py
@@ -38,5 +38,5 @@ s_main = "/hive/warehouse/wlservice.db/t_rong360_model_features_new/000000_0"
 rdd = sc.textFile(s_3j).flatMap(lambda x:f_3j_reindex(x)).groupByKey().mapValues(list).map(lambda (x, y): x)
 fea_3j = rdd.collect()
 rdd2 = sc.textFile(s_main)
-rdd2.map(lambda x:x.strip() + "".join(fea_3j)).saveAsTextFile('/user/wrt/temp/rong360_3j_features')
+rdd2.map(lambda x:x.strip() + "".join(fea_3j)).saveAsTextFile('/user/wrt/temp/collect_test')
 # rdd.saveAsTextFile('/user/wrt/temp/rong360_3j_features')
