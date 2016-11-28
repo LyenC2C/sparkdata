8aa77bacb9c386442a29631c67193dee2870a743
Gbacillus <gs464gan@gmail.com>
2016-11-28 14:41:51 +0800

    d

--- wrt/pinpaizhishu/huazhuangpin/sh/ppzs_week.sh
+++ wrt/pinpaizhishu/huazhuangpin/sh/ppzs_week.sh
@@ -4,7 +4,7 @@ dev_path='/home/wrt/sparkdata/wrt/pinpaizhishu/huazhuangpin'
 yes_day=$(date -d '1 days ago' +%Y%m%d)
 last_day=$(date -d '8 days ago' +%Y%m%d)
 threemonth_day=$(date -d '93 days ago' +%Y%m%d)
-save_day=$(date -d '0 days ago' +%Y%m%d)
+save_day=$(date -d '1 days ago' +%Y%m%d)
 
 #sh $dev_path/sh/ppzs_upgrad.sh 20161120 20161114 20160820 20161121 &> $dev_path/sh/ppzs_log/log_$save_day
 sh $dev_path/sh/ppzs_upgrad.sh $yes_day $last_day $threemonth_day $save_day &> $dev_path/sh/ppzs_log/log_$save_day
