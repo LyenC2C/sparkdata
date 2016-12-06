4f4f0ecbb122f8e00c0a52e1f82988ab7e8798fc
Gbacillus <gs464gan@gmail.com>
2016-12-06 10:57:33 +0800

    d

--- wrt/credit/shixin_person.py
+++ wrt/credit/shixin_person.py
@@ -57,9 +57,10 @@ def f(line):
     result.append(unperformpart)
     return (id,result)
 
-rdd_c = sc.textFile("/commit/shixin.info.20161029.json").map(lambda x:f(x))
+# rdd_c = sc.textFile("/commit/shixin.info.20161029.json").map(lambda x:f(x))
+rdd_c = sc.textFile("/commit/credit/shixin/shixin.info.person.20161205").map(lambda x:f(x))
 rdd = rdd_c.groupByKey().mapValues(list).map(lambda (x,y):"\001".join([valid_jsontxt(i) for i in y[0]]))
-rdd.saveAsTextFile("/user/wrt/temp/shixin_info_20161029")
+rdd.saveAsTextFile("/user/wrt/temp/shixin_personinfo")
 
 # spark-submit  --executor-memory 6G  --driver-memory 8G  --total-executor-cores 80 t_base_weibo_text.py
 # LOAD DATA  INPATH '/user/wrt/temp/shixin_info_20161029' OVERWRITE INTO TABLE t_court_shixin_person PARTITION (ds='20161029');
