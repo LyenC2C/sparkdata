3c2988981cc677e4e68e391d55201760d676bde9
Gbacillus <gs464gan@gmail.com>
2016-12-07 13:47:29 +0800

    d

--- wrt/data_base_process/sh/item_shop.sh
+++ wrt/data_base_process/sh/item_shop.sh
@@ -53,34 +53,34 @@ LOAD DATA  INPATH '/user/wrt/temp/shopinfo_tmp' OVERWRITE INTO TABLE t_base_ec_s
 
 insert OVERWRITE table t_base_ec_shop_dev_new PARTITION(ds = $today)
 select
-case when t1.shop_id is null then t2.shop_id else t1.shop_id,
-case when t1.shop_id is null then t2.seller_id else t1.seller_id,
-case when t1.shop_id is null then t2.shop_name else t1.shop_name,
-case when t1.shop_id is null then t2.seller_name else t1.seller_name,
-case when t1.shop_id is null then t2.star else t1.star,
-case when t1.shop_id is null then t2.credit else t1.credit,
-case when t1.shop_id is null then t2.starts else t1.starts,
-case when t1.shop_id is null then t2.bc_type else t1.bc_type,
-case when t1.shop_id is null then t2.item_count else t1.item_count,
-case when t1.shop_id is null then t2.fans_count else t1.fans_count,
-case when t1.shop_id is null then t2.good_rate_p else t1.good_rate_p,
-case when t1.shop_id is null then t2.weitao_id else t1.weitao_id,
-case when t1.shop_id is null then t2.desc_score else t1.desc_score,
-case when t1.shop_id is null then t2.service_score else t1.service_score,
-case when t1.shop_id is null then t2.wuliu_score else t1.wuliu_score,
-case when t1.shop_id is null then t2.location else t1.location,
-case when t1.shop_id is null then t2.ts else t1.ts,
-case when t1.shop_id is null then t2.desc_highgap else t1.desc_highgap,
-case when t1.shop_id is null then t2.service_highgap else t1.service_highgap,
-case when t1.shop_id is null then t2.wuliu_highgap else t1.wuliu_highgap,
-case when t1.shop_id is null then t2.is_online else t1.is_online,
-case when t1.shop_id is null then t2.shop_type else t1.shop_type,
-case when t1.shop_id is null then t2.shop_certifi else t1.shop_certifi
+case when t1.shop_id is null then t2.shop_id else t1.shop_id end,
+case when t1.shop_id is null then t2.seller_id else t1.seller_id end,
+case when t1.shop_id is null then t2.shop_name else t1.shop_name end,
+case when t1.shop_id is null then t2.seller_name else t1.seller_name end,
+case when t1.shop_id is null then t2.star else t1.star end,
+case when t1.shop_id is null then t2.credit else t1.credit end,
+case when t1.shop_id is null then t2.starts else t1.starts end,
+case when t1.shop_id is null then t2.bc_type else t1.bc_type end,
+case when t1.shop_id is null then t2.item_count else t1.item_count end,
+case when t1.shop_id is null then t2.fans_count else t1.fans_count end,
+case when t1.shop_id is null then t2.good_rate_p else t1.good_rate_p end,
+case when t1.shop_id is null then t2.weitao_id else t1.weitao_id end,
+case when t1.shop_id is null then t2.desc_score else t1.desc_score end,
+case when t1.shop_id is null then t2.service_score else t1.service_score end,
+case when t1.shop_id is null then t2.wuliu_score else t1.wuliu_score end,
+case when t1.shop_id is null then t2.location else t1.location end,
+case when t1.shop_id is null then t2.ts else t1.ts end,
+case when t1.shop_id is null then t2.desc_highgap else t1.desc_highgap end,
+case when t1.shop_id is null then t2.service_highgap else t1.service_highgap end,
+case when t1.shop_id is null then t2.wuliu_highgap else t1.wuliu_highgap end,
+case when t1.shop_id is null then t2.is_online else t1.is_online end,
+case when t1.shop_id is null then t2.shop_type else t1.shop_type end,
+case when t1.shop_id is null then t2.shop_certifi else t1.shop_certifi end
 from
 (select * from t_base_ec_shop_dev_new where ds = 'temp')t1
 full outer JOIN
 (select * from t_base_ec_shop_dev_new where ds = $lastday)t2
 ON
-t1.item_id = t2.item_id;
+t1.shop_id = t2.shop_id;
 
 EOF
