816165f44de336d5ad51222662cd3aeb9a6b7aba
Gbacillus <gs464gan@gmail.com>
2016-12-12 10:07:19 +0800

    d

--- wrt/data_base_process/t_base_item_info.py
+++ wrt/data_base_process/t_base_item_info.py
@@ -160,7 +160,7 @@ if __name__ == "__main__":
         from pyspark import SparkContext
         sc = SparkContext(appName="t_base_item_info")
         # s = "/hive/warehouse/wlbase_dev.db/t_base_ec_item_house/part*"
-        s = "/commit/iteminfo/house_tmp/*/*"
+        s = "/commit/iteminfo/*"
         s_dim = "/hive/warehouse/wlbase_dev.db/t_base_ec_dim/ds=20161122/000000_0"
         cate_dict = sc.broadcast(sc.textFile(s_dim).map(lambda x: get_cate_dict(x)).filter(lambda x:x!=None).collectAsMap()).value
         rdd_c = sc.textFile(s).map(lambda x: f(x,cate_dict)).filter(lambda x:x!=None)
