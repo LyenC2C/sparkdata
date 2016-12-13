2d4e47a97d0004da7c258d2b49dcc07a3184a3ef
Gbacillus <gs464gan@gmail.com>
2016-12-13 19:03:04 +0800

    d

--- wrt/credit/shixin/shixin.sh
+++ wrt/credit/shixin/shixin.sh
@@ -5,5 +5,5 @@ now_day = $(date -d '0 days ago' +%Y%m%d)
 #last_day = $(date -d '7 days ago' +%Y%m%d)
 last_day = "20161205"
 
-sh ./shixin_person_inhive.sh $now_day $last_day
-sh ./shixin_qiye_inhive.sh $now_day $last_day
+sh $pre_path/shixin_person_inhive.sh $now_day $last_day
+sh $pre_path/shixin_qiye_inhive.sh $now_day $last_day
