55ddebe833a01d376d9c45a10f2d8c3513456faf
Gbacillus <gs464gan@gmail.com>
2016-12-14 10:34:33 +0800

    d

--- wrt/credit/shixin/shixin_qiye.py
+++ wrt/credit/shixin/shixin_qiye.py
@@ -90,8 +90,8 @@ rdd_now = rdd_c.groupByKey().mapValues(list).map(lambda (x,y):"\001".join([valid
 # rdd_last = sc.textFile("/hive/warehouse/wlcredit.db/t_wrt_shixin_person/" + last_day).map(lambda x:f2(x))
 rdd_now.saveAsTextFile("/user/wrt/temp/shixin_qiyeinfo")
 
-rdd_count = (str(rdd.count()) + "\t" +  str(rdd_now.count())).collect()
-rdd_count.saveAsTextFile("/user/wrt/credit/shixin_qiye_count_" + now_day)
+parallelize(str(rdd.count()) + "\t" +  str(rdd_now.count()))\
+    .saveAsTextFile("/user/wrt/credit/shixin_qiye_count_" + now_day)
 
 # f_w = open("shixin_count/shixin_person_count_"+now_day,'w')
 # total = rdd.count()
