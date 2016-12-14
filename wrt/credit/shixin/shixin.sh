156ac41416736bf58b6d8bfa50964ecbbb7e47ce
Gbacillus <gs464gan@gmail.com>
2016-12-14 09:32:22 +0800

    d

--- wrt/credit/shixin/shixin.sh
+++ wrt/credit/shixin/shixin.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 source ~/.bashrc
 pre_path='/home/wrt/sparkdata/wrt/credit/shixin'
-now_day = $(date -d '0 days ago' +%Y%m%d)
+now_day=$(date -d '0 days ago' +%Y%m%d)
 #last_day = $(date -d '7 days ago' +%Y%m%d)
-last_day = "20161205"
+last_day="20161205"
 
 sh $pre_path/shixin_person_inhive.sh $now_day $last_day
 sh $pre_path/shixin_qiye_inhive.sh $now_day $last_day
