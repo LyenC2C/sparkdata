e27d8f479800032b3901210de27f1b805ee250d3
pengzhongzheng <pengzhongzheng@wolongdata.com>
2016-12-19 13:36:34 +0800

    about pzz

--- pzz/testdir/test_para.py
+++ pzz/testdir/test_para.py
@@ -10,10 +10,10 @@ if __name__ == '__main__':
     conf.set("spark.akka.timeout", "300")
     conf.set("spark.shuffle.memoryFraction", "0.5")
     conf.set("spark.core.connection.ack.wait.timeout", "1800")
-    conf.set("sc spark.default.parallelism","400")
+    conf.set("spark.default.parallelism","400")
 
     sc = SparkContext(appName="parallese test",conf=conf)
-    rdd = sc.textFile("/hive/warehouse/wlbase_dev.db/qun_member_info/ds=info/part-0000*")
+    rdd = sc.textFile("/hive/warehouse/wlbase_dev.db/qun_member_info/ds=info/part-*")
     rdd.count()
     rdd1 = rdd.map(lambda x:x.split("\001")).map(lambda x:[x[0],x[5]]).groupByKey()
     rdd2 = rdd1.mapValues(list).map(lambda (x,y):y[0]).filter(lambda x:x!=None).map(lambda x:[x,1]).groupByKey()
