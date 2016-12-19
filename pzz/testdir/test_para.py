22b0b3d3c4880f089beeee2ec4ee2b4114b45112
pengzhongzheng <pengzhongzheng@wolongdata.com>
2016-12-19 10:20:26 +0800

    about pzz

---
+++ pzz/testdir/test_para.py
@@ -0,0 +1,19 @@
+from pyspark import *
+
+
+
+if __name__ == '__main__':
+    conf = SparkConf()
+    conf.set("spark.kryoserializer.buffer.mb","512")
+    conf.set("spark.broadcast.compress","true")
+    conf.set("spark.driver.maxResultSize","4g")
+    conf.set("spark.akka.timeout", "300")
+    conf.set("spark.shuffle.memoryFraction", "0.5")
+    conf.set("spark.core.connection.ack.wait.timeout", "1800")
+    conf.set("sc spark.default.parallelism","400")
+
+    sc = SparkContext(appName="parallese test",conf=conf)
+    rdd = sc.textFile("/hive/warehouse/wlbase_dev.db/qun_member_info/ds=info")
+    rdd.count()
+    rdd.map(lambda x:[x.split("\001")[0],1]).groupByKey().count()
+    sc.stop()
