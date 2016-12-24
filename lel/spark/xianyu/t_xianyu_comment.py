9a8ac4e00c8cb7e72a44f4908d94c802a4014661
lyen <lienlian@wolongdata.com>
2016-12-24 21:39:06 +0800

    xianyu

--- lel/spark/xianyu/t_xianyu_comment.py
+++ lel/spark/xianyu/t_xianyu_comment.py
@@ -12,7 +12,6 @@ def valid_jsontxt(content):
         res = str(content)
     return res.replace('\n', "").replace("\r", "").replace('\001', "").replace("\u0001", "")
 def getJson(s):
-    if s == None:return None
     content = [valid_jsontxt(i) for i in (s.strip().split('\t'))]
     if len(content) == 4:
         ts = content[0]
@@ -20,32 +19,30 @@ def getJson(s):
         start = content[3].find("({") + 1
         js = content[3][start:-1]
         return(ts,itemid,json.loads(valid_jsontxt(js)))
-    else:
-        return None
 def parseJson(ob):
-    if ob == None:return None
+    if ob is None:return None
     ts = ob[0]
     itemid = ob[1]
     if type(ob[2]) != type({}): return None
     items = ob[2].get("data", {}).get("items", [])
     result = []
-    if len(items) > 0 and items != None:
-        for item in items:
-            lv = []
-            if item.get("itemId","") != "":
-                commentId = item.get("commentId", "\\N")
-                content = item.get("content", "\\N")
-                reportTime = item.get("reportTime", "\\N")
-                reporterName = item.get("reporterName", "\\N")
-                reporterNick = item.get("reporterNick", "\\N")
-                lv.append(itemid)
-                lv.append(commentId)
-                lv.append(content)
-                lv.append(reportTime)
-                lv.append(reporterName)
-                lv.append(reporterNick)
-                lv.append(ts)
-                result.append('\001'.join([valid_jsontxt(i) for i in lv]))
+    # if len(items) > 0 and items is not None :
+    #     for item in items:
+    #         lv = []
+    #         if item.get("itemId","") != "":
+    #             commentId = item.get("commentId", "\\N")
+    #             content = item.get("content", "\\N")
+    #             reportTime = item.get("reportTime", "\\N")
+    #             reporterName = item.get("reporterName", "\\N")
+    #             reporterNick = item.get("reporterNick", "\\N")
+    #             lv.append(itemid)
+    #             lv.append(commentId)
+    #             lv.append(content)
+    #             lv.append(reportTime)
+    #             lv.append(reporterName)
+    #             lv.append(reporterNick)
+    #             lv.append(ts)
+    #             result.append('\001'.join([valid_jsontxt(i) for i in lv]))
     return result
 
 
