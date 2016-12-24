500915fad36d4bc9cd13167efd6d36d09d6a8efc
lyen <lienlian@wolongdata.com>
2016-12-24 22:04:21 +0800

    xianyu

--- lel/spark/xianyu/t_xianyu_comment.py
+++ lel/spark/xianyu/t_xianyu_comment.py
@@ -27,30 +27,31 @@ def getJson(s):
 def parseJson(ob):
     ts = ob[0]
     itemid = ob[1]
-    if type(ob[2]) != type({}): return [None]
+    if type(ob[2]) != type({}): return []
     items = ob[2].get("data", {}).get("items", [])
     result = []
-    for item in items:
-        lv = []
-        if item.get("itemId", "") != "":
-            commentId = item.get("commentId", "\\N")
-            content = item.get("content", "\\N")
-            reportTime = item.get("reportTime", "\\N")
-            reporterName = item.get("reporterName", "\\N")
-            reporterNick = item.get("reporterNick", "\\N")
-            lv.append(itemid)
-            lv.append(commentId)
-            lv.append(content)
-            lv.append(reportTime)
-            lv.append(reporterName)
-            lv.append(reporterNick)
-            lv.append(ts)
-            result.append('\001'.join([valid_jsontxt(i) for i in lv]))
+    if len(items) > 0 and type(items) == type([]):
+        for item in items:
+            lv = []
+            if item.get("itemId", "") != "":
+                commentId = item.get("commentId", "\\N")
+                content = item.get("content", "\\N")
+                reportTime = item.get("reportTime", "\\N")
+                reporterName = item.get("reporterName", "\\N")
+                reporterNick = item.get("reporterNick", "\\N")
+                lv.append(itemid)
+                lv.append(commentId)
+                lv.append(content)
+                lv.append(reportTime)
+                lv.append(reporterName)
+                lv.append(reporterNick)
+                lv.append(ts)
+                result.append('\001'.join([valid_jsontxt(i) for i in lv]))
     return result
 
 
 sc = SparkContext(appName="xianyu_iteminfo_comment" + lastday)
 
 data = sc.textFile("/commit/2taobao/leave_comment/*" + lastday + "/*")
-re = data.flatMap(lambda a: parseJson(getJson(a))).filter(lambda x: x != None).saveAsTextFile(
+re = data.flatMap(lambda a: parseJson(getJson(a))).filter(lambda x: len(x) != 0).saveAsTextFile(
     "/user/lel/temp/xianyu_comment_2016")
