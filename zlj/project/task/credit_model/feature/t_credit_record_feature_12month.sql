19394adbe04aa160679bebd754a4cbaf479f1409
zhanglj <zhanglinjiang@wolongdata.com>
2016-12-25 11:46:27 +0800

    分词脚本更新

---
+++ zlj/project/task/credit_model/feature/t_credit_record_feature_12month.sql
@@ -0,0 +1,77 @@
+
+
+
+
+-- 价格的加减乘除
+drop table wlcredit.t_credit_record_feature_12month ;
+create table wlcredit.t_credit_record_feature_12month as
+    SELECT
+    tel_index,
+    max(user_id) user_id ,
+    round(sum(price),2)   month12_price_sum,
+    round(count(1) ,2)    month12_buy_count,
+    round(avg(price),2)   month12_price_avg,
+    round(max(price),2)   month12_price_max,
+    round(min(price),2)   month12_price_min,
+    round(std(price),2)   month12_price_std,
+    round(percentile(cast(price as int),0.5),2)   month12_price_median,
+    round(max(price)-min(price),2)      month12_price_cross ,
+    round(percentile(cast(price as int),0.25),2)   month12_price_025,
+    round(percentile(cast(price as int),0.75),2)   month12_price_075,
+    round(percentile(cast(price as int),0.10),2)   month12_price_010,
+
+    sum(CASE WHEN annoy = '1'       THEN 1        ELSE 0 END)     as        month12_annoy_num,
+    sum(CASE WHEN annoy = '1'       THEN 1        ELSE 0 END)/COUNT(1) as  month12_annoy_ratio,
+    COUNT(DISTINCT brand_id)  as  month12_brand_id_num,
+    COUNT(DISTINCT root_cat_id) as month12_root_cat_id_num,
+    sum(CASE WHEN bc_type = 'B'       THEN 1         ELSE 0 END)       as          month12_b_bc_type_num,
+    sum(CASE WHEN bc_type = 'B'       THEN 1         ELSE 0 END)/COUNT(1)      as     month12_b_bc_type_num_ratio,
+    sum(CASE WHEN bc_type = 'B'       THEN price         ELSE 0 END)/sum(price)   as  month12_b_bc_price_ratio,
+    sum(CASE WHEN CAST (brand_id as bigint )>5       THEN price         ELSE 0 END)/sum(price)  as  month12_brand_effec_price_ratio,
+    sum(CASE WHEN CAST (brand_id as bigint )>5     THEN 1        ELSE 0 END)/COUNT(1)    as        month12_brand_effec_num_ratio,
+    sum(case when price <=50 then 1 else 0 end)/count(*)        as month12_b50_num_ratio,
+    sum(case when price <=50 then price else 0 end)/sum(price ) as month12_b50_ratio,
+    sum(case when price <=30 then 1 else 0 end)/count(*)        as month12_b30_num_ratio,
+    sum(case when price <=30 then price else 0 end)/sum(price ) as month12_b30_ratio,
+    sum(case when price <=10 then 1 else 0 end)/count(*)        as month12_b10_num_ratio,
+    sum(case when price <=10 then price else 0 end)/sum(price ) as month12_b10_ratio,
+    sum(case when price <=5 then 1 else 0 end)/count(*)        as   month12_b5_num_ratio,
+    sum(case when price <=5 then price else 0 end)/sum(price ) as   month12_b5_ratio,
+
+          sum(case when price <=50 and CAST(price/5 as int )==10 then 1 else 0 end)/count(*) as  month12_b50_10_num_ratio,
+          sum(case when price <=50 and CAST(price/5 as int )==9  then 1 else 0 end)/count(*) as  month12_b50_9_num_ratio,
+          sum(case when price <=50 and CAST(price/5 as int )==8  then 1 else 0 end)/count(*) as  month12_b50_8_num_ratio,
+          sum(case when price <=50 and CAST(price/5 as int )==7  then 1 else 0 end)/count(*) as  month12_b50_7_num_ratio,
+          sum(case when price <=50 and CAST(price/5 as int )==6  then 1 else 0 end)/count(*) as  month12_b50_6_num_ratio,
+          sum(case when price <=50 and CAST(price/5 as int )==5  then 1 else 0 end)/count(*) as  month12_b50_5_num_ratio,
+          sum(case when price <=50 and CAST(price/5 as int )==4  then 1 else 0 end)/count(*) as  month12_b50_4_num_ratio,
+          sum(case when price <=50 and CAST(price/5 as int )==3  then 1 else 0 end)/count(*) as  month12_b50_3_num_ratio,
+          sum(case when price <=50 and CAST(price/5 as int )==2  then 1 else 0 end)/count(*) as  month12_b50_2_num_ratio,
+          sum(case when price <=50 and CAST(price/5 as int )==1  then 1 else 0 end)/count(*) as  month12_b50_1_num_ratio,
+          sum(case when price <=50 and CAST(price/5 as int )==0  then 1 else 0 end)/count(*) as  month12_b50_0_num_ratio,
+
+    sum(case when price <=50 and CAST(price/5 as int )==10 then price else 0 end)/sum(price) as month12_b50_10_ratio,
+    sum(case when price <=50 and CAST(price/5 as int )==9  then price else 0 end)/sum(price) as month12_b50_9_ratio,
+    sum(case when price <=50 and CAST(price/5 as int )==8  then price else 0 end)/sum(price) as month12_b50_8_ratio,
+    sum(case when price <=50 and CAST(price/5 as int )==7  then price else 0 end)/sum(price) as month12_b50_7_ratio,
+    sum(case when price <=50 and CAST(price/5 as int )==6  then price else 0 end)/sum(price) as month12_b50_6_ratio,
+    sum(case when price <=50 and CAST(price/5 as int )==5  then price else 0 end)/sum(price) as month12_b50_5_ratio,
+    sum(case when price <=50 and CAST(price/5 as int )==4  then price else 0 end)/sum(price) as month12_b50_4_ratio,
+    sum(case when price <=50 and CAST(price/5 as int )==3  then price else 0 end)/sum(price) as month12_b50_3_ratio,
+    sum(case when price <=50 and CAST(price/5 as int )==2  then price else 0 end)/sum(price) as month12_b50_2_ratio,
+    sum(case when price <=50 and CAST(price/5 as int )==1  then price else 0 end)/sum(price) as month12_b50_1_ratio,
+    sum(case when price <=50 and CAST(price/5 as int )==0  then price else 0 end)/sum(price) as month12_b50_0_ratio,
+
+    (sum(pow(2.8, datediff* (-0.005)))+20)/75  as month12_active_score
+    from wlbase_dev.t_base_record_cate_simple_xianyu
+    where  regexp_replace(date_sub(from_unixtime( unix_timestamp() ,'yyyy-MM-dd'),30*6),'-','' )>dsn
+    group by tel_index
+    ;
+
+-- SELECT
+-- tel_index,root_cat_id as cate_level1,
+-- concat_ws(':', concat_ws('_',cast( root_cat_id as string) ,'median_price_level1') ,cast( round(percentile(cast(price as int),0.5),2) as string) ) price_median,
+-- concat_ws(':', concat_ws('_',cast( root_cat_id as string) ,'cross_price_level1' ) ,cast( round(max(price)-min(price),2) as string) ) price_max
+-- from wlbase_dev.t_base_record_cate where tel_index is not null and tel_user_rn<4 and price<160000
+-- and  root_cat_id is not null
+-- group by tel_index,root_cat_id
--- zlj/project/task/credit_model/feature/t_credit_user_profile_telindex.sql
+++ zlj/project/task/credit_model/feature/t_credit_user_profile_telindex.sql
@@ -14,3 +14,18 @@ t_base_user_profile_telindex
 微博粉丝特征
 
 购买/年限月份  频次特征
+
+  drop table wlcredit.t_credit_user_profile_feature;
+  create table wlcredit.t_credit_user_profile_feature as
+  SELECT
+  tel_index ,
+  case when alipay like '已通过支付宝实名认证' then 1 else 0 end  as alipay_flag  ,
+  buycnt  ,
+  cast(regexp_replace(verify, 'VIP等级', '')  as int) as  verify_level ,
+  -- verify,
+  (12 * (2016 - YEAR(regexp_replace(regtime, '\\.', '-'))) + (7 - MONTH(regexp_replace(regtime, '\\.', '-')))) regtime_month ,
+  buycnt/((12 * (2016 - YEAR(regexp_replace(regtime, '\\.', '-'))) + (7 - MONTH(regexp_replace(regtime, '\\.', '-'))))+1) as buy_freq,
+  split(tel_loc,'\\s+')[0] as prov,
+  model_predict_gender
+  from
+  t_base_user_profile_telindex ;
