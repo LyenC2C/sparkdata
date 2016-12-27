e60f8cea417f3dbce07f88d5b2834e6603c97478
lyen <lienlian@wolongdata.com>
2016-12-27 10:54:01 +0800

    xianyu

---
+++ lel/spark/stuff/itemloss.py
@@ -0,0 +1,42 @@
+from pyspark import SparkContext
+
+sc = SparkContext(appName="xianyu_iteminfo")
+
+def valid_jsontxt(content):
+    if type(content) == type(u""):
+        res = content.encode("utf-8")
+    else:
+        res = str(content)
+    return res.replace('\n', "").replace("\r", "").replace('\001', "").replace("\u0001", "")
+
+
+data = sc.textFile("/user/lel/datas/lossitemid.csv").filter(lambda a: "enc_mobile" not in a).map(
+    lambda a: a.split(',')[1])
+
+itemid = sc.broadcast(data.collect())
+
+
+def getItemAndCate(s):
+    data = s.split('\001')
+    itemid = data[0]
+    root_cate = data[5]
+    if '-' not in root_cate:
+        if '/' in root_cate:
+            top_cate = root_cate.split('/')[0]
+        else:
+            top_cate = root_cate
+    else:
+        return None
+    return (itemid, top_cate)
+
+
+def re(a, b):
+    if a in itemid.value:
+        return valid_jsontxt(a)+'\001' +valid_jsontxt(b)
+    else:
+        return ''
+
+
+iteminfo = sc.textFile("/hive/warehouse/wl_base.db/t_base_ec_item_dev_new/ds=20161224").map(
+    lambda a: getItemAndCate(a)).filter(lambda a: a != None).map(lambda (a, b): re(a, b)).filter(
+    lambda a: a != '').saveAsTextFile("/user/lel/loss")
