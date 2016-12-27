6d54eedc8f7894fa9edc5bd6cf2d9e5fe6fd5dcd
lyen <lienlian@wolongdata.com>
2016-12-27 12:33:56 +0800

    xianyu

--- lel/spark/stuff/itemloss.py
+++ lel/spark/stuff/itemloss.py
@@ -13,8 +13,16 @@ def valid_jsontxt(content):
 
 data = sc.textFile("/user/lel/datas/lossitemid.csv").filter(lambda a: "enc_mobile" not in a).map(
     lambda a: a.split(',')[1])
+itemid_dict = sc.broadcast(data.collect())
 
-itemid = sc.broadcast(data.collect())
+
+
+
+
+# data = sc.textFile("/user/lel/datas/lossitemid.csv").filter(lambda a: "enc_mobile" not in a).map(
+#     lambda a: (a.split(',')[1], ""))
+#
+# itemid_dict = sc.broadcast(data.collectAsMap())
 
 
 def getItemAndCate(s):
@@ -31,11 +39,16 @@ def getItemAndCate(s):
     return (itemid, top_cate)
 
 
-itemids = itemid.value
+itemids = itemid_dict.value
 
 
-def re(a, b,itemids):
-    if a in itemids:
+# def re(a, b, itemids):
+#     if a in itemids:
+#         return valid_jsontxt(a) + '\001' + valid_jsontxt(b)
+#     else:
+#         return ''
+def re(a, b, itemids):
+    if itemids.has_key(a):
         return valid_jsontxt(a) + '\001' + valid_jsontxt(b)
     else:
         return ''
@@ -44,6 +57,5 @@ def re(a, b,itemids):
 iteminfo = sc.textFile("/hive/warehouse/wl_base.db/t_base_ec_item_dev_new/ds=20161224").map(
     lambda a: getItemAndCate(a)).filter(lambda a: a != None)
 
-iteminfo.map(lambda (a, b): re(a, b,itemids)).filter(
+iteminfo.map(lambda (a, b): re(a, b, itemids)).filter(
     lambda a: a != '').saveAsTextFile("/user/lel/loss")
-
