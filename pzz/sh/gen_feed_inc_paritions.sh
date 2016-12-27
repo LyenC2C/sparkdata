e2f9b8a8add380852c132b1d85aea2f994566e12
pengzhongzheng <pengzhongzheng@wolongdata.com>
2016-12-27 21:08:12 +0800

    about pzz

--- pzz/sh/gen_feed_inc_paritions.sh
+++ pzz/sh/gen_feed_inc_paritions.sh
@@ -7,7 +7,7 @@ uid_data_input=/data/develop/ec/tb/cmt/tmpdata/cmt_inc_data.uid.${end_date}
 hadoop fs -test -e ${uid_data_input}.partitions
 if [ $? -eq 0 ] ;then
     hadoop fs -rmr ${uid_data_input}.partitions
-if
+fi
 
 spark-submit  --master spark://cs220:7077 \
 --total-executor-cores  40 \
@@ -17,3 +17,4 @@ spark-submit  --master spark://cs220:7077 \
 ${workspace}/sparkdata/pzz/sh/partition_spark.jar \
 ${uid_data_input} \
 ${uid_data_input}.partitions
+
