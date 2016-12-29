08593d1005cef8f0351a69c13ae7aeb4c171679d
pengzhongzheng <pengzhongzheng@wolongdata.com>
2016-12-29 12:42:40 +0800

    about pzz

--- pzz/sh/gen_cmt_inc.sh
+++ pzz/sh/gen_cmt_inc.sh
@@ -2,8 +2,12 @@ source ~/.bashrc
 
 last_item_feedidls_date=$1
 last_uid_mark_date=$2
-mission_data=$3
+start_date=$3
 end_date=$4
+
+echo "last item feedid uid ls: "$last_item_feedidls_date
+echo -e "\nlast uid mark: "$last_uid_mark_date
+echo -e "\nmissiondata: "$mission_data
 
 spark-submit --master spark://cs100:7077 \
 --executor-memory 25g \
@@ -13,8 +17,11 @@ spark-submit --master spark://cs100:7077 \
 -gen_cmt_inc \
 /data/develop/ec/tb/cmt/itemid_feedid/itemid_feediduidls.${last_item_feedidls_date} \
 /data/develop/ec/tb/cmt/uid_mark/uid_mark_freq.json.${last_uid_mark_date} \
-${mission_data}/* \
+${start_date} \
+${end_date} \
 /data/develop/ec/tb/cmt/itemid_feedid/itemid_feediduidls.${end_date} \
 /data/develop/ec/tb/cmt/tmpdata/cmt_inc_data.uid.${end_date} \
 /data/develop/ec/tb/cmt/tmpdata.nouid/cmt_inc_data.nouid.${end_date} \
 /data/develop/ec/tb/cmt/user/user.${end_date}
+
+
