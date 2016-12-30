4bf778d736705f23decdaef4e7e8326e9451d3f4
pengzhongzheng <pengzhongzheng@wolongdata.com>
2016-12-30 11:04:39 +0800

    about pzz

--- pzz/sh/mv_step.sh
+++ pzz/sh/mv_step.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 inputdir=$2
-ds=`${inputdir##*/} | grep -E -o '[0-9]{8}'`
+ds=`echo ${inputdir##*/} | grep -E -o '[0-9]{8}'`
 filedate=$1
 table=$3
 ts=`date +%s`
 cmd="hadoop fs -mv ${inputdir}/$filedate  /hive/warehouse/wlbase_dev.db/$table/ds=$filedate/${filedate}_$ds_$ts"
 echo $cmd
-#$cmd
+$cmd
 #echo "-cp ${inputdir}/${1}_`date +%s`  /hive/warehouse/wlbase_dev.db/t_base_ec_item_feed_dev_test/ds=$1/"
 
 
