f89befd8d3cc9a4d21abd4b66d234b090d992c95
Gbacillus <gs464gan@gmail.com>
2017-01-03 11:42:08 +0800

    d

--- wrt/credit/feature_d_s_merge_online/feature_dense_sparse_online.py
+++ wrt/credit/feature_d_s_merge_online/feature_dense_sparse_online.py
@@ -42,7 +42,7 @@ def f_cat_reindex(line):
     features = ss[1].split()
     return [(valid_jsontxt(i.split(':')[0]),"") for i in features]
 
-def feature_cat(line,fea_cat_dict,len_main,feature5k):
+def feature_cat(line,fea_all_dict,feature5k):
     ss = line.strip().split("\001")
     tel = ss[0]
     features = ss[1].split()
@@ -54,7 +54,7 @@ def feature_cat(line,fea_cat_dict,len_main,feature5k):
         v_value = ff[1]
         if float(v_value) == 0.0: continue
         # if not feature5k.has_key(f_value): continue
-        f_index = fea_cat_dict[f_value] + len_main
+        f_index = fea_all_dict[f_value]
         if not feature5k.has_key(f_index): continue
         sort_dict[f_index] = round(float(v_value),2)
         # result.append(f_index + ":" + valid_jsontxt(v_value))
@@ -69,11 +69,13 @@ def tran_dict(fea_cat):
         fea_cat_dict[fea_cat[i]] = i
     return fea_cat_dict
 
-def fea_index(fea_all):
-    fea_all_index = []
+def fea_name(fea_all):
+    # fea_all_index = []
+    fea_all_dict = {}
     for i in range(len(fea_all)):
         fea_all_index.append(valid_jsontxt(fea_all[i]) + "\t" + str(i))
-    return fea_all_index
+        fea_all_dict[valid_jsontxt(fea_all[i])] = str(i)
+    return (fea_all_dict,fea_all_index)
 
 
 # def hebing(x,y,feature_5k):
@@ -99,14 +101,16 @@ def index_5k(x,fea_all):
     return (i,"")
 
 
-s_cat = "/hive/warehouse/wlcredit.db/t_wrt_credit_record_cate_feature_online/*" #稀疏
-s_main = "/hive/warehouse/wlcredit.db/t_credit_dense_features_online/*" #紧密
+# s_cat = "/hive/warehouse/wlcredit.db/t_wrt_credit_record_cate_feature_online/*" #稀疏
+s_cat = "/hive/warehouse/wlcredit.db/t_wrt_credit_record_cate_feature_online/000000_0"
+# s_main = "/hive/warehouse/wlcredit.db/t_credit_dense_features_online/*" #紧密
+s_main = "/hive/warehouse/wlcredit.db/t_credit_dense_features_online/000000_0"
 
 
 
 
 #紧密特征字段提取并自动排列好
-rdd_fea_main = hiveContext.sql('desc wlcredit.t_credit_dense_features')
+rdd_fea_main = hiveContext.sql('desc wlcredit.t_credit_dense_features_online')
 fea_main = [valid_jsontxt(ln.col_name) for ln in rdd_fea_main.collect()[1:]]
 # fea_main = ['buycnt','weibo_followers_count']
 #记录紧密特征长度，后面的在分配feature_cat的index时候用到
@@ -115,16 +119,17 @@ len_main = len(fea_main)
 rdd_fea_cat = sc.textFile(s_cat).flatMap(lambda x:f_cat_reindex(x)).groupByKey().mapValues(list).map(lambda (x, y): x)
 fea_cat = rdd_fea_cat.collect()
 #将稀疏特征字段变成map形式，以提高效率
-fea_cat_dict = tran_dict(fea_cat)
+# fea_cat_dict = tran_dict(fea_cat)
 #全部特征字段输出.
 fea_all = fea_main + fea_cat
-fea_all_index = fea_index(fea_all)
+fea_all_dict,fea_all_index = fea_name(fea_all)
 sc.parallelize(fea_all_index).saveAsTextFile('/user/wrt/temp/all_features_name')
 #将5k特征的index提取出来，用来后面过滤稀疏表与密集表的特征
 feature5k = sc.broadcast(sc.textFile("/user/wrt/feature_5k")\
     .map(lambda x:index_5k(x,fea_all)).collectAsMap()).value
 #每个电话按照新的稀疏特征顺序，将每个稀疏特征值依次输出，之前没有的特征用0代替
-rdd_cat = sc.textFile(s_cat).map(lambda x:feature_cat(x,fea_cat_dict,len_main,feature5k))
+# rdd_cat = sc.textFile(s_cat).map(lambda x:feature_cat(x,fea_cat_dict,len_main,feature5k))
+rdd_cat = sc.textFile(s_cat).map(lambda x:feature_cat(x,fea_all_dict,feature5k))
 # rdd_m = hiveContext.sql('select tel_index,buycnt,weibo_followers_count from wlbase_dev.t_base_user_profile_telindex')
 # rdd_main = rdd_m.map(lambda x:valid_jsontxt(x.tel_index) + "\001" + valid_jsontxt(x.buycnt) + "\001" \
 #                                             + valid_jsontxt(x.weibo_followers_count)).map(lambda x:feature_main(x))
