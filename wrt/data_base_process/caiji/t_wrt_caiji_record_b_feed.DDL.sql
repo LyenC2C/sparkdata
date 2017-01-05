8c5bbb20d9e78cf5abf720ee9eca3d5465b876e3
Gbacillus <gs464gan@gmail.com>
2017-01-05 10:16:52 +0800

    d

--- wrt/data_base_process/caiji/t_wrt_caiji_record_b_feed.DDL.sql
+++ wrt/data_base_process/caiji/t_wrt_caiji_record_b_feed.DDL.sql
@@ -5,3 +5,20 @@ sold string comment '销量(采集次序需要)'
 )
 COMMENT '采集天猫评论所需的商品id'
 PARTITIONED BY  (ds STRING );
+
+insert overwrite table wl_analysis.t_wrt_caiji_record_b_feed partition (ds = '20170104')
+select item_id,new_ds,sold from
+(
+select
+t1.item_id,
+case when t2.new_ds is null then "19760101" else t2.new_ds end as new_ds,
+t1.sold
+from
+(select item_id,sold from wlbase_dev.t_base_ec_shopitem_b where ds = 20170103)t1
+left join
+(select item_id,max(dsn) as new_ds from wl_base.t_base_ec_record_dev_new where ds = 'true' and bc_type = 'B'
+group by item_id)t2
+on
+t1.item_id = t2.item_id
+)t
+order by sold desc;
