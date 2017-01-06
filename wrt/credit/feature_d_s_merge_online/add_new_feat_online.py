53391561009a42efd1d29acb939c7fc05267f4ae
Gbacillus <gs464gan@gmail.com>
2017-01-06 11:13:28 +0800

    d

--- wrt/credit/feature_d_s_merge_online/add_new_feat_online.py
+++ wrt/credit/feature_d_s_merge_online/add_new_feat_online.py
@@ -8,6 +8,7 @@ from pyspark import SparkContext
 from pyspark.sql import *
 
 today = sys.argv[1]
+today_name = sys.argv[2]
 
 sc = SparkContext(appName="add_new_feat")
 sqlContext = SQLContext(sc)
@@ -22,7 +23,7 @@ def valid_jsontxt(content):
         res = str(content)
     return res.replace('\n',"").replace("\r","").replace('\001',"").replace("\u0001","")
 
-def need_index(feature5k):
+def add_index(feature5k):
     numerator_price = {}
     numerator_count = {}
     newfeature = copy.copy(feature5k)
@@ -33,10 +34,12 @@ def need_index(feature5k):
         ln = valid_jsontxt(feature5k[i])
         if ("sum_price_level" in ln) or ("price_sum" in ln):
             numerator_price[str(i)] = str(add_index)
+            ln += "_ratio"
             newfeature.append(ln)
             add_index += 1
         elif ("count_level" in ln) or ("buy_count" in ln):
             numerator_count[str(i)] = str(add_index)
+            ln += "_ratio"
             newfeature.append(ln)
             add_index += 1
         # if ("level" in ln) or ("buy_count" in ln) or ("price_sum" in ln):
@@ -57,11 +60,11 @@ def f(line,numerator_price,numerator_count):
     ss = line.strip().split('\001')
     features = ss[1].split()
     result = copy.copy(features)
-    buy_count = features[1]
-    buy_price = features[2]
+    buy_count = float(features[1].split(":")[1])
+    buy_price = float(features[2].split(":")[1])
     for ln in features:
         f_index = ln.split(":")[0]
-        f_value = ln.split(":")[1]
+        f_value = float(ln.split(":")[1])
         if numerator_price.has_key(f_index):
             new_index = numerator_price[f_index]
             new_value = str(round((f_value / buy_price),2))
@@ -69,8 +72,8 @@ def f(line,numerator_price,numerator_count):
         elif numerator_count.has_key(f_index):
             new_index = numerator_count[f_index]
             new_value = round((f_value / buy_count),2)
-            result.append(new_index + ":" + new_value)
-    return ss[0] + "\001" + " ".join(result)
+            result.append(valid_jsontxt(new_index) + ":" + valid_jsontxt(new_value))
+    return valid_jsontxt(ss[0]) + "\001" + " ".join(result)
 
 
 rdd = sc.textFile("/hive/warehouse/wlcredit.db/t_credit_feature_merge/ds=20170105_online")
@@ -79,9 +82,18 @@ feature5k = sc.textFile("/user/wrt/feature_5k").collect()
 numerator_price,numerator_count,newfeature = add_index(feature5k)
 fea_all_index = fea_index(newfeature)
 sc.parallelize(fea_all_index).saveAsTextFile('/user/wrt/temp/add_new_feature_name')
-rdd.map(lambda x:f(x)).saveAsTextFile('/user/wrt/temp/add_newfeature_inhive')
+rdd.map(lambda x:f(x,numerator_price,numerator_count)).saveAsTextFile('/user/wrt/temp/add_newfeature_inhive')
 
+hiveContext.sql('load data inpath "/user/wrt/temp/add_new_feature_name" overwrite into table \
+wlcredit.t_wrt_credit_all_features_name_online PARTITION (ds ='+ today +')')
+
+hiveContext.sql('LOAD DATA INPATH "/user/wrt/temp/add_newfeature_inhive" OVERWRITE \
+INTO TABLE wlcredit.t_credit_feature_merge PARTITION (ds = '+ today_name +')')
+
+
+# hfs -rmr /user/wrt/temp/add_new_feature_name && hfs -rmr /user/wrt/temp/add_newfeature_inhive
 # spark-submit --executor-memory 9G  --driver-memory 9G  --total-executor-cores 120
+
 
 
 
