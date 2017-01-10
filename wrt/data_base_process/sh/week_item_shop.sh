130d547ccbd622500d3a1aa12897f7f1d0888820
Gbacillus <gs464gan@gmail.com>
2017-01-10 15:38:41 +0800

    d

--- wrt/data_base_process/sh/week_item_shop.sh
+++ wrt/data_base_process/sh/week_item_shop.sh
@@ -2,10 +2,8 @@
 
 pre_path='/home/wrt/sparkdata/wrt'
 
-#today=$(date -d '0 days ago' +%Y%m%d)
-#lastday=$(date -d '7 days ago' +%Y%m%d)
+today=$(date -d '0 days ago' +%Y%m%d)
+lastday=$(date -d '7 days ago' +%Y%m%d)
 
-today='20170110'
-lastday='20170106'
 sh $pre_path/data_base_process/sh/item_shop.sh $today $lastday &> \
 $pre_path/data_base_process/sh/log_itemshop/log_$today
