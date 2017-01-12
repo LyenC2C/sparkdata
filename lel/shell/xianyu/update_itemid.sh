9793a0e5906b5220a333cb5b94ed40f68a2c5f54
lyen <lienlian@wolongdata.com>
2017-01-12 09:53:40 +0800

    xianyu

--- lel/shell/xianyu/update_itemid.sh
+++ lel/shell/xianyu/update_itemid.sh
@@ -9,6 +9,7 @@ last_7_days=$(date -d '7 days ago' +%Y%m%d)
 
 hive <<EOF
 use wlbase_dev;
+set hive.merge.mapredfiles = true;
 insert OVERWRITE table wlbase_dev.t_base_ec_xianyu_itemid_update PARTITION(ds = $today)
 select t1.itemid as itemid from
 (select itemid,commentnum from wlbase_dev.t_base_ec_xianyu_iteminfo where ds= $lastday and commentnum > 0 ) t1
