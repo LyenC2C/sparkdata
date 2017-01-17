0ded58ab83adb9e01524079732546567cdceba57
Gbacillus <gs464gan@gmail.com>
2017-01-17 20:39:14 +0800

    d

--- zlj/project/task/credit_model/feature/online/run.sh
+++ zlj/project/task/credit_model/feature/online/run.sh
@@ -1,11 +1,11 @@
 
-#hive -f t_credit_record_cate1_feature_online.sql
-#hive -f t_credit_record_cate2_feature_online.sql
-#hive -f t_credit_record_cate3_feature_online.sql
-#hive -f t_credit_record_cate4_feature_online.sql
-#
-#hive -f t_credit_record_feature_online.sql
-#hive -f t_credit_record_feature_1month_online.sql
-#hive -f t_credit_record_feature_3month_online.sql
+hive -f t_credit_record_cate1_feature_online.sql
+hive -f t_credit_record_cate2_feature_online.sql
+hive -f t_credit_record_cate3_feature_online.sql
+hive -f t_credit_record_cate4_feature_online.sql
+
+hive -f t_credit_record_feature_online.sql
+hive -f t_credit_record_feature_1month_online.sql
+hive -f t_credit_record_feature_3month_online.sql
 hive -f t_credit_record_feature_6month_online.sql
 hive -f t_credit_record_feature_12month_online.sql
