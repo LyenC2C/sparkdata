e9c6d10520178b8c620fb9c22e3b04ba0a463563
lyen <lienlian@wolongdata.com>
2017-01-18 13:40:46 +0800

    xianyu

--- lel/airflow/xianyu/source/xianyu_itemcomment_import.sh
+++ lel/airflow/xianyu/source/xianyu_itemcomment_import.sh
@@ -15,17 +15,17 @@ use wlbase_dev;
 LOAD DATA  INPATH '/user/lel/temp/xianyu_itemcomment' OVERWRITE INTO TABLE $table PARTITION (ds='0000tmp');
 insert OVERWRITE table $table PARTITION(ds = $lastday)
 select
-case when t1.itemid is null then t2.itemid else t1.itemid end,
-case when t1.itemid is null then t2.commentId else t1.commentId end,
-case when t1.itemid is null then t2.content else t1.content end,
-case when t1.itemid is null then t2.reportTime else t1.reportTime end,
-case when t1.itemid is null then t2.reporterName else t1.reporterName end,
-case when t1.itemid is null then t2.reporterNick else t1.reporterNick end,
-case when t1.itemid is null then t2.ts else t1.ts end
+case when t1.commentid is null then t2.itemid else t1.itemid end,
+case when t1.commentid is null then t2.commentid else t1.commentId end,
+case when t1.commentid is null then t2.content else t1.content end,
+case when t1.commentid is null then t2.reportTime else t1.reportTime end,
+case when t1.commentid is null then t2.reporterName else t1.reporterName end,
+case when t1.commentid is null then t2.reporterNick else t1.reporterNick end,
+case when t1.commentid is null then t2.ts else t1.ts end
 from
 (select * from  $table where ds = '0000tmp')t1
 full outer JOIN
 (select * from $table where ds = $last_2_days)t2
 ON
-t1.itemid = t2.itemid;
+t1.commentId = t2.commentId;
 EOF
