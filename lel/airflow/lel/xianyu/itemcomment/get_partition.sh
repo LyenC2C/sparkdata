0636ea2e57174e26ddbbcdd53d7364e73fcede65
lyen <lienlian@wolongdata.com>
2017-01-19 11:15:38 +0800

    xianyu

--- lel/airflow/lel/xianyu/itemcomment/get_partition.sh
+++ lel/airflow/lel/xianyu/itemcomment/get_partition.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-hadoop fs -ls /hive/warehouse/wlbase_dev.db/t_base_ec_xianyu_itemcomment | awk -F '=' '{print $2}' | sort -r |awk 'NR==1{print $0}'
+date=`hadoop fs -ls /hive/warehouse/wlbase_dev.db/t_base_ec_xianyu_itemcomment | awk -F '=' '{print $2}' | sort -r |awk 'NR==1{print $0}'`
+echo $date
