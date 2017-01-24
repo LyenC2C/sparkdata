9b19414bd4662bae390340ce8a29215cbcfa491a
Gbacillus <gs464gan@gmail.com>
2017-01-24 14:29:10 +0800

    s

--- wrt/data_base_process/itemsearch/t_base_item_search.py
+++ wrt/data_base_process/itemsearch/t_base_item_search.py
@@ -38,8 +38,11 @@ def f(line):
 
 # rdd = rdd1.union(rdd2)
 rdd = sc.textFile("/commit/itemsearch/*")
-rdd.map(lambda x:f(x)).filter(lambda x:x!=None).saveAsTextFile("/user/wrt/temp/itemsearch_else")
-# hfs -rmr /user/wrt/temp/itemsearch_20_30
+rdd.map(lambda x:f(x)).filter(lambda x:x!=None).saveAsTextFile("/user/wrt/temp/itemsearch")
+hiveContext.sql('load data inpath "/user/wrt/temp/itemsearch" overwrite into table \
+wl_base.t_base_item_search PARTITION (ds ="0temp")')
+
+# hfs -rmr /user/wrt/temp/itemsearch
 # spark-submit  --executor-memory 8G  --driver-memory 8G  --total-executor-cores 120 t_wrt_item_search.py
 # load data inpath "/user/wrt/temp/itemsearch_20_30" overwrite into table wlservice.t_wrt_item_search
 # partition (ds = '20170104_20_30')
