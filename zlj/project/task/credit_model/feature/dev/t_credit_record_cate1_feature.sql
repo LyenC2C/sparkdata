7973ca5606f7dfe4591ba384d1ca942c18e82f9a
Gbacillus <gs464gan@gmail.com>
2017-01-24 09:53:17 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	zlj/project/task/credit_model/feature/dev/t_credit_record_cate1_feature.sql

--- zlj/project/task/credit_model/feature/dev/t_credit_record_cate1_feature.sql
+++ zlj/project/task/credit_model/feature/dev/t_credit_record_cate1_feature.sql
@@ -29,7 +29,6 @@ concat_ws(':', concat_ws('_',cast( root_cat_id as string) ,'min_price_level1' ) 
 concat_ws(':', concat_ws('_',cast( root_cat_id as string) ,'std_price_level1' ) ,cast( round(std(price),2) as string) ) price_std,
 concat_ws(':', concat_ws('_',cast( root_cat_id as string) ,'median_price_level1') ,cast( round(percentile(cast(price as int),0.5),2) as string) ) price_median,
 concat_ws(':', concat_ws('_',cast( root_cat_id as string) ,'cross_price_level1' ) ,cast(round(max(price)-min(price),2) as string) ) price_cross ,
-
 concat_ws(':', concat_ws('_',cast( root_cat_id as string) ,'price_025_level1') ,cast( round(percentile(cast(price as int),0.25),2) as string) ) price_025,
 concat_ws(':', concat_ws('_',cast( root_cat_id as string) ,'price_010_level1') ,cast( round(percentile(cast(price as int),0.10),2) as string) ) price_010,
 concat_ws(':', concat_ws('_',cast( root_cat_id as string) ,'price_075_level1') ,cast( round(percentile(cast(price as int),0.75),2) as string) ) price_075
