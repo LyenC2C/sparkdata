bc0d2b7daaeb834fcf85c5044ca4cf79f761b8e1
lyen <lienlian@wolongdata.com>
2017-02-04 11:39:41 +0800

    xianyu

--- lel/airflow/wrt/ec/tb/ec_shopitem_b_backfill.py
+++ lel/airflow/wrt/ec/tb/ec_shopitem_b_backfill.py
@@ -11,5 +11,5 @@ for i in exec_days:
     exec_day = (today + datetime.timedelta(days=-i)).strftime('%Y%m%d')
     last_day = (today + datetime.timedelta(days=-i-1)).strftime('%Y%m%d')
     last_2_days =  (today + datetime.timedelta(days=-i-2)).strftime('%Y%m%d')
-    command = "ssh -p 22 wrt@10.3.4.220 bash ec_shopitem_b_backfill.sh {last_day} {last_2_days}".format(last_day=last_day,last_2_days=last_2_days)
+    command = "ssh -p 22 wrt@10.3.4.220 bash /home/lel/wolong/sparkdata/lel/airflow/wrt/ec/tb/ec_shopitem_b_backfill.sh {last_day} {last_2_days}".format(last_day=last_day,last_2_days=last_2_days)
     os.system(command)
