88ee509680e4e7cecfe944c6f09ab3206bdadd87
lyen <lienlian@wolongdata.com>
2017-02-05 09:08:36 +0800

    xianyu

--- lel/airflow/wrt/ec/tb/ec_itemsold_backfill.py
+++ lel/airflow/wrt/ec/tb/ec_itemsold_backfill.py
@@ -9,10 +9,9 @@ iteminfo_date = 20170128
 
 for i in exec_days:
     today = datetime.datetime.now()
-    exec_day = (today + datetime.timedelta(days=-i)).strftime('%Y%m%d')
-    last_day = (today + datetime.timedelta(days=-i-1)).strftime('%Y%m%d')
-    last_2_days =  (today + datetime.timedelta(days=-i-2)).strftime('%Y%m%d')
+    exec_day = (today + datetime.timedelta(days=-i-1)).strftime('%Y%m%d')
+    last_day = (today + datetime.timedelta(days=-i-2)).strftime('%Y%m%d')
+    last_2_days =  (today + datetime.timedelta(days=-i-3)).strftime('%Y%m%d')
     # print exec_day,last_day,last_2_days
     command = "bash /home/wrt/sparkdata/lel/airflow/wrt/ec/tb/ec_itemsold_backfill.sh  {last_day} {last_2_days} {iteminfo_date}".format(last_day=last_day,last_2_days=last_2_days,iteminfo_date=iteminfo_date)
     os.system(command)
-
