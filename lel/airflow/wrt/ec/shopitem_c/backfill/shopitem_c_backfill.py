98f86bbca4ddaea3205e1c519b49e51e279bfdb3
lyen <lienlian@wolongdata.com>
2017-02-07 13:53:03 +0800

    xianyu

--- lel/airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.py
+++ lel/airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.py
@@ -6,12 +6,11 @@ import os
 exec_days = list(xrange(1, 3))
 exec_days.reverse()
 
-for i in exec_days:
-    today = datetime.datetime.now()
-    exec_day = (today + datetime.timedelta(days=-i)).strftime('%Y%m%d')
-    last_day = (today + datetime.timedelta(days=-i-1)).strftime('%Y%m%d')
-    last_2_days =  (today + datetime.timedelta(days=-i-2)).strftime('%Y%m%d')
-    # print exec_day,last_day,last_2_days
-    command = "bash /home/wrt/sparkdata/lel/airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.sh {last_day} {last_2_days}".format(
-        last_day=last_day, last_2_days=last_2_days)
-    os.system(command)
+# for i in exec_days:
+#     today = datetime.datetime.now()
+#     exec_day = (today + datetime.timedelta(days=-i)).strftime('%Y%m%d')
+#     last_day = (today + datetime.timedelta(days=-i-1)).strftime('%Y%m%d')
+#     last_2_days =  (today + datetime.timedelta(days=-i-2)).strftime('%Y%m%d')
+#     # print exec_day,last_day,last_2_days
+command = "bash /home/wrt/sparkdata/lel/airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.sh {last_day} {last_2_days}".format(last_day=20170204, last_2_days=20170202)
+os.system(command)
