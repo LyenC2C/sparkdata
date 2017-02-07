1ca140cff5b774b26f5af775d08fea40dce2b11a
lyen <lienlian@wolongdata.com>
2017-02-07 11:47:40 +0800

    xianyu

---
+++ lel/airflow/wrt/ec/itemsold/ec_itemsold_sale_import.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+source ~/.bashrc
+
+#zuotian=$(date -d '1 days ago' +%Y%m%d)
+
+hive<<EOF
+use wlbase_dev;
+LOAD DATA  INPATH '/user/wrt/sale_tmp' OVERWRITE INTO TABLE t_base_ec_item_sold_dev PARTITION (ds=$1);
+EOF
+
+
