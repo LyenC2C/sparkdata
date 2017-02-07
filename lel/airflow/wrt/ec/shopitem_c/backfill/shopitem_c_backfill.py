d31670628a503baa81478cac54eb1b7aaf132012
lyen <lienlian@wolongdata.com>
2017-02-07 13:59:30 +0800

    xianyu

--- lel/airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.py
+++ lel/airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.py
@@ -12,6 +12,6 @@ exec_days.reverse()
 #     last_day = (today + datetime.timedelta(days=-i-1)).strftime('%Y%m%d')
 #     last_2_days =  (today + datetime.timedelta(days=-i-2)).strftime('%Y%m%d')
 #     # print exec_day,last_day,last_2_days
-command = "bash /home/wrt/sparkdata/lel/airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.sh {last_day} {last_2_days}".format(last_day=20170204, last_2_days=20170202)
+command = "bash /home/wrt/sparkdata/lel/airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.sh {last_day} {last_2_days}".format(last_day=20170205, last_2_days=20170204)
 os.system(command)
 
