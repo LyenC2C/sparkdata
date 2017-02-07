ea5022c99e256747fc87fbf9bf3b2281ca4eaa1b
lyen <lienlian@wolongdata.com>
2017-02-07 10:37:49 +0800

    xianyu

---
+++ lel/airflow/wrt/ec/shopitem_b/check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+date=$1
+hadoop fs -test -e /commit/2taobao/leave_comment/*$date*
+if [ $? -eq 0 ] ;then
+        size=`hadoop fs -count -q /commit/shopitem_b/*$date* | awk '{print $7}'`
+        if [ $size -gt 0 ];then
+        #目录存在且容量大于0
+        echo  '1'
+        else
+        #目录存在且容量小于0
+        echo  '0'
+        fi
+else
+    #目录不存在
+    echo  '0'
+fi
