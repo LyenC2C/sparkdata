33dc3cc2e59088079a692a046b5eb63e2c95cbc7
lyen <lienlian@wolongdata.com>
2017-02-07 12:47:46 +0800

    xianyu

--- lel/airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.py
+++ lel/airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.py
@@ -3,14 +3,15 @@ import sys
 import os
 
 # sys.argv[1]
-exec_days = list(xrange(3, 39))
+exec_days = list(xrange(1, 4))
 exec_days.reverse()
 
 for i in exec_days:
     today = datetime.datetime.now()
-    last_day = (today + datetime.timedelta(days=-i)).strftime('%Y%m%d')
-    last_2_days = (today + datetime.timedelta(days=-i - 1)).strftime('%Y%m%d')
-    # print last_day,last_2_days
+    exec_day = (today + datetime.timedelta(days=-i)).strftime('%Y%m%d')
+    last_day = (today + datetime.timedelta(days=-i-1)).strftime('%Y%m%d')
+    last_2_days =  (today + datetime.timedelta(days=-i-2)).strftime('%Y%m%d')
+    # print exec_day,last_day,last_2_days
     command = "bash /home/wrt/sparkdata/lel/airflow/wrt/backfill/shopitem_c_backfill.sh {last_day} {last_2_days}".format(
         last_day=last_day, last_2_days=last_2_days)
     os.system(command)
