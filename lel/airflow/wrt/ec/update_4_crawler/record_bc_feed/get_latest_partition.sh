62cf8c4d02e4b108b09bf5a307d0320749e6000b
lyen <lienlian@wolongdata.com>
2017-02-08 10:32:57 +0800

    xianyu

--- lel/airflow/wrt/ec/update_4_crawler/record_bc_feed/get_latest_partition.sh
+++ lel/airflow/wrt/ec/update_4_crawler/record_bc_feed/get_latest_partition.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 date_b=`hadoop fs -ls /hive/warehouse/wlbase_dev.db/t_base_ec_shopitem_b | awk -F '=' '{print $2}' | sort -r |awk 'NR==1{print $0}'`
 date_c=`hadoop fs -ls /hive/warehouse/wlbase_dev.db/t_base_ec_shopitem_c | awk -F '=' '{print $2}' | sort -r |awk 'NR==1{print $0}'`
-echo $date_b+"\t"+$date_c
+echo $date_b    $date_c
