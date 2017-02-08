73c9e0a34c95d5be64977d062d6ef3bebfc84cf1
lyen <lienlian@wolongdata.com>
2017-02-08 10:46:42 +0800

    xianyu

--- lel/airflow/wrt/ec/update_4_crawler/airflows/record_bc_feed.py
+++ lel/airflow/wrt/ec/update_4_crawler/airflows/record_bc_feed.py
@@ -17,24 +17,21 @@ sys.setdefaultencoding('utf-8')
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2017, 2, 7, 4, 55),
-    'email': ['lienlian@wolongdata.com'],
+    'start_date': datetime(2017, 2, 8),
+    'email': ['airflow_airflow@163.com'],
     'email_on_failure': True,
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    # 'end_date': datetime(2016, 12, 21),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
 }
 
-dag = DAG('record_bc_feed', default_args=default_args, schedule_interval='0 5 * * *')
+dag = DAG('record_bc_feed', default_args=default_args, schedule_interval='None')
 sshHook = SSHHook(conn_id="cs220_wrt")
 path = Variable.get('cs220_update_4_crawler')
 
-check_partition_b_cmd = "ssh -p 22 wrt@cs220 bash {path}/get_latest_b_partition.sh".format(path=path)
-check_partition_c_cmd = "ssh -p 22 wrt@cs220 bash {path}/get_latest_c_partition.sh".format(path=path)
+check_partition_b_cmd = "ssh -p 22 wrt@cs220 bash {path}/record_b_feed/get_latest_b_partition.sh".format(path=path)
+check_partition_c_cmd = "ssh -p 22 wrt@cs220 bash {path}/record_b_feed/get_latest_c_partition.sh".format(path=path)
+
 
 def get_last_update_b_date():
     try:
@@ -44,7 +41,8 @@ def get_last_update_b_date():
     else:
         return str(eval(result))
 
-def get_last_update_b_date():
+
+def get_last_update_c_date():
     try:
         result = os.popen(check_partition_c_cmd, "r").readline()
     except:
@@ -53,18 +51,18 @@ def get_last_update_b_date():
         return str(eval(result))
 
 
+b = SSHExecuteOperator(
+    task_id="update_b",
+    bash_command='(bash {path}/record_b_feed/record_b_feed.sh {latest_partition})'.format(path=path,
+                                                                            latest_partition=get_last_update_b_date()),
+    ssh_hook=sshHook,
+    dag=dag)
 
-# b = SSHExecuteOperator(
-#     task_id="update_b",
-#     bash_command='(bash {path}/record_b_feed.sh {latest_partition})'.format(path=path,
-#                                                                             latest_partition=get_last_update_date()[0]),
-#     ssh_hook=sshHook,
-#     dag=dag)
-#
-# c = SSHExecuteOperator(
-#     task_id="update_c",
-#     bash_command='(bash {path}/record_b_feed.sh {latest_partition})'.format(path=path, lastday=get_last_update_date()[1]),
-#     ssh_hook=sshHook,
-#     dag=dag)
-#
-# chain(b, c)
+c = SSHExecuteOperator(
+    task_id="update_c",
+    bash_command='(bash {path}/record_b_feed/record_b_feed.sh {latest_partition})'.format(path=path,
+                                                                            lastday=get_last_update_c_date()),
+    ssh_hook=sshHook,
+    dag=dag)
+
+chain(b, c)
