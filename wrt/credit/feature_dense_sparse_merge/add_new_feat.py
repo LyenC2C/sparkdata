bcfe237bec2e0ad934abd7e4a49d76ecabf979b3
Gbacillus <gs464gan@gmail.com>
2017-02-13 11:09:21 +0800

    s

--- wrt/credit/feature_dense_sparse_merge/add_new_feat.py
+++ wrt/credit/feature_dense_sparse_merge/add_new_feat.py
@@ -76,8 +76,8 @@ def f(line,numerator_price,numerator_count):
     return valid_jsontxt(ss[0]) + "\001" + " ".join(result)
 
 
-rdd = sc.textFile("/hive/warehouse/wlcredit.db/t_credit_feature_merge/ds=20170113_cms")
-feature_raw = sc.textFile("/hive/warehouse/wlcredit.db/t_wrt_credit_all_features_name/ds=20170113_cms")\
+rdd = sc.textFile("/hive/warehouse/wlcredit.db/t_credit_feature_merge/ds=20170210_cms1234")
+feature_raw = sc.textFile("/hive/warehouse/wlcredit.db/t_wrt_credit_all_features_name/ds=20170210_cms1234")\
     .map(lambda x:valid_jsontxt(x.split("\t")[0])).collect()
 # feature5k = sc.textFile("/user/wrt/feature_5k").collect()
 #提取原始特征中会用到的特征
@@ -86,13 +86,13 @@ fea_all_index = fea_index(newfeature)
 sc.parallelize(fea_all_index).saveAsTextFile('/user/wrt/temp/add_new_feature_name')
 rdd.map(lambda x:f(x,numerator_price,numerator_count)).saveAsTextFile('/user/wrt/temp/add_newfeature_inhive')
 
-hiveContext.sql('load data inpath "/user/wrt/temp/add_new_feature_name" overwrite into table \
-wlcredit.t_wrt_credit_all_features_name PARTITION (ds = "20170113_cms_anf" )')
+hiveContext.sql("load data inpath '/user/wrt/temp/add_new_feature_name' overwrite into table \
+wlcredit.t_wrt_credit_all_features_name PARTITION (ds = '"+  today +"')" )
 
-hiveContext.sql('LOAD DATA INPATH "/user/wrt/temp/add_newfeature_inhive" OVERWRITE \
-INTO TABLE wlcredit.t_credit_feature_merge PARTITION (ds = "20170113_cms_anf")')
+hiveContext.sql("LOAD DATA INPATH '/user/wrt/temp/add_newfeature_inhive' OVERWRITE \
+INTO TABLE wlcredit.t_credit_feature_merge PARTITION (ds = '"+  today +"')" )
 
-
+# cms代表cate_month_cross anf代表add_new_feature
 # hfs -rmr /user/wrt/temp/add_new_feature_name && hfs -rmr /user/wrt/temp/add_newfeature_inhive
 # spark-submit --executor-memory 9G  --driver-memory 9G  --total-executor-cores 120 add_new_feat_online.py 20170106
 
