5e53f99705970729e763d233c69de8f3e0eec856
Gbacillus <gs464gan@gmail.com>
2017-02-16 16:57:26 +0800

    s

--- zlj/project/task/credit_model/feature/dev/run.sh
+++ zlj/project/task/credit_model/feature/dev/run.sh
@@ -1,8 +1,8 @@
 
-#hive -f t_credit_record_cate1_feature.sql
-#hive -f t_credit_record_cate2_feature.sql
-hive -f t_credit_record_cate3_feature.sql
-hive -f t_credit_record_cate4_feature.sql
+hive -f t_credit_record_cate1_feature_months.sql
+hive -f t_credit_record_cate2_feature_months.sql
+hive -f t_credit_record_cate3_feature_months.sql
+hive -f t_credit_record_cate4_feature_months.sql
 
 hive -f t_credit_record_feature.sql
 hive -f t_credit_record_feature_1month.sql
