7e4f124392231b596289f3268deec82a19f97ab7
Gbacillus <gs464gan@gmail.com>
2017-02-22 09:13:59 +0800

    s

--- wrt/credit/sh/feature_engine.sh
+++ wrt/credit/sh/feature_engine.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
-
-zlj_path='/home/wrt/sparkdata/zlj/project/task/credit_model/feature'
-wrt_path='/home/wrt/sparkdata/wrt/credit/feature_dense_sparse_merge'
-today=$(date -d '0 days ago' +%Y%m%d)
-
-hive -f $zlj_path/cate_month_cross/t_credit_record_cate1_feature_months.sql
-hive -f $zlj_path/cate_month_cross/t_credit_record_cate2_feature_months.sql
-hive -f $zlj_path/cate_month_cross/t_credit_record_cate3_feature_months.sql
-hive -f $zlj_path/cate_month_cross/t_credit_record_cate4_feature_months.sql
-
-hive -f $zlj_path/dev/t_credit_record_feature.sql
-hive -f $zlj_path/dev/t_credit_record_feature_1month.sql
-hive -f $zlj_path/dev/t_credit_record_feature_3month.sql
-hive -f $zlj_path/dev/t_credit_record_feature_6month.sql
-hive -f $zlj_path/dev/t_credit_record_feature_12month.sql
-
-hive -f $wrt_path/feature_dense_merge.sql
-hive -f $wrt_path/feature_sparse_merge.sql
+source ~/.bashrc
+#zlj_path='/home/wrt/sparkdata/zlj/project/task/credit_model/feature'
+#wrt_path='/home/wrt/sparkdata/wrt/credit/feature_dense_sparse_merge'
+#today=$(date -d '0: days ago' +%Y%m%d)
+#
+#hive -f $zlj_path/cate_month_cross/t_credit_record_cate1_feature_months.sql
+#hive -f $zlj_path/cate_month_cross/t_credit_record_cate2_feature_months.sql
+#hive -f $zlj_path/cate_month_cross/t_credit_record_cate3_feature_months.sql
+#hive -f $zlj_path/cate_month_cross/t_credit_record_cate4_feature_months.sql
+#
+#hive -f $zlj_path/dev/t_credit_record_feature.sql
+#hive -f $zlj_path/dev/t_credit_record_feature_1month.sql
+#hive -f $zlj_path/dev/t_credit_record_feature_3month.sql
+#hive -f $zlj_path/dev/t_credit_record_feature_6month.sql
+#hive -f $zlj_path/dev/t_credit_record_feature_12month.sql
+#
+#hive -f $wrt_path/feature_dense_merge.sql
+#hive -f $wrt_path/feature_sparse_merge.sql
 
 hfs -rmr /user/wrt/temp/all_features_name
 hfs -rmr /user/wrt/temp/all_feature_dense_sparse_inhive
