4eceb8029fa04122f3ed14673abf18bbe06b6727
Gbacillus <gs464gan@gmail.com>
2017-02-22 18:55:23 +0800

    s

--- wrt/credit/feature_dense_sparse_merge/add_new_feat.py
+++ wrt/credit/feature_dense_sparse_merge/add_new_feat.py
@@ -81,7 +81,7 @@ rdd = sc.textFile("/hive/warehouse/wlcredit.db/t_credit_feature_merge/ds=" + tod
 feature_raw = sc.textFile("/hive/warehouse/wlcredit.db/t_wrt_credit_all_features_name/ds=" + today + "_cms1234")\
     .map(lambda x:valid_jsontxt(x.split("\t")[0])).collect()
 #统计原始特征数量
-add_index = feature_raw.count()
+add_index = len(feature_raw)
 add_index += 1
 #提取原始特征中会用到的特征
 numerator_price,numerator_count,newfeature = add_index(feature_raw,add_index)
