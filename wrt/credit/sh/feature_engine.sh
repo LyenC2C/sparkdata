dde9da2610f9a15360c3300aa70b7f55846c613b
Gbacillus <gs464gan@gmail.com>
2017-02-22 22:06:10 +0800

    s

--- wrt/credit/sh/feature_engine.sh
+++ wrt/credit/sh/feature_engine.sh
@@ -3,33 +3,33 @@ source ~/.bashrc
 zlj_path='/home/wrt/sparkdata/zlj/project/task/credit_model/feature'
 wrt_path='/home/wrt/sparkdata/wrt/credit/feature_dense_sparse_merge'
 today=$(date -d '0 days ago' +%Y%m%d)
-#today='20170114'
-#
-#hive -f $zlj_path/cate_month_cross/t_credit_record_cate1_feature_months.sql
-#hive -f $zlj_path/cate_month_cross/t_credit_record_cate2_feature_months.sql
-#hive -f $zlj_path/cate_month_cross/t_credit_record_cate3_feature_months.sql
-#hive -f $zlj_path/cate_month_cross/t_credit_record_cate4_feature_months.sql
-#
-#hive -f $zlj_path/dev/t_credit_record_feature.sql
-#hive -f $zlj_path/dev/t_credit_record_feature_1month.sql
-#hive -f $zlj_path/dev/t_credit_record_feature_3month.sql
-#hive -f $zlj_path/dev/t_credit_record_feature_6month.sql
-#hive -f $zlj_path/dev/t_credit_record_feature_12month.sql
-#
-#hive -f $wrt_path/feature_dense_merge.sql
-#hive -f $wrt_path/feature_sparse_merge.sql
+today='20170114'
 
-#hfs -rmr /user/wrt/temp/all_features_name
-#hfs -rmr /user/wrt/temp/all_feature_dense_sparse_inhive
-#
-#spark-submit  --executor-memory 9G  --driver-memory 9G  --total-executor-cores 120 \
-#$wrt_path/feature_dense_sparse.py $today
+hive -f $zlj_path/cate_month_cross/t_credit_record_cate1_feature_months.sql
+hive -f $zlj_path/cate_month_cross/t_credit_record_cate2_feature_months.sql
+hive -f $zlj_path/cate_month_cross/t_credit_record_cate3_feature_months.sql
+hive -f $zlj_path/cate_month_cross/t_credit_record_cate4_feature_months.sql
 
-#hfs -rmr /user/wrt/temp/add_new_feature_name
-#hfs -rmr /user/wrt/temp/add_newfeature_inhive
-#
-#spark-submit  --executor-memory 9G  --driver-memory 9G  --total-executor-cores 120 \
-#$wrt_path/add_new_feat.py $today
+hive -f $zlj_path/dev/t_credit_record_feature.sql
+hive -f $zlj_path/dev/t_credit_record_feature_1month.sql
+hive -f $zlj_path/dev/t_credit_record_feature_3month.sql
+hive -f $zlj_path/dev/t_credit_record_feature_6month.sql
+hive -f $zlj_path/dev/t_credit_record_feature_12month.sql
+
+hive -f $wrt_path/feature_dense_merge.sql
+hive -f $wrt_path/feature_sparse_merge.sql
+
+hfs -rmr /user/wrt/temp/all_features_name
+hfs -rmr /user/wrt/temp/all_feature_dense_sparse_inhive
+
+spark-submit  --executor-memory 9G  --driver-memory 9G  --total-executor-cores 120 \
+$wrt_path/feature_dense_sparse.py $today
+
+hfs -rmr /user/wrt/temp/add_new_feature_name
+hfs -rmr /user/wrt/temp/add_newfeature_inhive
+
+spark-submit  --executor-memory 9G  --driver-memory 9G  --total-executor-cores 120 \
+$wrt_path/add_new_feat.py $today
 
 hfs -rmr /user/wrt/credit/allexample.libsvm
 hfs -rmr /user/wrt/credit/allexample_filter.libsvm
