e77a0381f8f4614e1d7da56579bb4084d2a2832b
lyen <lienlian@wolongdata.com>
2017-02-28 19:32:17 +0800

    xianyu

--- airflow/lel/xianyu/iteminfo/xianyu_iteminfo_parse.sh
+++ airflow/lel/xianyu/iteminfo/xianyu_iteminfo_parse.sh
@@ -5,6 +5,7 @@ date
 date  +%Y%m%d
 
 lastday=$(date -d '1 days ago' +%Y%m%d)
+spark=/home/lel/sparkdata/lel/spark
 
 hadoop fs -test -e /user/lel/temp/xianyu_iteminfo
 if [ $? -eq 0 ] ;then
@@ -13,4 +14,4 @@ else
     echo 'Directory is not exist,you can run your spark as you want!!!'
 fi
 
-spark-submit  --executor-memory 6G  --driver-memory 6G  --total-executor-cores 60 $work/spark/xianyu/xianyu_iteminfo.py $lastday
+spark-submit  --executor-memory 6G  --driver-memory 6G  --total-executor-cores 60 $spark/xianyu/xianyu_iteminfo.py $lastday
