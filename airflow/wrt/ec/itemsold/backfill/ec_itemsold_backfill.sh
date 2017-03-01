549e8b786d6e167e439518dbadeccd7cc8b0a2ec
lyen <lienlian@wolongdata.com>
2017-03-01 11:16:34 +0800

    xianyu

--- airflow/wrt/ec/itemsold/backfill/ec_itemsold_backfill.sh
+++ airflow/wrt/ec/itemsold/backfill/ec_itemsold_backfill.sh
@@ -4,20 +4,20 @@ pre_path='/home/wrt/sparkdata'
 
 hadoop fs -rmr /user/wrt/sale_tmp
 
-spark-submit  --executor-memory 10G  --driver-memory 10G  --total-executor-cores 120 \
+spark-submit  --driver-memory 4G --num-executors 20 --executor-memory 20G --executor-cores 5 \
 $pre_path/wrt/data_base_process/t_base_item_sale.py $2 $1 $3
 
 hive<<EOF
-use wlbase_dev;
+use wl_base;
 LOAD DATA  INPATH '/user/wrt/sale_tmp' OVERWRITE INTO TABLE t_base_ec_item_sold_dev PARTITION (ds='$1');
 EOF
 
 hadoop fs -rm -r /user/wrt/daysale_tmp
 
-spark-submit  --total-executor-cores  120   --executor-memory  8g  --driver-memory 8g \
+spark-submit  --driver-memory 4G --num-executors 20 --executor-memory 20G --executor-cores 5 \
 $pre_path/wrt/data_base_process/cal_daysale.py $2 $1
 
 hive<<EOF
-use wlbase_dev;
+use wl_base;
 LOAD DATA  INPATH '/user/wrt/daysale_tmp' OVERWRITE INTO TABLE t_base_ec_item_daysale_dev_new PARTITION (ds='$2');
 EOF
