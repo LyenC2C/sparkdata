4bf873e464054e3fc6cb2a2b044cb1c83c41c59d
lyen <lienlian@wolongdata.com>
2017-03-01 10:33:23 +0800

    xianyu

--- airflow/wrt/ec/shopitem_b/shopitem_b_import.sh
+++ airflow/wrt/ec/shopitem_b/shopitem_b_import.sh
@@ -4,7 +4,6 @@ source ~/.bashrc
 lastday=$1
 
 hive<<EOF
-use wlbase_dev;
 set hive.merge.mapfiles = true;
-LOAD DATA  INPATH '/user/wrt/shopitem_tmp' OVERWRITE INTO TABLE t_base_ec_shopitem_b PARTITION (ds='$lastday');
+LOAD DATA  INPATH '/user/wrt/shopitem_tmp' OVERWRITE INTO TABLE wl_base.t_base_ec_shopitem_b PARTITION (ds='$lastday');
 EOF
--- airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.sh
+++ airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.sh
@@ -6,15 +6,18 @@ last_2_days=$2
 
 hadoop fs  -rmr /user/wrt/shopitem_c_tmp
 
-spark-submit  --executor-memory 6G  --driver-memory 5G  --total-executor-cores 80 \
+spark-submit --driver-memory 4G --num-executors 20 --executor-memory 20G --executor-cores 5 \
 $pre_path/wrt/data_base_process/t_base_shopitem_c.py $lastday
 
+table=wl_base.t_base_ec_shopitem_c
 
 hive<<EOF
-use wlbase_dev;
-LOAD DATA  INPATH '/user/wrt/shopitem_c_tmp' OVERWRITE INTO TABLE t_base_ec_shopitem_c PARTITION (ds='0temp');
-
-insert OVERWRITE table t_base_ec_shopitem_c PARTITION(ds = $lastday)
+set hive.merge.mapredfiles = true;
+set hive.merge.mapfiles = true;
+set hive.merge.size.per.task = 240000000;
+set hive.merge.smallfiles.avgsize= 180000000;
+LOAD DATA  INPATH '/user/wrt/shopitem_c_tmp' OVERWRITE INTO TABLE $table PARTITION (ds='0temp');
+insert OVERWRITE table $table PARTITION(ds = $lastday)
 select
 case when t1.item_id is null then t2.shop_id else t1.shop_id end,
 case when t1.item_id is null then t2.item_id else t1.item_id end,
@@ -24,9 +27,9 @@ case when t2.item_id is null then t1.up_day else t2.up_day end,
 case when t1.item_id is null then t2.update_day else t1.update_day end,
 case when t1.item_id is null then t2.ts else t1.ts end
 from
-(select * from t_base_ec_shopitem_c where ds = '0temp')t1
+(select * from $table where ds = '0temp')t1
 full outer join
-(select * from t_base_ec_shopitem_c where ds = $last_2_days)t2
+(select * from $table where ds = $last_2_days)t2
 on
 t1.item_id = t2.item_id;
 EOF
