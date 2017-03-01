6cbda7c04d978de8084e22b36eae1ee8b301aeaf
lyen <lienlian@wolongdata.com>
2017-03-01 19:24:28 +0800

    xianyu

--- airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.sh
+++ airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.sh
@@ -3,7 +3,7 @@ source ~/.bashrc
 pre_path='/home/wrt/sparkdata'
 lastday=$1
 last_2_days=$2
-
+table=wl_base.t_base_ec_shopitem_c
 hive<<EOF
 LOAD DATA  INPATH '/user/wrt/shopitem_c_tmp' OVERWRITE INTO TABLE $table PARTITION (ds='0temp');
 EOF
