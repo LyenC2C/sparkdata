125a9de738a1fda5cf71ff939f0d391558982276
Gbacillus <gs464gan@gmail.com>
2017-03-02 14:31:43 +0800

    s

--- airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.sh
+++ airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.sh
@@ -6,6 +6,7 @@ last_2_days=$2
 table=wl_base.t_base_ec_shopitem_c
 
 hadoop fs  -rmr /user/wrt/shopitem_c_tmp
+hadoop fs -rmr /hive/warehouse/wl_base.db/t_base_ec_shopitem_c/ds=00tmp/*
 spark-submit --driver-memory 4G --num-executors 20 --executor-memory 20G --executor-cores 5 \
 $pre_path/wrt/data_base_process/t_base_shopitem_c.py $lastday
 hadoop fs -mv /user/wrt/shopitem_c_tmp/* /hive/warehouse/wl_base.db/t_base_ec_shopitem_c/ds=00tmp/
@@ -13,7 +14,7 @@ hadoop fs -mv /user/wrt/shopitem_c_tmp/* /hive/warehouse/wl_base.db/t_base_ec_sh
 hive<<EOF
 set hive.merge.mapfiles= true;
 set hive.merge.mapredfiles= true;
-MSCK REPAIR TABLE t_base_ec_shopitem_c
+MSCK REPAIR TABLE t_base_ec_shopitem_c;
 insert OVERWRITE table $table PARTITION(ds = $lastday)
 select
 case when t1.item_id is null then t2.shop_id else t1.shop_id end,
