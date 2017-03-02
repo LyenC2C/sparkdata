2307bb1c43d2d094d4d24d862b3c9f5830fb7280
lyen <lienlian@wolongdata.com>
2017-03-02 10:42:57 +0800

    xianyu

--- airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.sh
+++ airflow/wrt/ec/shopitem_c/backfill/shopitem_c_backfill.sh
@@ -10,7 +10,7 @@ spark-submit --driver-memory 4G --num-executors 20 --executor-memory 20G --execu
 $pre_path/wrt/data_base_process/t_base_shopitem_c.py $lastday
 
 hive<<EOF
-LOAD DATA  INPATH '/user/wrt/shopitem_c_tmp' OVERWRITE INTO TABLE $table PARTITION (ds='0temp');
+LOAD DATA  INPATH '/user/wrt/shopitem_c_tmp' OVERWRITE INTO TABLE $table PARTITION (ds='00tmp');
 insert OVERWRITE table $table PARTITION(ds = $lastday)
 select
 case when t1.item_id is null then t2.shop_id else t1.shop_id end,
@@ -21,7 +21,7 @@ case when t2.item_id is null then t1.up_day else t2.up_day end,
 case when t1.item_id is null then t2.update_day else t1.update_day end,
 case when t1.item_id is null then t2.ts else t1.ts end
 from
-(select * from $table where ds = '0temp')t1
+(select * from $table where ds = '00tmp')t1
 full outer join
 (select * from $table where ds = $last_2_days)t2
 on
