d3f47e2285a1bd1a4c03f7deaaaf16bad04fcf18
lyen <lienlian@wolongdata.com>
2017-03-02 20:20:28 +0800

    xianyu

--- airflow/wrt/ec/itemsold/backfill/ec_itemsold_backfill.sh
+++ airflow/wrt/ec/itemsold/backfill/ec_itemsold_backfill.sh
@@ -4,26 +4,22 @@ pre_path='/home/wrt/sparkdata'
 
 hadoop fs -rmr /user/wrt/sale_tmp
 
-spark-submit  --driver-memory 6G --num-executors 30 --executor-memory 30G --executor-cores 5 \
+spark-submit  --driver-memory 6G --num-executors 15 --executor-memory 15G --executor-cores 5 \
 $pre_path/wrt/data_base_process/t_base_item_sale.py $2 $1 $3
 
 hive<<EOF
 use wl_base;
-LOAD DATA  INPATH '/user/wrt/sale_tmp' OVERWRITE INTO TABLE t_base_ec_item_sold_dev PARTITION (ds='$1');
+LOAD DATA  INPATH '/user/wrt/sale_tmp' OVERWRITE INTO TABLE t_base_ec_item_sold_dev PARTITION (ds=$1);
 EOF
 
 hadoop fs -rm -r /user/wrt/daysale_tmp
 
-spark-submit  --driver-memory 6G --num-executors 30 --executor-memory 30G --executor-cores 5 \
+spark-submit  --driver-memory 6G --num-executors 15 --executor-memory 15G --executor-cores 5 \
 $pre_path/wrt/data_base_process/cal_daysale.py $2 $1
 
 hive<<EOF
-set hive.merge.mapredfiles = true;
-set hive.merge.mapfiles = true;
-set hive.merge.size.per.task = 240000000;
-set hive.merge.smallfiles.avgsize= 180000000;
 use wl_base;
-LOAD DATA  INPATH '/user/wrt/daysale_tmp' OVERWRITE INTO TABLE t_base_ec_item_daysale_dev_new PARTITION (ds='$2');
+LOAD DATA  INPATH '/user/wrt/daysale_tmp' OVERWRITE INTO TABLE wl_base.t_base_ec_item_daysale_dev_new PARTITION (ds=$2);
 EOF
 
 
