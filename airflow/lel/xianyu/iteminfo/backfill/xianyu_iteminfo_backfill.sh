56acd52f3fbe060b6c2af15aa08cae97ede3c849
lyen <lienlian@wolongdata.com>
2017-03-02 11:19:27 +0800

    xianyu

--- airflow/lel/xianyu/iteminfo/backfill/xianyu_iteminfo_backfill.sh
+++ airflow/lel/xianyu/iteminfo/backfill/xianyu_iteminfo_backfill.sh
@@ -12,7 +12,7 @@ else
     echo 'Directory is not exist,you can run your spark as you want!!!'
 fi
 
-spark-submit  --driver-memory 4G --num-executors 20 --executor-memory 20G --executor-cores 5 $work/spark/xianyu/xianyu_iteminfo.py
+spark-submit  --driver-memory 4G --num-executors 20 --executor-memory 20G --executor-cores 5 $work/spark/xianyu/xianyu_iteminfo.py $lastday
 
 
 #闲鱼iteminfo——日更——当天处理的是昨天的数据
