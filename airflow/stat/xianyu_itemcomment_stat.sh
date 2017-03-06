fd1980319431aca53e44356a1d7b696e7563698c
lyen <lienlian@wolongdata.com>
2017-03-06 21:17:49 +0800

    xianyu

--- airflow/stat/xianyu_itemcomment_stat.sh
+++ airflow/stat/xianyu_itemcomment_stat.sh
@@ -14,8 +14,7 @@ last_2_days=`hadoop fs -ls /hive/warehouse/wl_base.db/t_base_ec_xianyu_itemcomme
 last_2_days_size=`hadoop fs -du -s  /hive/warehouse/wl_base.db/t_base_ec_xianyu_itemcomment/ds=$last_2_days | awk '{print $1/1024/1024" ""MB"}'`
 last_2_days_rows=`hive -S -e "select count(1) from wl_base.t_base_ec_xianyu_itemcomment where ds=$last_2_days"`
 last_2_days_files=`hadoop fs -ls /hive/warehouse/wl_base.db/t_base_ec_xianyu_itemcomment/ds=$last_2_days | wc -l`
-
-#echo "{'update_day':'$lastday','table':'wl_base.t_base_ec_xianyu_itemcomment','total_files':'$lastday_files','total_rows':'$last_2_days_rows','total_size':'$last_2_days_size'}"
-#echo "{'last_update_day':'$last_2_days','table':'wl_base.t_base_ec_xianyu_itemcomment','total_files':'$last_2_days_files','total_rows':'$last_2_days_rows','total_size':'$last_2_days_files'}"
-echo  "{'update_day':{'$lastday','table':'wl_base.t_base_ec_xianyu_itemcomment','total_files':'$lastday_files','total_rows':'$last_2_days_rows','total_size':'$last_2_days_size'},'last_update_day':{'$last_2_days','table':'wl_base.t_base_ec_xianyu_itemcomment','total_files':'$last_2_days_files','total_rows':'$last_2_days_rows','total_size':'$last_2_days_files'}}"
+echo "{'last_update_day':'$last_2_days','table':'wl_base.t_base_ec_xianyu_itemcomment','total_files':'$last_2_days_files','total_rows':'$last_2_days_rows','total_size':'$last_2_days_files'}"
+echo "{'update_day':'$lastday','table':'wl_base.t_base_ec_xianyu_itemcomment','total_files':'$lastday_files','total_rows':'$last_2_days_rows','total_size':'$last_2_days_size'}"
+#echo  "{'update_day':{'$lastday','table':'wl_base.t_base_ec_xianyu_itemcomment','total_files':'$lastday_files','total_rows':'$last_2_days_rows','total_size':'$last_2_days_size'},'last_update_day':{'$last_2_days','table':'wl_base.t_base_ec_xianyu_itemcomment','total_files':'$last_2_days_files','total_rows':'$last_2_days_rows','total_size':'$last_2_days_files'}}"
 
