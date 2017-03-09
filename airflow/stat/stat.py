cda09f1d0404d8ec486c5346f4a25f9fb2fd6bf9
lyen <lienlian@wolongdata.com>
2017-03-09 17:58:01 +0800

    xianyu

--- airflow/stat/stat.py
+++ airflow/stat/stat.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
@@ -16,8 +17,9 @@ import re
 #
 # dag = DAG('stat', default_args=default_args, schedule_interval="@once")
 #
-# # xianyu_iteminfo_cmd = "(bash /home/lel/sparkdata/airflow/stat/xianyu_iteminfo_stat.sh)"
+# xianyu_iteminfo_cmd = "(bash /home/lel/sparkdata/airflow/stat/xianyu_iteminfo_stat.sh)"
 xianyu_itemcomment_cmd = "(bash /home/lel/sparkdata/airflow/stat/xianyu_itemcomment_stat.sh)"
+
 def valid_jsontxt(content):
     if type(content) == type(u""):
         res = content.encode("utf-8")
@@ -32,22 +34,37 @@ def get_from_shell(cmd):
         raise Exception("operation failed!")
     else:
         return result
+'''
 def parse_str(content):
     arr_v = re.findall("(\d+)",content)[8:-1]
     arr_key = ["update_day","total_files","total_rows","total_size"]
     return dict(zip(arr_key,arr_v))
+'''
 
-def process(jsonStr,date):
-    ob = json.loads(jsonStr)
-    last_day = ob.get(date)
-    update_day = last_day.get("update_day")
-    table = last_day.get("table")
-    total_files = last_day.get("total_files")
+def process(jsonStr):
+    ob = json.loads(valid_jsontxt(jsonStr))
+    table = ob.get("table")
+
+    last_day = ob.get("update_day")
+    update_day = "update_time:"+"\t "+last_day.get("update_day")
+    total_files = "fiels:"+last_day.get("total_files")
     total_rows = last_day.get("total_rows")
-    rows = re.findall("\\d+",total_rows)[0]
-    return [table,update_day,total_files,rows]
+    rows = "rows:"+re.findall("\\d+",total_rows)[0]
 
-for i in process(get_from_shell(xianyu_itemcomment_cmd)):
-    print i
+    last_2_days = ob.get("last_update_day")
+    last_update_day = "last_update_time:"+last_2_days.get("last_update_day")
+    last_total_files = "fiels:"+last_2_days.get("total_files")
+    last_total_rows = last_2_days.get("total_rows")
+    last_rows = "rows:"+re.findall("\\d+",last_total_rows)[0]
+def processMulti(arr):
+    result = []
+    [result.append(process(table)) for table in arr]
+    return result
+
+def formatOutput(arr):
+    return "\n------------------------------------------------------\n".join(arr)
+
+print formatOutput(processMulti(process(get_from_shell())))
+
 
 
