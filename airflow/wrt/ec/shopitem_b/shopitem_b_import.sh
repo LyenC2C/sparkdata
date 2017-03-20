d0ee878979f2011848cdf6a3a6e34aef6a3ff60c
lyen <lienlian@wolongdata.com>
2017-03-20 11:27:10 +0800

    xianyu

--- airflow/wrt/ec/shopitem_b/shopitem_b_import.sh
+++ airflow/wrt/ec/shopitem_b/shopitem_b_import.sh
@@ -6,7 +6,10 @@ lastday=$1
 hive<<EOF
 set hive.merge.mapfiles=true;
 set hive.merge.mapredfiles=true;
-set hive.merge.size.per.task=256000000;
-set hive.merge.smallfiles.avgsize=192000000;
+set hive.merge.size.per.task=268435456;
+set hive.merge.smallfiles.avgsize=201326592;
+set hive.support.quoted.identifiers=none;
 LOAD DATA  INPATH '/user/wrt/shopitem_tmp' OVERWRITE INTO TABLE wl_base.t_base_ec_shopitem_b PARTITION (ds=$lastday);
+insert OVERWRITE table wl_base.t_base_ec_shopitem_b PARTITION(ds = '$lastday')
+select `(ds)?+.+` from wl_base.t_base_ec_shopitem_b where ds = '$lastday'
 EOF
