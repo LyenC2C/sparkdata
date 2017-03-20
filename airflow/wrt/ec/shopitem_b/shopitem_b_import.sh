f05ef9fbc6253729bd88ea5edc2e63ad84730f57
lyen <lienlian@wolongdata.com>
2017-03-20 11:32:51 +0800

    xianyu

--- airflow/wrt/ec/shopitem_b/shopitem_b_import.sh
+++ airflow/wrt/ec/shopitem_b/shopitem_b_import.sh
@@ -11,5 +11,5 @@ set hive.merge.size.per.task=268435456;
 set hive.merge.smallfiles.avgsize=201326592;
 set hive.support.quoted.identifiers=none;
 insert OVERWRITE table wl_base.t_base_ec_shopitem_b PARTITION(ds = $lastday)
-select `(ds)?+.+` from wl_base.t_base_ec_shopitem_b where ds = $lastday
+select `(ds)?+.+` from wl_base.t_base_ec_shopitem_b where ds = $lastday;
 EOF
