cc08177abce896fcbc69bd63e1b03635a44ae0cd
lyen <lienlian@wolongdata.com>
2017-03-22 15:44:26 +0800

    xianyu

---
+++ airflow/wrt/credit/shixin/ppd_listinfo_inhive.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+source ~/.bashrc
+dev_path='/home/wrt/sparkdata/wrt/credit/ppd/'
+now_day=$1
+#last_day=$2
+
+hadoop fs -rmr /user/wrt/temp/ppd_info_tmp
+spark-submit  --executor-memory 6G  --driver-memory 8G  --total-executor-cores 80 \
+$dev_path/t_base_ppd_listinfo.py $now_day
+
+hive<<EOF
+
+LOAD DATA INPATH '/user/wrt/temp/ppd_info_tmp' OVERWRITE INTO TABLE wl_base.t_base_credit_ppd_listinfo PARTITION (ds=$now_day);
+
+EOF
---
+++ airflow/wrt/credit/shixin/shixin_person_parse.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+source ~/.bashrc
+dev_path='/home/wrt/sparkdata/wrt/credit/shixin/'
+now_day=$(date -d '0 days ago' +%Y%m%d)
+
+
+hadoop fs -test -e /user/wrt/temp/shixin_personinfo
+if [ $? -eq 0 ] ;then
+hadoop fs  -rmr /user/wrt/temp/shixin_personinfo
+else
+echo 'Directory is not exist,you can run you spark job as you want!!!'
+fi
+spark-submit  --executor-memory 6G  --driver-memory 8G  --total-executor-cores 80 $dev_path/shixin_person.py $now_day
+
