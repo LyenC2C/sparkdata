bd6dcdd50d86c0b150064a6d07d8a83ecefc6953
Gbacillus <gs464gan@gmail.com>
2017-04-21 19:05:56 +0800

    s

--- wrt/credit/feature_dense_sparse_merge/feature_sparse_merge.sql
+++ wrt/credit/feature_dense_sparse_merge/feature_sparse_merge.sql
@@ -1,21 +1,21 @@
 --稀疏表合并
-drop table wlcredit.t_wrt_credit_record_cate_feature;
-create table wlcredit.t_wrt_credit_record_cate_feature as
+drop table wl_feature.t_wrt_credit_record_cate_feature;
+create table wl_feature.t_wrt_credit_record_cate_feature as
 select t1.tel_index,
 concat_ws(' ',t1.feature,coalesce(t2.feature,''),coalesce(t3.feature,''),coalesce(t4.feature,''))
 as feature
 from
-wlcredit.t_credit_record_cate1_feature_months t1
+wl_feature.t_credit_record_cate1_feature_months t1
 left JOIN
-wlcredit.t_credit_record_cate2_feature_months t2
+wl_feature.t_credit_record_cate2_feature_months t2
 ON
 t1.tel_index = t2.tel_index
 left JOIN
-wlcredit.t_credit_record_cate3_feature_months t3
+wl_feature.t_credit_record_cate3_feature_months t3
 ON
 t1.tel_index = t3.tel_index
 left JOIN
-wlcredit.t_credit_record_cate4_feature_months t4
+wl_feature.t_credit_record_cate4_feature_months t4
 ON
 t1.tel_index = t4.tel_index
 ;
