01a9837b8cc93f4c41e3316d1d51c4de790e241a
lyen <lienlian@wolongdata.com>
2017-04-21 19:34:09 +0800

    xianyu

--- airflow/wrt/ec/itemsold/backfill/ec_itemsold_backfill.py
+++ airflow/wrt/ec/itemsold/backfill/ec_itemsold_backfill.py
@@ -5,7 +5,7 @@ import os
 
 '''
 :param 0~4
-first param plus 1 day 1~4 for airflow to execute
+first param plus 1 day 1~4 for airflow.sql to execute
 '''
 def backfill_continuously(latest_day, days,iteminfo_date):
     exec_days = list(xrange(latest_day, days))
@@ -15,22 +15,26 @@ def backfill_continuously(latest_day, days,iteminfo_date):
         exec_day = (today + datetime.timedelta(days=-i)).strftime('%Y%m%d')
         last_day = (today + datetime.timedelta(days=-i - 1)).strftime('%Y%m%d')
         last_2_days = (today + datetime.timedelta(days=-i - 2)).strftime('%Y%m%d')
-        # print exec_day, last_day, last_2_days
-        command =" bash /home/wrt/sparkdata/airflow/wrt/ec/itemsold/backfill/ec_itemsold_backfill.sh  {last_day} {last_2_days} {iteminfo_date}".format(last_day=last_day,last_2_days=last_2_days,iteminfo_date=iteminfo_date)
-        os.system(command)
+        print exec_day, last_day, last_2_days
+        # command =" bash /home/wrt/sparkdata/airflow.sql/wrt/ec/itemsold/backfill/ec_itemsold_backfill.sh  {last_day} {last_2_days} {iteminfo_date}".format(last_day=last_day,last_2_days=last_2_days,iteminfo_date=iteminfo_date)
+        # os.system(command)
 
 def backfill_individually(last_day, last_update_day,iteminfo_date):
-    command = "bash /home/wrt/sparkdata/airflow/wrt/ec/itemsold/backfill/ec_itemsold_backfill.sh  {last_day} {last_update_day} {iteminfo_date}".format(last_day=last_day,last_update_day=last_update_day,iteminfo_date=iteminfo_date)
-    os.system(command)
+    command = "bash /home/wrt/sparkdata/airflow.sql/wrt/ec/itemsold/backfill/ec_itemsold_backfill.sh  {last_day} {last_update_day} {iteminfo_date}".format(last_day=last_day,last_update_day=last_update_day,iteminfo_date=iteminfo_date)
+    print last_day,last_update_day,iteminfo_date
+    # os.system(command)
+
 
 
 '''
 :param 0~4
-first param plus 1 day like 1~4 for airflow to execute
+first param plus 1 day like 1~4 for airflow.sql to execute
 second param subtract 1 day like 1~3 for tolerancing
 '''
 def backfill_allkindsof(last_day, last_update_day, latest_day, days,iteminfo_date):
-    backfill_individually(last_day, last_update_day,iteminfo_date)
-    backfill_continuously(latest_day, days-1,iteminfo_date)
+    backfill_individually(20170417, 20170407,20170418)
+    backfill_continuously(1,3,20170421)
 
-backfill_continuously(1,6,20170225)
+
+# backfill_continuously(1,3,20170421)
+# backfill_individually(20170417, 20170407,20170418)
