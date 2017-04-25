c2eebd28aec0ef20b182931a643ecc837715e01b
lyen <lienlian@wolongdata.com>
2017-04-25 10:03:23 +0800

    xianyu

--- airflow/wrt/ec/itemsold/backfill/ec_itemsold_backfill.sh
+++ airflow/wrt/ec/itemsold/backfill/ec_itemsold_backfill.sh
@@ -10,10 +10,13 @@ hadoop fs -rmr /user/wrt/sale_tmp
 spark-submit  --driver-memory 8G --num-executors 30 --executor-memory 20G --executor-cores 5 \
 $pre_path/wrt/data_base_process/t_base_item_sale.py $last_2_days $lastday $iteminfoday
 
-hadoop fs -chmod -R 777 /user/wrt/daysale_tmp
 
 echo $1
 echo $2
+
+hadoop fs -chmod 777 /user/wrt/daysale_tmp
+
+
 
 beeline -u "jdbc:hive2://cs105:10000/;principal=hive/cs105@HADOOP.COM"<<EOF
 LOAD DATA  INPATH "/user/wrt/sale_tmp" OVERWRITE INTO TABLE wl_base.t_base_ec_item_sold_dev PARTITION (ds=$lastday);
@@ -24,7 +27,7 @@ hadoop fs -rm -r /user/wrt/daysale_tmp
 spark-submit  --driver-memory 8G --num-executors 30 --executor-memory 20G --executor-cores 5 \
 $pre_path/wrt/data_base_process/cal_daysale.py $2 $1
 
-hadoop fs -chmod -R 777 /user/wrt/daysale_tmp
+hadoop fs -chmod 777 /user/wrt/daysale_tmp
 
 beeline -u "jdbc:hive2://cs105:10000/;principal=hive/cs105@HADOOP.COM"<<EOF
 LOAD DATA  INPATH "/user/wrt/daysale_tmp" OVERWRITE INTO TABLE wl_base.t_base_ec_item_daysale_dev_new PARTITION (ds=$last_2_days);
