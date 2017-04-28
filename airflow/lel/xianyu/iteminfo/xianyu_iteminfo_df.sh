ae6a84ed82eebf11d9d352b868be2278ce382898
lyen <lienlian@wolongdata.com>
2017-04-28 14:04:41 +0800

    xianyu

--- airflow/lel/xianyu/iteminfo/xianyu_iteminfo_df.sh
+++ airflow/lel/xianyu/iteminfo/xianyu_iteminfo_df.sh
@@ -7,4 +7,4 @@ date  +%Y%m%d
 lastday=$(date -d '1 days ago' +%Y%m%d)
 last_2day=$(date -d '2 days ago' +%Y%m%d)
 
-spark2-submit  --driver-memory 4G --num-executors 20 --executor-memory 20G --executor-cores 5 /home/lel/sparkdata/lel/spark/xianyu/xianyu_iteminfo_df.py $lastday $last_2day
+spark-submit  --driver-memory 4G --num-executors 20 --executor-memory 20G --executor-cores 5 /home/lel/sparkdata/lel/spark/xianyu/xianyu_iteminfo_df.py $lastday $last_2day
