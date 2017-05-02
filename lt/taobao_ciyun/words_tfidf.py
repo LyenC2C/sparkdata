a953b85c836f9c7c1255beaf95e2759a9c9bb31f
sf <liuting@wolongdata.com>
2017-05-02 10:31:52 +0800

    0502_lt

--- lt/taobao_ciyun/words_tfidf.py
+++ lt/taobao_ciyun/words_tfidf.py
@@ -2,16 +2,19 @@
 __author__ = 'zlj'
 
 from pyspark import SparkContext
+
 from pyspark.sql import *
 from pyspark.sql.types import *
 from itertools import groupby
+
 import math
+import sys
 
 sc = SparkContext(appName="hmm")
 sqlContext = SQLContext(sc)
 hiveContext = HiveContext(sc)
-hiveContext.sql('use wl_analysis')
 
+hiveContext.sql('use wl_base')
 
 
 def tf(x, y):
@@ -27,40 +30,27 @@ def df(x, y):
     return lv
 
 
-def tf_idf(x,dict):
-    word=x[0]
-    doc_id=x[1][0]
-    tf=x[1][1]
-    tfidf=tf*dict.get(word,0.5)
-    return (doc_id,(word,tfidf))
-
-schema = StructType([
-    StructField("user_id", StringType(), True),
-    StructField("tfidftags", StringType(), True)
-])
-
 
 sql_hmm='''
-select * from t_lt_base_record_item_segment_words_group
-
+select * from table_name  where ds>%s
 '''
 
-import sys
+
 if __name__ == "__main__":
 
     if len(sys.argv)<4:
         print ' py  min_freq limit feed_ds'
         sys.exit(0)
-
     min_freq=int(sys.argv[1])  #限制最低词频
     limit=int(sys.argv[2])     #限制产出用户词云个数 30-50个
-    feed_ds=sys.argv[3]
-    # hmm_ds=sys.argv[4]
-    rdd_pre = hiveContext.sql(sql_hmm%feed_ds).map(lambda x: (x.user_id, [i for i in x[1].split('_') if len(i) > 3]))
+    feed_ds=sys.argv[3]       #分词表最新分区
+
+    #加载数据
+    rdd_pre = hiveContext.sql(sql_hmm%feed_ds).map(lambda x: (x.user_id, x[1].split('_')))
     # top_freq=5   and x[1]<top_freq
     # 统计所有的词
-    words = set(rdd_pre.map(lambda x: x[1]).flatMap(lambda x: x).map(lambda x: (x, 1)).groupByKey().map(
-        lambda (x, y): (x, len(y))).filter(lambda x: x[1] > min_freq).map(lambda x: x[0]).collect())
+    words = set(rdd_pre.map(lambda x: x[1]).flatMap(lambda x: x).map(lambda x: (x, 1)).reduceByKey(
+        lambda a,b:a+b).filter(lambda x: x[1] > min_freq).map(lambda x: x[0]).collect())
     broadcastVar = sc.broadcast(words)
     dict = broadcastVar.value
 
@@ -71,10 +61,9 @@ if __name__ == "__main__":
     # (word,(doc_id,tf))
     tfrdd = rdd.map(lambda (item_id, words): tf(item_id, words)).flatMap(lambda x: x)
     # word ,len
-    dfrdd = rdd.map(lambda (item_id, words): df(item_id, words)).flatMap(lambda x: x).groupByKey().map(lambda (x, y): (x, len(y)))
+    dfrdd = rdd.map(lambda (item_id, words): df(item_id, words)).flatMap(lambda x: x).reduceByKey(lambda a,b:a+b)
     # word idf
     idfrdd = dfrdd.map(lambda (word ,word_len): (word, math.log((doc_num + 1) * 1.0 / (word_len + 1))))
 
     #广播 idf
-
 
