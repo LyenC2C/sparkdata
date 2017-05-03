0ba4628c9594dddb12f6b3cac75f2229b4e051d4
lyen <lienlian@wolongdata.com>
2017-05-03 12:29:19 +0800

    xianyu

--- airflow/wrt/ec/itemsearch/distinct.sh
+++ airflow/wrt/ec/itemsearch/distinct.sh
@@ -2,6 +2,7 @@
 source ~/.bashrc
 
 beeline -u "jdbc:hive2://cs105:10000/;principal=hive/cs105@HADOOP.COM"<<EOF
+load data inpath '/user/wrt/temp/itemsearch' overwrite into table t_base_item_search partition(ds = '0temp');
 insert overwrite table wl_base.t_base_item_search partition(ds = '0temp')
 SELECT
 nid,max(user_id),max(comment_count),max(encryptedUserId),max(nick)
