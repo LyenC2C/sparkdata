be4c8717b452050b3bb788e89bf296199d0ab809
Gbacillus <gs464gan@gmail.com>
2017-05-08 18:26:36 +0800

    s

--- wrt/qianxing/ftp_hadoop.sh
+++ wrt/qianxing/ftp_hadoop.sh
@@ -4,9 +4,9 @@ jintian=$(date -d '0 days ago' +%Y%m%d)
 #wget ftp://ftp.blhdk.wolongdata.com/$zuotian*.csv --ftp-user=qx --ftp-password=NzS7d2oWe47LPVXx --no-passive-ftp
 
 today=`ls $jintian*`
-python jiema.py $today > $jintian
+python jiema.py $today > $jintian_qianxing
 
-hadoop fs -put ./$zuotian*.csv /user/wrt/qianxing/
+hadoop fs -put ./$jintian_qianxing /user/wrt/qianxing/
 
 
 
