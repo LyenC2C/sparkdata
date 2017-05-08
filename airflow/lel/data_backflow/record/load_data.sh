9e13ddb9bdec7e92e66499d11fe79bb71d205fbf
lyen <lienlian@wolongdata.com>
2017-05-08 21:27:58 +0800

    xianyu

--- airflow/lel/data_backflow/record/load_data.sh
+++ airflow/lel/data_backflow/record/load_data.sh
@@ -2,7 +2,16 @@
 source ~/.bashrc
 
 today=$(date -d '0 days ago' +%Y%m%d)
+
+hadoop fs -test -e /hive/warehouse/wl_base.db/t_base_record_data_backflow/ds=$today
+if [ $? -eq 0 ] ;then
+    hadoop fs -rmr /hive/warehouse/wl_base.db/t_base_record_data_backflow/ds=$today
+else
+    echo 'Directory is not exist,go ahead!!!'
+fi
+
 last_update=`hadoop fs -ls /hive/warehouse/wl_base.db/t_base_record_data_backflow | awk -F '=' '{if($2 ~ /^[0-9]+$/)print $2}' | sort -r |awk 'NR==1{print $0}'`
+
 beeline -u "jdbc:hive2://cs105:10000/;principal=hive/cs105@HADOOP.COM"<<EOF
 set hive.support.quoted.identifiers=none;
 load data inpath '/user/lel/temp/record_data_backflow' overwrite into table wl_base.t_base_record_data_backflow partition(ds='00tmp');
