99ef9d652e585edd4414785d14531f7607d85df2
Gbacillus <gs464gan@gmail.com>
2017-05-08 18:12:10 +0800

    s

--- wrt/qianxing/jiema.py
+++ wrt/qianxing/jiema.py
@@ -1,15 +1,9 @@
 #coding:utf-8
 
-import sys
+import csv
+import glob
 
-def transform_df_fileds(content):
-    if isinstance(content, unicode):
-        res = content.encode("utf-8").decode("latin-1").encode("iso-8859-1").decode("utf-8")
-    elif isinstance(content, str):
-        res = content.decode("latin-1").encode("iso-8859-1").decode("utf-8")
-    else:
-        res = str(content)
-    return res
-
-for line in sys.stdin:
-    print transform_df_fileds(line.strip())
+csv_file = glob(sys.argv[1])
+reader = csv.reader(file(csv_file, 'rb'))
+for line in reader:
+    print "\001".join(line)
