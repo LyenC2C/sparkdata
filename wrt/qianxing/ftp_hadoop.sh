2daaab8d7d8fe66455b0f9cc1a697fbc10abe315
Gbacillus <gs464gan@gmail.com>
2017-05-08 18:25:37 +0800

    s

--- wrt/qianxing/ftp_hadoop.sh
+++ wrt/qianxing/ftp_hadoop.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
-zuotian=$(date -d '0 days ago' +%Y%m%d)
+jintian=$(date -d '0 days ago' +%Y%m%d)
 
-wget ftp://ftp.blhdk.wolongdata.com/$zuotian*.csv --ftp-user=qx --ftp-password=NzS7d2oWe47LPVXx --no-passive-ftp
-hfs -put ./$zuotian*.csv /user/wrt/qianxing/
+#wget ftp://ftp.blhdk.wolongdata.com/$zuotian*.csv --ftp-user=qx --ftp-password=NzS7d2oWe47LPVXx --no-passive-ftp
+
+today=`ls $jintian*`
+python jiema.py $today > $jintian
+
+hadoop fs -put ./$zuotian*.csv /user/wrt/qianxing/
+
+
+
+#hive<<EOF
+#load data inpath '/user/wrt/qianxing/*' insert overwrite table wl_link.t_qianxing_huafei partition(ds =$zuotian)
+#EOF
+
+
