00a8ebafd18d2a1837ed3f643d2c92a3dbf465a4
lyen <lienlian@wolongdata.com>
2017-05-08 20:40:19 +0800

    xianyu

--- airflow/lel/data_backflow/record/load_data.sh
+++ airflow/lel/data_backflow/record/load_data.sh
@@ -4,9 +4,10 @@ source ~/.bashrc
 today=$(date -d '0 days ago' +%Y%m%d)
 last_update=`hadoop fs -ls /hive/warehouse/wl_base.db/t_base_record_data_backflow | awk -F '=' '{if($2 ~ /^[0-9]+$/)print $2}' | sort -r |awk 'NR==1{print $0}'`
 beeline -u "jdbc:hive2://cs105:10000/;principal=hive/cs105@HADOOP.COM"<<EOF
-load data inpath '/user/lel/temp/record_data_backflow' into table wl_base.t_base_record_data_backflow partition(ds='00tmp');
-insert overwrite wl_base.t_base_record_data_backflow ds=$today
-select * from wl_base.t_base_record_data_backflow where ds='00tmp'
+set hive.support.quoted.identifiers=none;
+load data inpath '/user/lel/temp/record_data_backflow' overwrite into table wl_base.t_base_record_data_backflow partition(ds='00tmp');
+insert overwrite table wl_base.t_base_record_data_backflow partition(ds=$today)
+select `(ds)?+.+` from wl_base.t_base_record_data_backflow where ds='00tmp'
 union all
-select * from wl_base.t_base_record_data_backflow where ds=$last_update;
+select `(ds)?+.+` from wl_base.t_base_record_data_backflow where ds=$last_update;
 EOF
