a02c47ae2669ca72d3e6632baa9772d9cc8113b0
Gbacillus <gs464gan@gmail.com>
2017-05-08 16:40:08 +0800

    s

--- wrt/qianxing/ftp_hadoop.sh
+++ wrt/qianxing/ftp_hadoop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-zuotian=$(date -d '1 days ago' +%Y%m%d)
+zuotian=$(date -d '0 days ago' +%Y%m%d)
 
 wget ftp://ftp.blhdk.wolongdata.com/$zuotian*.csv --ftp-user=qx --ftp-password=NzS7d2oWe47LPVXx --no-passive-ftp
 hfs -put ./$zuotian*.csv /user/wrt/qianxing/
