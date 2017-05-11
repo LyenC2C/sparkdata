aba6d0aa9a3f7b41fb945adc2c01aa814c0c7bb6
Gbacillus <gs464gan@gmail.com>
2017-05-11 11:51:34 +0800

    s

--- wrt/qianxing/jiema.py
+++ wrt/qianxing/jiema.py
@@ -11,5 +11,5 @@ for line in reader:
         try:
             print "\001".join(ln.decode('gbk').encode('utf-8') for ln in line)
         except:
-            print "haha "+ str(n)
+            print "fuhuodezero "+ str(n)
     n += 1
