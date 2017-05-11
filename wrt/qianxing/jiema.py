144c20b09ae8b66c6f70674833ce5dc4148e705e
Gbacillus <gs464gan@gmail.com>
2017-05-11 14:41:34 +0800

    s

--- wrt/qianxing/jiema.py
+++ wrt/qianxing/jiema.py
@@ -9,7 +9,8 @@ n = 0
 for line in reader:
     if n != 0:
         try:
-            print "\001".join(ln.decode('gbk').encode('utf-8') for ln in line)
+            print "",
+            #print "\001".join(ln.decode('gbk').encode('utf-8') for ln in line)
         except:
             print "fuhuodezero "+ str(n)
     n += 1
