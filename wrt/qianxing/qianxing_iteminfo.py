f9d4bb31b2d27e74e11df7126bb1073244346169
Gbacillus <gs464gan@gmail.com>
2017-05-11 10:04:49 +0800

    s

--- wrt/qianxing/qianxing_iteminfo.py
+++ wrt/qianxing/qianxing_iteminfo.py
@@ -30,6 +30,9 @@ def f(line):
     ts = ss[0]
     ob = json.loads(valid_jsontxt(ss[2]))
     if type(ob) != type({}):return None
+    seller = ob.get('seller',{})
+    shopId = seller.get('shopId','-')
+    if shopId != '68907524': return None
     itemInfoModel = ob.get('itemInfoModel',"-")
     if itemInfoModel == "-": return None
     title = itemInfoModel.get('title','-').replace("\n","").decode("utf-8")
@@ -62,22 +65,22 @@ schema = StructType([
 
 df=hiveContext.createDataFrame(rdd,schema)
 hiveContext.registerDataFrameAsTable(df,'qianxing_iteminfo')
-# hiveContext.sql("insert overwrite table wl_base.t_base_qianxing_iteminfo partition (ds =" + today + ")\
-#  select * from qianxing_iteminfo")
-sql_merge = '''
-insert overwrite table wl_base.t_base_qianxing_iteminfo partition (ds =''' + today + ''')
-select
-COALESCE(t1.item_id,t2.item_id),
-COALESCE(t1.title,t2.title),
-COALESCE(t1.ts,t2.ts)
-from
-qianxing_iteminfo t1
-full join
-(select * from  wl_base.t_base_qianxing_iteminfo where ds =''' + yesterday +''')t2
-on
-t1.item_id = t2.item_id
-'''
-hiveContext.sql(sql_merge)
+hiveContext.sql("insert overwrite table wl_base.t_base_qianxing_iteminfo partition (ds =" + today + ")\
+select * from qianxing_iteminfo")
+# sql_merge = '''
+# insert overwrite table wl_base.t_base_qianxing_iteminfo partition (ds =''' + today + ''')
+# select
+# COALESCE(t1.item_id,t2.item_id),
+# COALESCE(t1.title,t2.title),
+# COALESCE(t1.ts,t2.ts)
+# from
+# qianxing_iteminfo t1
+# full join
+# (select * from  wl_base.t_base_qianxing_iteminfo where ds =''' + yesterday +''')t2
+# on
+# t1.item_id = t2.item_id
+# '''
+# hiveContext.sql(sql_merge)
 # rdd.saveAsTextFile('/user/wrt/temp/iteminfo_tmp')
 
 
